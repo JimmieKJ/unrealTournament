13a6e7edceb185d93cfc93d41ca70a51f833976b
Joe Wilcox <Joe.Wilcox@epicgames.com>
2014-05-14 17:11:12 -0400

    Fixed a typo. Thanks Pastasfuture and MariusMyburg #UT
    [CL 2073436 by Joe Wilcox in UT branch]

--- UnrealTournament/Source/UnrealTournament/UTHUD.cpp
+++ UnrealTournament/Source/UnrealTournament/UTHUD.cpp
@@ -6,8 +6,8 @@
 AUTHUD::AUTHUD(const class FPostConstructInitializeProperties& PCIP) : Super(PCIP)
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshiarTexObj(TEXT("/Game/Textures/Crosshair"));
-	CrosshairTex = CrosshiarTexObj.Object;
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Textures/Crosshair"));
+	CrosshairTex = CrosshairTexObj.Object;
 }
 
 
@@ -21,7 +21,7 @@ void AUTHUD::DrawHUD()
 	const FVector2D Center(Canvas->ClipX * 0.5f, Canvas->ClipY * 0.5f);
 
 	// offset by half the texture's dimensions so that the center of the texture aligns with the center of the Canvas
-	const FVector2D CrosshairDrawPosition( (Center.X - (CrosshairTex->GetSurfaceWidth() * 0.5)),
+	const FVector2D CrosshairDrawPosition( (Center.X - (CrosshairTex->GetSurfaceWidth() * 0.5f)),
 										   (Center.Y - (CrosshairTex->GetSurfaceHeight() * 0.5f)) );
 
 	// draw the crosshair
