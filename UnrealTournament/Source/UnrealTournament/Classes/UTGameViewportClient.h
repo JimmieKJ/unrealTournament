ceeab005378a2e16d065181f8f636d8b12d9b653
Joe Wilcox <Joe.Wilcox@epicgames.com>
2014-06-12 22:17:30 -0400

    - Added a UTGameViewportClient #ut
    [CL 2104023 by Joe Wilcox in UT branch]

---
+++ UnrealTournament/Source/UnrealTournament/Classes/UTGameViewportClient.h
@@ -0,0 +1,11 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+#pragma once
+
+#include "UTGameViewportClient.generated.h"
+
+UCLASS()
+class UUTGameViewportClient : public UGameViewportClient
+{
+	GENERATED_UCLASS_BODY()
+};
+
---
+++ UnrealTournament/Source/UnrealTournament/Private/UTGameViewportClient.cpp
@@ -0,0 +1,11 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealTournament.h"
+#include "UTGameViewportClient.h"
+
+
+UUTGameViewportClient::UUTGameViewportClient(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+}
+
