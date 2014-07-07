edb32555b55d3f395c38cdff72faf3330a2b8f0b
Steve Polge <steve.polge@epicgames.com>
2014-07-07 17:45:09 -0400

    Added support for specifying player character class on commandline. ?Playerclass=/game/restrictedassets/blueprints/wip/steve/SteveUTCharacter.SteveUTCharacter_c  will give the default character.  Also, can use the gamemode alias array to create a shortcut for the fully specifed path (see DefaultGame.ini)
    [CL 2171002 by Steve Polge in UT branch]

--- UnrealTournament/Source/UnrealTournament/Private/UTGameMode.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/UTGameMode.cpp
@@ -121,6 +121,22 @@ void AUTGameMode::InitGame( const FString& MapName, const FString& Options, FStr
 			AddMutator(LeftOpt);
 		}
 	}
+
+	InOpt = ParseOption(Options, TEXT("Playerclass"));
+	if (InOpt.Len() > 0)
+	{
+		UE_LOG(UT, Log, TEXT("Set PlayerClass: %s"), *InOpt);
+
+		// Note that to specify the normal SteveUTCharacter, the command is ?Playerclass=/game/restrictedassets/blueprints/wip/steve/SteveUTCharacter.SteveUTCharacter_c
+		// allow remapping using the GameModeClassName array in Game.ini
+		InOpt = AGameMode::StaticGetFullGameClassName(InOpt);
+		TSubclassOf<AUTCharacter> CharClass = LoadClass<AUTCharacter>(NULL, *InOpt, NULL, 0, NULL);
+		if (CharClass)
+		{
+			UE_LOG(UT, Log, TEXT("FOUND PlayerClass: %s"), *InOpt);
+			DefaultPawnClass = CharClass;
+		}
+	}
 }
 
 void AUTGameMode::AddMutator(const FString& MutatorPath)
