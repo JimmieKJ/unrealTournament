0ea1d06f373c5fd1ed7bad8eee055af3c74ed4f8
Matt Oelfke <Matt.Oelfke@epicgames.com>
2014-07-11 15:58:03 -0400

    Fixed flak shard damage attenuation; centered flak fire offset
    [CL 2192406 by Matt Oelfke in UT branch]

--- UnrealTournament/Source/UnrealTournament/Private/UTProj_FlakShard.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/UTProj_FlakShard.cpp
@@ -7,8 +7,8 @@
 AUTProj_FlakShard::AUTProj_FlakShard(const class FPostConstructInitializeProperties& PCIP)
 	: Super(PCIP)
 {
-	ProjectileMovement->InitialSpeed = 7000.0f;
-	ProjectileMovement->MaxSpeed = 7000.0f;
+	ProjectileMovement->InitialSpeed = 6500.0f;
+	ProjectileMovement->MaxSpeed = 6500.0f;
 	ProjectileMovement->ProjectileGravityScale = 0.f;
 	ProjectileMovement->bShouldBounce = true;
 	ProjectileMovement->BounceVelocityStopSimulatingThreshold = 0.0f;
@@ -19,7 +19,7 @@ AUTProj_FlakShard::AUTProj_FlakShard(const class FPostConstructInitializePropert
 	Momentum = 28000.f;
 
 	DamageAttenuation = 5.0f;
-	DamageAttenuationDelay = 1.0f;
+	DamageAttenuationDelay = 0.75f;
 	MinDamageSpeed = 800.f;
 
 	InitialLifeSpan = 2.f;
@@ -61,7 +61,7 @@ void AUTProj_FlakShard::OnBounce(const struct FHitResult& ImpactResult, const FV
 FRadialDamageParams AUTProj_FlakShard::GetDamageParams_Implementation(AActor* OtherActor, const FVector& HitLocation, float& OutMomentum) const
 {
 	FRadialDamageParams Result = Super::GetDamageParams_Implementation(OtherActor, HitLocation, OutMomentum);
-	Result.BaseDamage = FMath::Max<float>(Result.MinimumDamage, Result.BaseDamage - DamageAttenuation * FMath::Max<float>(0.0f, InitialLifeSpan - GetLifeSpan() - DamageAttenuationDelay));
+	Result.BaseDamage = FMath::Max<float>(Result.MinimumDamage, Result.BaseDamage - DamageAttenuation * FMath::Max<float>(0.0f, GetClass()->GetDefaultObject<AUTProj_FlakShard>()->InitialLifeSpan - GetLifeSpan() - DamageAttenuationDelay));
 	return Result;
 }
 
