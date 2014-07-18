80b162511482b91b774b1f2479a086b05f8ed272
Joe Wilcox <Joe.Wilcox@epicgames.com>
2014-07-18 16:56:43 -0400

    - Moved Malcolm's mesh
    [CL 2223916 by Joe Wilcox in UT branch]

--- UnrealTournament/Source/UnrealTournament/Private/UTDMGameMode.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/UTDMGameMode.cpp
@@ -1,10 +1,12 @@
 // Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
 
 #include "UnrealTournament.h"
+#include "UTHUD_DM.h"
 #include "UTDMGameMode.h"
 
 
 AUTDMGameMode::AUTDMGameMode(const class FPostConstructInitializeProperties& PCIP)
 : Super(PCIP)
 {
+	HUDClass = AUTHUD_DM::StaticClass();
 }
