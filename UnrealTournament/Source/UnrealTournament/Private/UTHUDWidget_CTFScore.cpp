7b5f4658284cade830805ca97c473f698a3b1c8c
Peter Knepley <peter.knepley@epicgames.com>
2014-07-25 18:38:32 -0400

    Fix nullptr issue
    [CL 2231908 by Peter Knepley in UT branch]

--- UnrealTournament/Source/UnrealTournament/Private/UTHUDWidget_CTFScore.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/UTHUDWidget_CTFScore.cpp
@@ -33,7 +33,7 @@ void UUTHUDWidget_CTFScore::Draw_Implementation(float DeltaTime)
 		else if (FlagState == CarriedObjectState::Held)
 		{
 			DrawFlagIcon(-46 * RedScale,53 * RedScale, 43,35, 843, 50, 43,35, FLinearColor::Red, RedScale);
-			if (!CGS->GetFlagHolder(0)->PlayerName.IsEmpty())
+			if (CGS->GetFlagHolder(0) != nullptr && !CGS->GetFlagHolder(0)->PlayerName.IsEmpty())
 			{
 				DrawText(FText::FromString(CGS->GetFlagHolder(0)->PlayerName), -75* RedScale, 56 * RedScale, UTHUDOwner->GetFontFromSizeIndex(0), RedScale,1.0, FLinearColor::White, ETextHorzPos::Right);
 			}
@@ -51,7 +51,7 @@ void UUTHUDWidget_CTFScore::Draw_Implementation(float DeltaTime)
 		else if (FlagState == CarriedObjectState::Held)
 		{
 			DrawFlagIcon(46 * BlueScale,53 * BlueScale, 43,35, 843, 50, 43,35, FLinearColor::Blue, RedScale);
-			if (!CGS->GetFlagHolder(1)->PlayerName.IsEmpty())
+			if (CGS->GetFlagHolder(1) != nullptr && !CGS->GetFlagHolder(1)->PlayerName.IsEmpty())
 			{
 				DrawText(FText::FromString(CGS->GetFlagHolder(1)->PlayerName), 75* BlueScale, 56 * BlueScale, UTHUDOwner->GetFontFromSizeIndex(0), BlueScale,1.0, FLinearColor::White);
 			}
