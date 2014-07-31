6e743c5d2e6eca2e2e6c8c710087eb303f1fc18d
Matt Oelfke <Matt.Oelfke@epicgames.com>
2014-07-31 11:48:50 -0400

    Added teleporters
    [CL 2238417 by Matt Oelfke in UT branch]

---
+++ UnrealTournament/Source/UnrealTournament/Private/UTTeleporter.cpp
@@ -0,0 +1,167 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+#include "UnrealTournament.h"
+#include "UTTeleporter.h"
+#include "AI/Navigation/NavAreas/NavArea_Default.h"
+#include "AI/NavigationSystemHelpers.h"
+#include "AI/NavigationOctree.h"
+
+AUTTeleporter::AUTTeleporter(const FPostConstructInitializeProperties& PCIP)
+: Super(PCIP)
+{
+	TriggerBox = PCIP.CreateDefaultSubobject<UBoxComponent>(this, TEXT("TriggerBox"));
+	TriggerBox->SetCollisionProfileName(TEXT("Trigger"));
+	RootComponent = TriggerBox;
+
+#if WITH_EDITORONLY_DATA
+	ExitArrow = PCIP.CreateEditorOnlyDefaultSubobject<UArrowComponent>(this, TEXT("ExitArrow"));
+	if (ExitArrow != NULL)
+	{
+		ExitArrow->AttachParent = RootComponent;
+		ExitArrow->ArrowColor = FLinearColor(0.0f, 0.0f, 1.0f, 1.0f);
+	}
+	bLockTeleportTarget = true;
+#endif
+
+	bSetRotation = true;
+	TeleportTarget = FTransform(FRotator::ZeroRotator, FVector(100.0f, 0.0f, 100.0f));
+
+	// note that we use the Actor event so adding different collision primitives automatically works, as we don't need the more detailed parameters of the component version
+	OnActorBeginOverlap.AddDynamic(this, &AUTTeleporter::OnOverlapBegin);
+
+	TeleportTarget.SetIdentity();
+}
+
+bool AUTTeleporter::CanTeleport_Implementation(AActor* OtherActor)
+{
+	AUTCharacter* C = Cast<AUTCharacter>(OtherActor);
+	return (C != NULL && !C->IsDead() && C->Controller != NULL);
+}
+
+void AUTTeleporter::OnOverlapBegin(AActor* OtherActor)
+{
+	if (OtherActor != NULL && CanTeleport(OtherActor))
+	{
+		FTransform WorldTransform = TeleportTarget * ActorToWorld();
+		FVector AdjustedTeleportLoc = WorldTransform.GetLocation();
+		FRotator TargetRot = WorldTransform.Rotator();
+		// don't pitch or roll Pawns
+		if (OtherActor->IsA(APawn::StaticClass()))
+		{
+			TargetRot.Pitch = 0.0f;
+			TargetRot.Roll = 0.0f;
+		}
+		if (GetWorld()->FindTeleportSpot(OtherActor, AdjustedTeleportLoc, bSetRotation ? TargetRot : OtherActor->GetActorRotation()))
+		{
+			if (bSetRotation)
+			{
+				OtherActor->SetActorLocationAndRotation(AdjustedTeleportLoc, TargetRot, false);
+			}
+			else
+			{
+				OtherActor->SetActorLocation(AdjustedTeleportLoc, false);
+			}
+			APawn* P = Cast<APawn>(OtherActor);
+			if (P != NULL && P->Controller != NULL)
+			{
+				P->Controller->ClientSetLocation(P->GetActorLocation(), P->GetActorRotation());
+			}
+		}
+		else
+		{
+			UE_LOG(UT, Warning, TEXT("Teleporter %s failed to teleport %s"), *GetName(), *OtherActor->GetName());
+		}
+	}
+}
+
+void AUTTeleporter::PostTeleport_Implementation(AActor* OtherActor)
+{
+
+}
+
+#if WITH_EDITOR
+void AUTTeleporter::UpdateExitArrow()
+{
+	if (ExitArrow != NULL)
+	{
+		ExitArrow->SetRelativeLocation(TeleportTarget.GetLocation());
+		ExitArrow->SetRelativeRotation(TeleportTarget.Rotator());
+	}
+}
+void AUTTeleporter::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+	UpdateNavigationRelevancy();
+
+	if (PropertyChangedEvent.Property && IsNavigationRelevant() && GetWorld() != NULL && GetWorld()->GetNavigationSystem() != NULL)
+	{
+		GetWorld()->GetNavigationSystem()->UpdateNavOctree(this);
+	}
+
+	UpdateExitArrow();
+}
+// below editor functions are a workaround to the editor FTransform widget not having a setting to work in world space
+void AUTTeleporter::EditorApplyTranslation(const FVector& DeltaTranslation, bool bAltDown, bool bShiftDown, bool bCtrlDown)
+{
+	FTransform PrevTeleportTarget = TeleportTarget * ActorToWorld();
+	Super::EditorApplyTranslation(DeltaTranslation, bAltDown, bShiftDown, bCtrlDown);
+	if (bLockTeleportTarget)
+	{
+		TeleportTarget = PrevTeleportTarget;
+		TeleportTarget.SetToRelativeTransform(ActorToWorld());
+	}
+	UpdateExitArrow();
+}
+void AUTTeleporter::EditorApplyRotation(const FRotator& DeltaRotation, bool bAltDown, bool bShiftDown, bool bCtrlDown)
+{
+	FTransform PrevTeleportTarget = TeleportTarget * ActorToWorld();
+	Super::EditorApplyRotation(DeltaRotation, bAltDown, bShiftDown, bCtrlDown);
+	if (bLockTeleportTarget)
+	{
+		TeleportTarget = PrevTeleportTarget;
+		TeleportTarget.SetToRelativeTransform(ActorToWorld());
+	}
+	UpdateExitArrow();
+}
+void AUTTeleporter::EditorApplyScale(const FVector& DeltaScale, const FVector* PivotLocation, bool bAltDown, bool bShiftDown, bool bCtrlDown)
+{
+	FTransform PrevTeleportTarget = TeleportTarget * ActorToWorld();
+	Super::EditorApplyScale(DeltaScale, PivotLocation, bAltDown, bShiftDown, bCtrlDown);
+	if (bLockTeleportTarget)
+	{
+		TeleportTarget = PrevTeleportTarget;
+		TeleportTarget.SetToRelativeTransform(ActorToWorld());
+	}
+	UpdateExitArrow();
+}
+#endif
+
+bool AUTTeleporter::UpdateNavigationRelevancy()
+{
+	PointLink = FNavigationLink(FVector(0.0f), TeleportTarget.GetLocation());
+	PointLink.Direction = ENavLinkDirection::LeftToRight;
+	PointLink.AreaClass = UNavArea_Default::StaticClass(); // TODO: figure out the right setting here
+
+	SetNavigationRelevancy(true);
+	return true;
+}
+
+bool AUTTeleporter::GetNavigationRelevantData(struct FNavigationRelevantData& Data) const
+{
+	TArray<FNavigationLink> TempList;
+	TempList.Add(PointLink);
+
+	NavigationHelper::ProcessNavLinkAndAppend(&Data.Modifiers, this, TempList);
+	return false;
+}
+
+bool AUTTeleporter::GetNavigationLinksClasses(TArray<TSubclassOf<class UNavLinkDefinition> >& OutClasses) const
+{
+	return false;
+}
+
+bool AUTTeleporter::GetNavigationLinksArray(TArray<FNavigationLink>& OutLink, TArray<FNavigationSegmentLink>& OutSegments) const
+{
+	OutLink.Add(PointLink);
+	return true;
+}
---
+++ UnrealTournament/Source/UnrealTournament/Public/UTTeleporter.h
@@ -0,0 +1,72 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "UTTeleporter.generated.h"
+
+/**
+* jump pad for launching characters
+*/
+UCLASS(Blueprintable, Abstract)
+class AUTTeleporter : public AActor, public INavLinkHostInterface, public INavRelevantActorInterface
+{
+	GENERATED_UCLASS_BODY()
+
+	/** The player will teleport when overlapping this box */
+	UPROPERTY(VisibleAnywhere, Category = Teleporter)
+	TSubobjectPtr<class UBoxComponent> TriggerBox;
+
+#if WITH_EDITORONLY_DATA
+	/** arrow component to show exit direction defined in TeleportTarget if bSetRotation is true */
+	UPROPERTY(VisibleAnywhere, Category = Teleporter)
+	TSubobjectPtr<class UArrowComponent> ExitArrow;
+
+	/** if set then lock the TeleportTarget when moving/rotating the teleporter */
+	UPROPERTY(EditAnywhere, Category = Editor)
+	bool bLockTeleportTarget;
+#endif
+
+	/** The destination of this teleporter (note: scale is ignored)
+	 * NOTE: local space; editor doesn't give the option for world space
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Teleporter, meta = (MakeEditWidget = ""))
+	FTransform TeleportTarget;
+
+	/** if true set rotation of target to the rotation in TeleportTarget */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Teleporter)
+	bool bSetRotation;
+
+	/** returns whether we support teleporting OtherActor */
+	UFUNCTION(BlueprintNativeEvent, BlueprintAuthorityOnly)
+	bool CanTeleport(AActor* OtherActor);
+
+	/** called after teleportation is complete
+	 * note that this is still server only so any effects need to be replicated in some way
+	 */
+	UFUNCTION(BlueprintNativeEvent, BlueprintAuthorityOnly)
+	void PostTeleport(AActor* OtherActor);
+
+	UFUNCTION()
+	virtual void OnOverlapBegin(AActor* OtherActor);
+
+#if WITH_EDITOR
+	void UpdateExitArrow();
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+	virtual void EditorApplyTranslation(const FVector& DeltaTranslation, bool bAltDown, bool bShiftDown, bool bCtrlDown) override;
+	virtual void EditorApplyRotation(const FRotator& DeltaRotation, bool bAltDown, bool bShiftDown, bool bCtrlDown) override;
+	virtual void EditorApplyScale(const FVector& DeltaScale, const FVector* PivotLocation, bool bAltDown, bool bShiftDown, bool bCtrlDown) override;
+#endif
+
+	/** The Nav link between the teleporter and the target */
+	FNavigationLink PointLink;
+
+	// BEGIN INavRelevantActorInterface
+	virtual bool UpdateNavigationRelevancy() override;
+	virtual bool GetNavigationRelevantData(struct FNavigationRelevantData& Data) const override;
+	// END INavRelevantActorInterface
+
+	// BEGIN INavLinkHostInterface
+	virtual bool GetNavigationLinksClasses(TArray<TSubclassOf<class UNavLinkDefinition> >& OutClasses) const override;
+	virtual bool GetNavigationLinksArray(TArray<FNavigationLink>& OutLink, TArray<FNavigationSegmentLink>& OutSegments) const override;
+	// END INavLinkHostInterface
+};
