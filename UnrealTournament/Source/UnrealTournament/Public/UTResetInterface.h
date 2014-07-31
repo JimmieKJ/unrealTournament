67a0e755a19694f1e15fbe121ee2fdb8015e7827
Matt Oelfke <Matt.Oelfke@epicgames.com>
2014-07-31 16:14:37 -0400

    Added missing file
    [CL 2238732 by Matt Oelfke in UT branch]

---
+++ UnrealTournament/Source/UnrealTournament/Public/UTResetInterface.h
@@ -0,0 +1,19 @@
+// implement this interface for Actors that should handle game mode resets (halftime in CTF, role swap in Assault, etc)
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+#pragma once
+
+#include "UTResetInterface.generated.h"
+
+UINTERFACE(MinimalAPI)
+class UUTResetInterface : public UInterface
+{
+	GENERATED_UINTERFACE_BODY()
+};
+
+class UNREALTOURNAMENT_API IUTResetInterface
+{
+	GENERATED_IINTERFACE_BODY()
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = Game)
+	void Reset();
+};
