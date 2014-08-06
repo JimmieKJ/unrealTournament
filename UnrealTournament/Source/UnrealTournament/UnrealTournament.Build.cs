9bfe5d1663245a0bf894148a52c328e08ce94a0e
Matt Oelfke <Matt.Oelfke@epicgames.com>
2014-08-06 18:38:48 -0400

    Attempt to reduce stuff included in the dedicated server build
    [CL 2246256 by Matt Oelfke in UT branch]

--- UnrealTournament/Source/UnrealTournament/UnrealTournament.Build.cs
+++ UnrealTournament/Source/UnrealTournament/UnrealTournament.Build.cs
@@ -4,15 +4,19 @@ using UnrealBuildTool;
 
 public class UnrealTournament : ModuleRules
 {
-	public UnrealTournament(TargetInfo Target)
-	{
+    public UnrealTournament(TargetInfo Target)
+    {
         bFasterWithoutUnity = true;
         MinFilesUsingPrecompiledHeaderOverride = 1;
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "RHI", "Slate", "SlateCore", "SlateRHIRenderer" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule" });
+        if (Target.Type != TargetRules.TargetType.Server)
+        {
+            PublicDependencyModuleNames.AddRange(new string[] { "RHI", "Slate", "SlateCore", "SlateRHIRenderer" });
+        }
         if (Target.Type == TargetRules.TargetType.Editor)
         {
             PublicDependencyModuleNames.AddRange(new string[] { "UnrealEd", "PropertyEditor" });
         }
-	}
+    }
 }
