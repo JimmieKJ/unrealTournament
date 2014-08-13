05fb82c909d8731d325af88a412492652b4e52c8
Steve Polge <steve.polge@epicgames.com>
2014-08-13 15:34:11 -0400

    Added C++ base class for enforcer.
    [CL 2255082 by Steve Polge in UT branch]

---
+++ UnrealTournament/Source/UnrealTournament/Private/UTWeap_Enforcer.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+#include "UnrealTournament.h"
+#include "UTWeap_Enforcer.h"
+
+AUTWeap_Enforcer::AUTWeap_Enforcer(const FPostConstructInitializeProperties& PCIP)
+: Super(PCIP)
+{
+
+}
---
+++ UnrealTournament/Source/UnrealTournament/Public/UTWeap_Enforcer.h
@@ -0,0 +1,9 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+#include "UTWeap_Enforcer.generated.h"
+
+UCLASS()
+class AUTWeap_Enforcer : public AUTWeapon
+{
+	GENERATED_UCLASS_BODY()
+};
