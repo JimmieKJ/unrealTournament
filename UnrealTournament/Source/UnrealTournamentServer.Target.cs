4b7da95ea07435596aac6765b943afc21a2c1144
Peter Knepley <peter.knepley@epicgames.com>
2014-08-19 18:07:23 -0400

    Dedicated server build target update
    [CL 2263046 by Peter Knepley in UT branch]

--- UnrealTournament/Source/UnrealTournamentServer.Target.cs
+++ UnrealTournament/Source/UnrealTournamentServer.Target.cs
@@ -7,19 +7,14 @@ public class UnrealTournamentServerTarget : TargetRules
 {
     public UnrealTournamentServerTarget(TargetInfo Target)
 	{
-		Type = TargetType.Server;
+        Type = TargetType.Server;
+        bUsesSlate = false;
 	}
 
 	//
 	// TargetRules interface.
 	//
-
-	public override bool GetSupportedPlatforms(ref List<UnrealTargetPlatform> OutPlatforms)
-	{
-		// It is valid for only desktop platforms
-		return UnrealBuildTool.UnrealBuildTool.GetAllDesktopPlatforms(ref OutPlatforms, false);
-	}
-
+    
 	public override void SetupBinaries(
 		TargetInfo Target,
 		ref List<UEBuildBinaryConfiguration> OutBuildBinaryConfigurations,
@@ -28,13 +23,4 @@ public class UnrealTournamentServerTarget : TargetRules
 	{
         OutExtraModuleNames.Add("UnrealTournament");
 	}
-
-    public override List<UnrealTargetPlatform> GUBP_GetPlatforms_MonolithicOnly(UnrealTargetPlatform HostPlatform)
-    {
-        if (HostPlatform == UnrealTargetPlatform.Win64)
-        {
-            return new List<UnrealTargetPlatform> { UnrealTargetPlatform.Win32 };
-        }
-        return new List<UnrealTargetPlatform>();
-    }
 }
