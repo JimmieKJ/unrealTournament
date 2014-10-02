baf0f5c0ecee0a1c24e0520319324304eebfd3e5
Peter Knepley <peter.knepley@epicgames.com>
2014-10-02 13:14:03 -0400

    Fix build break
    [CL 2317521 by Peter Knepley in UT branch]

--- UnrealTournament/Source/UnrealTournament/Public/UTLobbyGameState.h
+++ UnrealTournament/Source/UnrealTournament/Public/UTLobbyGameState.h
@@ -1,6 +1,8 @@
 // Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
 #pragma once
 
+#include "UTLobbyPlayerState.h"
+
 #include "UTLobbyGameState.generated.h"
 
 UCLASS(notplaceable, Config = Game)
