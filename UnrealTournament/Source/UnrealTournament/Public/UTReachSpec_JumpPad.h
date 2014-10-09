fca3ef816c0393a053b14bac5331bce6437562e4
Matt Oelfke <Matt.Oelfke@epicgames.com>
2014-10-09 11:40:42 -0400

    Added missing file
    [CL 2324217 by Matt Oelfke in UT branch]

---
+++ UnrealTournament/Source/UnrealTournament/Public/UTReachSpec_JumpPad.h
@@ -0,0 +1,48 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "UTReachSpec.h"
+#include "UTRecastNavMesh.h"
+
+#include "UTReachSpec_JumpPad.generated.h"
+
+UCLASS(CustomConstructor)
+class UUTReachSpec_JumpPad : public UUTReachSpec
+{
+	GENERATED_UCLASS_BODY()
+
+	/** the jump pad Actor that needs to be touched to jump */
+	UPROPERTY()
+	TWeakObjectPtr<AActor> JumpPad;
+
+	UUTReachSpec_JumpPad(const FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+	{
+		PathColor = FLinearColor(0.0f, 0.0f, 1.0f);
+	}
+
+	virtual TWeakObjectPtr<AActor> GetMoveTargetActor() const override
+	{
+		return JumpPad;
+	}
+
+	virtual int32 CostFor(int32 DefaultCost, const FUTPathLink& OwnerLink, APawn* Asker, const FNavAgentProperties& AgentProps, NavNodeRef StartPoly, const class AUTRecastNavMesh* NavMesh) const override
+	{
+		// TODO: check if pad is active?
+		return JumpPad.IsValid() ? DefaultCost : BLOCKED_PATH_COST;
+	}
+
+	virtual bool GetMovePoints(const FUTPathLink& OwnerLink, const FVector& StartLoc, APawn* Asker, const FNavAgentProperties& AgentProps, const struct FRouteCacheItem& Target, const TArray<FRouteCacheItem>& FullRoute, const class AUTRecastNavMesh* NavMesh, TArray<FComponentBasedPosition>& MovePoints) const
+	{
+		TArray<NavNodeRef> PolyRoute;
+		NavMesh->FindPolyPath(StartLoc, AgentProps, FRouteCacheItem(NavMesh->GetPolyCenter(OwnerLink.StartEdgePoly), OwnerLink.StartEdgePoly), PolyRoute, false);
+		NavMesh->DoStringPulling(StartLoc, PolyRoute, AgentProps, MovePoints);
+		if (JumpPad.IsValid())
+		{
+			MovePoints.Add(FComponentBasedPosition(JumpPad->GetRootComponent(), JumpPad->GetActorLocation()));
+		}
+		MovePoints.Add(FComponentBasedPosition(Target.GetLocation(Asker)));
+		return true;
+	}
+};
