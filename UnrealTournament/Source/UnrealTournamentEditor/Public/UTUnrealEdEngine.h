678aa878531083bf8f3bc8fc3437c9d36acf5417
Matt Oelfke <Matt.Oelfke@epicgames.com>
2014-12-11 11:13:26 -0500

    Removed no longer needed PIE gametype finding code as engine was fixed in 4.6
    [CL 2385628 by Matt Oelfke in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournamentEditor/Public/UTUnrealEdEngine.h
+++ UnrealTournament/Source/UnrealTournamentEditor/Public/UTUnrealEdEngine.h
@@ -18,42 +18,5 @@ class UUTUnrealEdEngine : public UUnrealEdEngine
 	: Super(ObjectInitializer)
 	{}
 
-	// FIXME: workaround for engine bugs with default map prefixes
-	virtual FString BuildPlayWorldURL(const TCHAR* MapName, bool bSpectatorMode = false, FString AdditionalURLOptions = FString())
-	{
-		FString URL = Super::BuildPlayWorldURL(MapName, bSpectatorMode, AdditionalURLOptions);
-		FURL RealURL = FURL(NULL, *URL, TRAVEL_Absolute);
-		if (!RealURL.HasOption(TEXT("game")))
-		{
-			FString MapNameNoPIE(FPaths::GetBaseFilename(MapName));
-			MapNameNoPIE.RemoveFromStart(PLAYWORLD_PACKAGE_PREFIX);
-			if (MapNameNoPIE.StartsWith(TEXT("_")))
-			{
-				MapNameNoPIE.RemoveFromStart(TEXT("_"));
-				MapNameNoPIE = MapNameNoPIE.RightChop(MapNameNoPIE.Find(TEXT("_")) + 1);
-			}
-			
-			// see if we have a per-prefix default specified
-			bool bFound = false;
-			AUTWorldSettings* Settings = AUTWorldSettings::StaticClass()->GetDefaultObject<AUTWorldSettings>();
-			for (int32 Idx = 0; Idx < Settings->DefaultMapPrefixes.Num(); Idx++)
-			{
-				const FGameModePrefix& GTPrefix = Settings->DefaultMapPrefixes[Idx];
-				if (GTPrefix.Prefix.Len() > 0 && MapNameNoPIE.StartsWith(GTPrefix.Prefix))
-				{
-					URL += TEXT("?game=") + GTPrefix.GameMode;
-					bFound = true;
-					break;
-				}
-			}
-			if (!bFound)
-			{
-				URL += TEXT("?game=") + AUTMenuGameMode::StaticClass()->GetDefaultObject<AUTMenuGameMode>()->SetGameMode(MapNameNoPIE, TEXT(""), TEXT(""))->GetPathName();
-			}
-		}
-
-		return URL;
-	}
-
 	UT_LOADMAP_DEFINITION()
 };
