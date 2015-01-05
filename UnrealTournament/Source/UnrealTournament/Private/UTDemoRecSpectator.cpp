feb86d8e72c0b05c5777165c53a9f967dd64fd1c
Matt Oelfke <Matt.Oelfke@epicgames.com>
2015-01-05 15:40:34 -0500

    Fixed scoreboard and HUD in demo playback; fixed switching between players to view in demo playback
    [CL 2397927 by Matt Oelfke in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/UTDemoRecSpectator.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/UTDemoRecSpectator.cpp
@@ -8,6 +8,92 @@ AUTDemoRecSpectator::AUTDemoRecSpectator(const FObjectInitializer& OI)
 {
 }
 
+void AUTDemoRecSpectator::ViewAPlayer(int32 dir)
+{
+	BehindView(bSpectateBehindView);
+
+	APlayerState* const PlayerState = GetNextViewablePlayer(dir);
+	if (PlayerState != NULL)
+	{
+		// we have to redirect back to the Pawn because engine hardcoded FTViewTarget code will reject a PlayerState with NULL owner
+		for (FConstPawnIterator It = GetWorld()->GetPawnIterator(); It; ++It)
+		{
+			if (It->IsValid() && It->Get()->PlayerState == PlayerState)
+			{
+				SetViewTarget(It->Get());
+			}
+		}
+	}
+}
+APlayerState* AUTDemoRecSpectator::GetNextViewablePlayer(int32 dir)
+{
+	int32 CurrentIndex = -1;
+	if (PlayerCameraManager->ViewTarget.GetTargetPawn() != NULL)
+	{
+		APlayerState* TestPS = PlayerCameraManager->ViewTarget.GetTargetPawn()->PlayerState;
+		// Find index of current viewtarget's PlayerState
+		for (int32 i = 0; i < GetWorld()->GameState->PlayerArray.Num(); i++)
+		{
+			if (TestPS == GetWorld()->GameState->PlayerArray[i])
+			{
+				CurrentIndex = i;
+				break;
+			}
+		}
+	}
+
+	// Find next valid viewtarget in appropriate direction
+	int32 NewIndex;
+	for (NewIndex = CurrentIndex + dir; (NewIndex >= 0) && (NewIndex < GetWorld()->GameState->PlayerArray.Num()); NewIndex = NewIndex + dir)
+	{
+		APlayerState* const PlayerState = GetWorld()->GameState->PlayerArray[NewIndex];
+		if (PlayerState != NULL && !PlayerState->bOnlySpectator)
+		{
+			for (FConstPawnIterator It = GetWorld()->GetPawnIterator(); It; ++It)
+			{
+				if (It->IsValid() && It->Get()->PlayerState == PlayerState)
+				{
+					return PlayerState;
+				}
+			}
+		}
+	}
+
+	// wrap around
+	CurrentIndex = (NewIndex < 0) ? GetWorld()->GameState->PlayerArray.Num() : -1;
+	for (NewIndex = CurrentIndex + dir; (NewIndex >= 0) && (NewIndex < GetWorld()->GameState->PlayerArray.Num()); NewIndex = NewIndex + dir)
+	{
+		APlayerState* const PlayerState = GetWorld()->GameState->PlayerArray[NewIndex];
+		if (PlayerState != NULL && !PlayerState->bOnlySpectator)
+		{
+			for (FConstPawnIterator It = GetWorld()->GetPawnIterator(); It; ++It)
+			{
+				if (It->IsValid() && It->Get()->PlayerState == PlayerState)
+				{
+					return PlayerState;
+				}
+			}
+		}
+	}
+
+	return NULL;
+}
+
+void AUTDemoRecSpectator::ReceivedPlayer()
+{
+	Super::ReceivedPlayer();
+
+	if (GetWorld()->IsServer())
+	{
+		AUTGameMode* Game = GetWorld()->GetAuthGameMode<AUTGameMode>();
+		if (Game != NULL)
+		{
+			TSubclassOf<UUTScoreboard> ScoreboardClass = LoadClass<UUTScoreboard>(NULL, *Game->ScoreboardClassName.AssetLongPathname, NULL, LOAD_None, NULL);
+			ClientSetHUDAndScoreboard(Game->HUDClass, ScoreboardClass);
+		}
+	}
+}
+
 bool AUTDemoRecSpectator::CallRemoteFunction(UFunction* Function, void* Parameters, FOutParmRec* OutParms, FFrame* Stack)
 {
 	// if we're the demo server, record into the demo
--- UnrealTournament/Source/UnrealTournament/Public/UTDemoRecSpectator.h
+++ UnrealTournament/Source/UnrealTournament/Public/UTDemoRecSpectator.h
@@ -10,6 +10,24 @@ class AUTDemoRecSpectator : public AUTPlayerController
 {
 	GENERATED_UCLASS_BODY()
 
+	virtual void ReceivedPlayer() override;
+
+	virtual void OnFire() override
+	{
+		if (GetWorld()->GetGameState() != NULL)
+		{
+			ViewAPlayer(+1);
+		}
+	}
+	virtual void OnStopAltFire() override
+	{
+		ResetCameraMode();
+		SetViewTarget(this);
+	}
+
+	virtual void ViewAPlayer(int32 dir) override;
+	virtual APlayerState* GetNextViewablePlayer(int32 dir) override;
+
 	virtual bool CallRemoteFunction(UFunction* Function, void* Parameters, FOutParmRec* OutParms, FFrame* Stack) override;
 	
 	virtual void CleanupPlayerState()
