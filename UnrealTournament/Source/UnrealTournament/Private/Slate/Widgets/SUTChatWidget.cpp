a8eb0730999094d0b15a98b32ead8eafcb453a71
James Hopkin <James.Hopkin@epicgames.com>
2015-01-21 08:54:41 -0500

    #ut Committed correct version of SUTChatWidget
    [CL 2413684 by James Hopkin in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/Slate/Widgets/SUTChatWidget.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/Slate/Widgets/SUTChatWidget.cpp
@@ -1,29 +1,28 @@
 // Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
 
-#include "FortniteUIPCH.h"
-#include "SFortChatWidget.h"
+#include "../Public/UnrealTournament.h"
+#include "SUTChatWidget.h"
+#include "FriendsAndChat.h"
 
 #define CHAT_BOX_WIDTH 576.0f
 #define CHAT_BOX_HEIGHT 260.0f
 #define CHAT_BOX_PADDING 20.0f
 #define MAX_CHAT_MESSAGES 10
 
-void SFortChatWidget::Construct(const FArguments& InArgs, const FLocalPlayerContext& InCtx)
+void SUTChatWidget::Construct(const FArguments& InArgs, const FLocalPlayerContext& InCtx)
 {
-	FortHUDPCTrackerBase::Init(InCtx);
-
-	bKeepVisible = InArgs._bKeepVisible;
-	LastVisibility = bKeepVisible ? EVisibility::Visible : EVisibility::Hidden;
-	ChatFadeTime = 10.0;
-	LastChatLineTime = -1.0;
+	LastVisibility = EVisibility::Visible;
+	Ctx = InCtx;
 
 	ViewModel = IFriendsAndChatModule::Get().GetFriendsAndChatManager()->GetChatViewModel();
-	ViewModel->OnChatMessageCommitted().AddSP(this, &SFortChatWidget::OnChatTextCommitted);
-	ViewModel->OnChatListUpdated().AddSP(this, &SFortChatWidget::HandleChatListUpdated);
-	ViewModel->OnNetworkMessageSentEvent().AddSP(this, &SFortChatWidget::HandleFriendsNetworkChatMessage);
-	
+	ViewModel->OnChatMessageCommitted().AddSP(this, &SUTChatWidget::OnChatTextCommitted);
+	ViewModel->OnChatListUpdated().AddSP(this, &SUTChatWidget::HandleChatListUpdated);
+	ViewModel->OnNetworkMessageSentEvent().AddSP(this, &SUTChatWidget::HandleFriendsNetworkChatMessage);
+
 	//some constant values
 	const int32 PaddingValue = 2;
+
+	auto& FriendsAndChat = *IFriendsAndChatModule::Get().GetFriendsAndChatManager();
 
 	// Initialize Menu
 	ChildSlot
@@ -38,77 +37,43 @@ void SFortChatWidget::Construct(const FArguments& InArgs, const FLocalPlayerCont
 			.HeightOverride(CHAT_BOX_HEIGHT)
 			.WidthOverride(CHAT_BOX_WIDTH)
 			[
-				IFriendsAndChatModule::Get().GetFriendsAndChatManager()->GenerateChatWidget(&FFortUIStyle::Get().GetWidgetStyle< FFriendsAndChatStyle >( "FriendsStyle" ), ViewModel.ToSharedRef()).ToSharedRef()
+				FriendsAndChat.GenerateChatWidget(
+					&SUWindowsStyle::Get().GetWidgetStyle< FFriendsAndChatStyle >( "FriendsStyle" ),
+					ViewModel.ToSharedRef()
+				).ToSharedRef()
 			]
 		]
 	];
 
-	if(!bKeepVisible)
-	{
-		const FLinearColor OverrideFontColor = FLinearColor(1,1,1,0);
-		ViewModel->SetFontOverrideColor(OverrideFontColor);
-		ViewModel->SetEntryBarVisibility(EVisibility::Hidden);
-	}
-	else
-	{
-		ViewModel->SetEntryBarVisibility(EVisibility::Visible);
-	}
-
-	// Zone turns off global chat, lobby or anything else turns it back on
-	AFortPlayerController* FortPC = GetPlayerController();
-	AFortPlayerControllerZone* ZonePC = Cast<AFortPlayerControllerZone>(FortPC);
-	if (ZonePC)
-	{
-		ViewModel->SetInGameUI(true);
-	}
-	else
-	{
-		ViewModel->SetInGameUI(false);
-	}
+	ViewModel->SetEntryBarVisibility(EVisibility::Visible);
 }
 
-void SFortChatWidget::HandleFriendsNetworkChatMessage(const FString& NetworkMessage)
+void SUTChatWidget::HandleFriendsNetworkChatMessage(const FString& NetworkMessage)
 {
-	const double CurrentTime = FSlateApplication::Get().GetCurrentTime();
-	LastChatLineTime = CurrentTime;
 	ViewModel->SetOverrideColorActive(false);
-	GetPlayerController()->Say(NetworkMessage);
+//	Ctx.GetPlayerController()->Say(NetworkMessage);
 }
 
-void SFortChatWidget::HandleChatListUpdated()
+void SUTChatWidget::HandleChatListUpdated()
 {
-	const double CurrentTime = FSlateApplication::Get().GetCurrentTime();
-	LastChatLineTime = CurrentTime;
 	ViewModel->SetOverrideColorActive(false);
 }
 
-EVisibility SFortChatWidget::GetEntryVisibility() const
+void SUTChatWidget::OnChatTextCommitted()
 {
-	return ViewModel->GetEntryBarVisibility();
+//	FSlateApplication::Get().PlaySound(FFortUIStyle::Get().GetSound("Fortnite.ChatTextCommittedSound"));
 }
 
-void SFortChatWidget::SetEntryVisibility( TAttribute<EVisibility> InVisibility )
-{
-	if (ViewModel.IsValid())
-	{
-		ViewModel->SetEntryBarVisibility(InVisibility.Get());
-	}
-}
-
-void SFortChatWidget::OnChatTextCommitted()
-{
-	FSlateApplication::Get().PlaySound(FFortUIStyle::Get().GetSound("Fortnite.ChatTextCommittedSound"));
-}
-
-void SFortChatWidget::Tick(const FGeometry& AllottedGeometry, const double InCurrentTime, const float InDeltaTime)
+void SUTChatWidget::Tick(const FGeometry& AllottedGeometry, const double InCurrentTime, const float InDeltaTime)
 {
 	//Always tick the super
 	SCompoundWidget::Tick(AllottedGeometry, InCurrentTime, InDeltaTime);
 
-	if (GetEntryVisibility() != LastVisibility)
+	auto EntryBarVisibility = ViewModel->GetEntryBarVisibility();
+	if (EntryBarVisibility != LastVisibility)
 	{
-		LastVisibility = GetEntryVisibility();
-		if (LastVisibility == EVisibility::Visible)
+		LastVisibility = EntryBarVisibility;
+		if (EntryBarVisibility == EVisibility::Visible)
 		{
 			// Enter UI mode
 			SetFocus();
@@ -119,26 +84,9 @@ void SFortChatWidget::Tick(const FGeometry& AllottedGeometry, const double InCur
 			FSlateApplication::Get().SetAllUserFocusToGameViewport();
 		}
 	}
-
-	if(!bKeepVisible)
-	{
-		if(ViewModel->GetOverrideColorSet() && GetEntryVisibility() ==  EVisibility::Visible)
-		{
-			ViewModel->SetOverrideColorActive(false);
-		}
-		else if(!ViewModel->GetOverrideColorSet() && GetEntryVisibility() != EVisibility::Visible)
-		{
-			const double CurrentTime = FSlateApplication::Get().GetCurrentTime();
-			if(LastChatLineTime + ChatFadeTime < CurrentTime)
-			{
-				ViewModel->SetOverrideColorActive(true);
-				SetEntryVisibility(EVisibility::Hidden);
-			}
-		}
-	}
 }
 
-void SFortChatWidget::SetFocus()
+void SUTChatWidget::SetFocus()
 {
 	FSlateApplication::Get().SetKeyboardFocus(SharedThis(this));
 
@@ -148,7 +96,7 @@ void SFortChatWidget::SetFocus()
 	}
 }
 
-FReply SFortChatWidget::OnFocusReceived( const FGeometry& MyGeometry, const FFocusEvent& InFocusEvent )
+FReply SUTChatWidget::OnFocusReceived( const FGeometry& MyGeometry, const FFocusEvent& InFocusEvent )
 {
 	return FReply::Handled().ReleaseMouseCapture().LockMouseToWidget( SharedThis( this ) );
 }
--- UnrealTournament/Source/UnrealTournament/Private/Slate/Widgets/SUTChatWidget.h
+++ UnrealTournament/Source/UnrealTournament/Private/Slate/Widgets/SUTChatWidget.h
@@ -1,48 +1,36 @@
-// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
-
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
 #pragma once
 
 #include "SlateBasics.h"
-#include "FortniteGame/Public/UI/FortHUDPCTrackerBase.h"
+#if !UE_SERVER
+class IChatViewModel;
 
-//class declare
-class SFortChatWidget : public SCompoundWidget, public FortHUDPCTrackerBase
+class SUTChatWidget : public SCompoundWidget
 {
 public:
-	SLATE_BEGIN_ARGS(SFortChatWidget)
-		: _bKeepVisible(false)
+	SLATE_BEGIN_ARGS(SUTChatWidget)
 	{}
-
-	/** Should the chat widget be kept visible at all times */
-	SLATE_ARGUMENT(bool, bKeepVisible)
 
 	SLATE_END_ARGS()
 
 	/** Needed for every widget */
 	void Construct(const FArguments& InArgs, const FLocalPlayerContext& InCtx);
 
-	/** Gets the visibility of the entry widget */
-	virtual EVisibility GetEntryVisibility() const;
+	/** Focus edit box so user can immediately start typing */
+	void SetFocus();
 
-	/** Sets the visibility of the entry widget and associated logic */
-	virtual void SetEntryVisibility( TAttribute<EVisibility> InVisibility );
-
+private:
 	/** Handle Friends network message sent */
 	void HandleFriendsNetworkChatMessage(const FString& NetworkMessage);
 
 	/** Handle Friends chat list updated */
 	void HandleChatListUpdated();
 
-	/** Focus edit box so user can immediately start typing */
-	void SetFocus();
-
-protected:
-
 	/** Update function. Kind of a hack. Allows us to focus keyboard. */
 	virtual void Tick(const FGeometry& AllottedGeometry, const double InCurrentTime, const float InDeltaTime) override;
 
 	/** The UI sets up the appropriate mouse settings upon focus */
-	virtual FReply OnFocusReceived( const FGeometry& MyGeometry, const FFocusEvent& InFocusEvent ) override;
+	virtual FReply OnFocusReceived(const FGeometry& MyGeometry, const FFocusEvent& InFocusEvent) override;
 
 	/** Callback for when we commit some text */
 	void OnChatTextCommitted();
@@ -50,22 +38,13 @@ protected:
 	/** Visibility of the entry widget previous frame */
 	EVisibility LastVisibility;
 
-	/** Time to show the chat lines as visible after receiving a chat message */
-	double ChatFadeTime;
-
-	/** When we received our last chat message */
-	double LastChatLineTime;
-
 	/** The edit text widget */
-	TSharedPtr< SBorder > ChatEditBox;
+	TSharedPtr<SBorder> ChatEditBox;
 
-	/** Should this chatbox be kept visible */
-	bool bKeepVisible;
-
-	// Holds the chat widget view model
+	/** Holds the chat widget view model */
 	TSharedPtr<IChatViewModel> ViewModel;
+
+	/** Player context */
+	FLocalPlayerContext Ctx;
 };
-
-
-
-
+#endif
