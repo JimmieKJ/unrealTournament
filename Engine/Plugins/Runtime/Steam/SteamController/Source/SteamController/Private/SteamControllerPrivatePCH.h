8561495db0efcb013cd590c6032b2be8523b3555
Dmitry Rekman <Dmitry.Rekman@epicgames.com>
2015-02-04 14:41:34 -0500

    Disable crazy warnings that claim that standard C library is "deprecated".
    (Merging 2432171 from main to UT branch).
    
    [CL 2432179 by Dmitry Rekman in UT-Clean branch]

--- Engine/Plugins/Runtime/Steam/SteamController/Source/SteamController/Private/SteamControllerPrivatePCH.h
+++ Engine/Plugins/Runtime/Steam/SteamController/Source/SteamController/Private/SteamControllerPrivatePCH.h
@@ -6,7 +6,18 @@
 #if WITH_STEAM_CONTROLLER
 
 #include "InputDevice.h"
+
+// Disable crazy warnings that claim that standard C library is "deprecated".
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable:4996)
+#endif
+
 #include <steam/steam_api.h>
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
 
 #endif // WITH_STEAM_CONTROLLER
 
