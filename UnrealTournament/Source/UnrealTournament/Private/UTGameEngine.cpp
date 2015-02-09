81d6ef0612a48d3977047d1efa897f4005a5a08e
Peter Knepley <peter.knepley@epicgames.com>
2015-02-09 10:29:00 -0500

    Increment game network version
    [CL 2438037 by Peter Knepley in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/UTGameEngine.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/UTGameEngine.cpp
@@ -16,7 +16,7 @@ UUTGameEngine::UUTGameEngine(const FObjectInitializer& ObjectInitializer)
 	bAllowClientNetProfile = false;
 	ReadEULACaption = NSLOCTEXT("UTGameEngine", "ReadEULACaption", "READ ME FIRST");
 	ReadEULAText = NSLOCTEXT("UTGameEngine", "ReadEULAText", "EULA TEXT");
-	GameNetworkVersion = 3008024;
+	GameNetworkVersion = 3008025;
 
 	SmoothedDeltaTime = 0.01f;
 
