ea5002a5606b246503fc4c9ba6c38e1ef0e81332
Maciej Mroz <maciej.mroz@epicgames.com>
2015-02-12 17:15:15 -0500

    UE-9653 Some blueprints fail to compile only when playing with -game
    [CL 2443701 by Maciej Mroz in UT-Clean branch]

--- Engine/Source/Runtime/CoreUObject/Private/Blueprint/BlueprintSupport.cpp
+++ Engine/Source/Runtime/CoreUObject/Private/Blueprint/BlueprintSupport.cpp
@@ -781,9 +781,10 @@ void ULinkerLoad::FinalizeBlueprint(UClass* LoadClass)
 		{
 			// because it is tracked by FUnresolvedStructTracker, it must be a struct
 			DEFERRED_DEPENDENCY_CHECK(Cast<UStruct>(Import.XObject) != nullptr);
-			if (Import.SourceLinker)
+			auto SourceLinker = FindExistingLinkerForImport(ImportIndex);
+			if (SourceLinker)
 			{
-				Import.SourceLinker->ResolveDeferredDependencies((UStruct*)Import.XObject);
+				SourceLinker->ResolveDeferredDependencies((UStruct*)Import.XObject);
 			}
 		}
 	}
