26fffd3a1ac43d8a8598d03eb4b61d25ae65c2dd
Steve Polge <steve.polge@epicgames.com>
2015-02-21 15:30:47 -0500

    Capitalization fix.
    [CL 2454906 by Steve Polge in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/UTLocalPlayer.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/UTLocalPlayer.cpp
@@ -1007,7 +1007,7 @@ void UUTLocalPlayer::OnWriteUserFileComplete(bool bWasSuccessful, const FUniqueN
 	{
 #if !UE_SERVER
 		// Should give a warning here if it fails.
-		ShowMessage(NSLOCTEXT("MCPMessages", "ProfileSaveErrorTitle", "An Error has occured"), NSLOCTEXT("MCPMessages", "ProfileSaveErrorText", "UT could not save your profile with the MCP.  Your settings may be lost."), UTDIALOG_BUTTON_OK, NULL);
+		ShowMessage(NSLOCTEXT("MCPMessages", "ProfileSaveErrorTitle", "An error has occured"), NSLOCTEXT("MCPMessages", "ProfileSaveErrorText", "UT could not save your profile with the MCP.  Your settings may be lost."), UTDIALOG_BUTTON_OK, NULL);
 #endif
 	}
 }
