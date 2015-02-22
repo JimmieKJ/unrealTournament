8bf9065ce96a0daf572314280e217d832cdb2b4c
Steve Polge <steve.polge@epicgames.com>
2015-02-21 22:03:38 -0500

    Always add bot to  duel game that starts with less than 2 players.
    [CL 2455193 by Steve Polge in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/UTDuelGame.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/UTDuelGame.cpp
@@ -81,7 +81,7 @@ void AUTDuelGame::InitGame(const FString& MapName, const FString& Options, FStri
 	Super::InitGame(MapName, Options, ErrorMessage);
 
 	GameSession->MaxPlayers = 2;
-	BotFillCount = FMath::Min<int32>(BotFillCount, 2);
+	BotFillCount = 2;
 	bForceRespawn = true;
 	bBalanceTeams = true;
 }
