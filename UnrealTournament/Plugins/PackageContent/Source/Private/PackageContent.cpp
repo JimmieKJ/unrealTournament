bdd0ab448cc501d30aa00ce878d92cfe83f80fae
Peter Knepley <peter.knepley@epicgames.com>
2015-02-25 13:07:33 -0500

    Convert second package content success dialog to a hyperlink in the first dialog
    [CL 2460517 by Peter Knepley in UT-Clean branch]

--- UnrealTournament/Plugins/PackageContent/Source/Private/PackageContent.cpp
+++ UnrealTournament/Plugins/PackageContent/Source/Private/PackageContent.cpp
@@ -9,10 +9,263 @@
 #include "SNotificationList.h"
 #include "EdGraphSchema_K2.h"
 #include "DesktopPlatformModule.h"
+#include "SHyperlink.h"
 
 #define LOCTEXT_NAMESPACE "PackageContent"
 
 DEFINE_LOG_CATEGORY(LogPackageContent);
+
+
+class SPackageCompleteChoiceDialog : public SCompoundWidget
+{
+public:
+	SLATE_BEGIN_ARGS(SPackageCompleteChoiceDialog)	{}
+		SLATE_ATTRIBUTE(TSharedPtr<SWindow>, ParentWindow)
+		SLATE_ATTRIBUTE(FText, Message)
+		SLATE_ATTRIBUTE(float, WrapMessageAt)
+		SLATE_ATTRIBUTE(EAppMsgType::Type, MessageType)
+		SLATE_ATTRIBUTE(FString, FilePath)
+		SLATE_ATTRIBUTE(FText, FilePathLinkText)
+	SLATE_END_ARGS()
+
+	BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
+	void Construct(const FArguments& InArgs)
+	{
+		ParentWindow = InArgs._ParentWindow.Get();
+		ParentWindow->SetWidgetToFocusOnActivate(SharedThis(this));
+		Response = EAppReturnType::Cancel;
+
+		FSlateFontInfo MessageFont(FEditorStyle::GetFontStyle("StandardDialog.LargeFont"));
+		MyMessage = InArgs._Message;
+		FilePath = InArgs._FilePath.Get();
+		FText FilePathLinkText = InArgs._FilePathLinkText.Get();
+
+		TSharedPtr<SUniformGridPanel> ButtonBox;
+
+		this->ChildSlot
+		[
+			SNew(SBorder)
+			.BorderImage(FEditorStyle::GetBrush("ToolPanel.GroupBorder"))
+			[
+				SNew(SVerticalBox)
+				
+				+ SVerticalBox::Slot()
+				.HAlign(HAlign_Fill)
+				.VAlign(VAlign_Fill)
+				.FillHeight(1.0f)
+				.MaxHeight(550)
+				.Padding(12.0f)
+				[
+					SNew(SScrollBox)
+					+ SScrollBox::Slot()
+					[
+						SNew(STextBlock)
+						.Text(MyMessage)
+						.Font(MessageFont)
+						.WrapTextAt(InArgs._WrapMessageAt)
+					]
+				]
+
+				+ SVerticalBox::Slot()
+				.AutoHeight()
+				.Padding(0.0f)
+				[
+					SNew(SHorizontalBox)
+
+					+ SHorizontalBox::Slot()
+					.FillWidth(1.0f)
+					.HAlign(HAlign_Left)
+					.VAlign(VAlign_Bottom)
+					.Padding(12.0f)
+					[
+						SNew(SHyperlink)
+						.OnNavigate(this, &SPackageCompleteChoiceDialog::HandleFilePathHyperlinkNaviate)
+						.Text(FilePathLinkText)
+						.ToolTipText(FilePath)
+					]
+
+					+ SHorizontalBox::Slot()
+					.AutoWidth()
+					.HAlign(HAlign_Right)
+					.VAlign(VAlign_Bottom)
+					.Padding(2.f)
+					[
+						SAssignNew(ButtonBox, SUniformGridPanel)
+						.SlotPadding(FEditorStyle::GetMargin("StandardDialog.SlotPadding"))
+						.MinDesiredSlotWidth(FEditorStyle::GetFloat("StandardDialog.MinDesiredSlotWidth"))
+						.MinDesiredSlotHeight(FEditorStyle::GetFloat("StandardDialog.MinDesiredSlotHeight"))
+					]
+				]
+			]
+		];
+
+		int32 SlotIndex = 0;
+
+#define ADD_SLOT(Button)\
+	ButtonBox->AddSlot(SlotIndex++, 0)\
+	[\
+	SNew(SButton)\
+	.Text(EAppReturnTypeToText(EAppReturnType::Button))\
+	.OnClicked(this, &SPackageCompleteChoiceDialog::HandleButtonClicked, EAppReturnType::Button)\
+	.ContentPadding(FEditorStyle::GetMargin("StandardDialog.ContentPadding"))\
+	.HAlign(HAlign_Center)\
+	];
+
+		switch (InArgs._MessageType.Get())
+		{
+		case EAppMsgType::Ok:
+			ADD_SLOT(Ok)
+				break;
+		case EAppMsgType::YesNo:
+			ADD_SLOT(Yes)
+			ADD_SLOT(No)
+				break;
+		case EAppMsgType::OkCancel:
+			ADD_SLOT(Ok)
+			ADD_SLOT(Cancel)
+				break;
+		case EAppMsgType::YesNoCancel:
+			ADD_SLOT(Yes)
+			ADD_SLOT(No)
+			ADD_SLOT(Cancel)
+				break;
+		case EAppMsgType::CancelRetryContinue:
+			ADD_SLOT(Cancel)
+			ADD_SLOT(Retry)
+			ADD_SLOT(Continue)
+				break;
+		case EAppMsgType::YesNoYesAllNoAll:
+			ADD_SLOT(Yes)
+			ADD_SLOT(No)
+			ADD_SLOT(YesAll)
+			ADD_SLOT(NoAll)
+				break;
+		case EAppMsgType::YesNoYesAllNoAllCancel:
+			ADD_SLOT(Yes)
+			ADD_SLOT(No)
+			ADD_SLOT(YesAll)
+			ADD_SLOT(NoAll)
+			ADD_SLOT(Cancel)
+				break;
+		case EAppMsgType::YesNoYesAll:
+			ADD_SLOT(Yes)
+			ADD_SLOT(No)
+			ADD_SLOT(YesAll)
+				break;
+		default:
+			UE_LOG(LogPackageContent, Fatal, TEXT("Invalid Message Type"));
+		}
+
+#undef ADD_SLOT
+	
+	}
+	END_SLATE_FUNCTION_BUILD_OPTIMIZATION
+
+
+	EAppReturnType::Type GetResponse()
+	{
+		return Response;
+	}
+	
+	virtual	FReply OnKeyDown(const FGeometry& MyGeometry, const FKeyEvent& InKeyEvent)
+	{
+		//see if we pressed the Enter or Spacebar keys
+		if (InKeyEvent.GetKey() == EKeys::Escape)
+		{
+			return HandleButtonClicked(EAppReturnType::Cancel);
+		}
+
+		if (InKeyEvent.GetKey() == EKeys::C && InKeyEvent.IsControlDown())
+		{
+			CopyMessageToClipboard();
+
+			return FReply::Handled();
+		}
+
+		//if it was some other button, ignore it
+		return FReply::Unhandled();
+	}
+
+	/** Override the base method to allow for keyboard focus */
+	virtual bool SupportsKeyboardFocus() const
+	{
+		return true;
+	}
+
+	/** Converts an EAppReturnType into a localized FText */
+	static FText EAppReturnTypeToText(EAppReturnType::Type ReturnType)
+	{
+		switch (ReturnType)
+		{
+		case EAppReturnType::No:
+			return LOCTEXT("EAppReturnTypeNo", "No");
+		case EAppReturnType::Yes:
+			return LOCTEXT("EAppReturnTypeYes", "Yes");
+		case EAppReturnType::YesAll:
+			return LOCTEXT("EAppReturnTypeYesAll", "Yes All");
+		case EAppReturnType::NoAll:
+			return LOCTEXT("EAppReturnTypeNoAll", "No All");
+		case EAppReturnType::Cancel:
+			return LOCTEXT("EAppReturnTypeCancel", "Cancel");
+		case EAppReturnType::Ok:
+			return LOCTEXT("EAppReturnTypeOk", "OK");
+		case EAppReturnType::Retry:
+			return LOCTEXT("EAppReturnTypeRetry", "Retry");
+		case EAppReturnType::Continue:
+			return LOCTEXT("EAppReturnTypeContinue", "Continue");
+		default:
+			return LOCTEXT("MissingType", "MISSING RETURN TYPE");
+		}
+	}
+
+protected:
+
+	/**
+	 * Copies the message text to the clipboard.
+	 */
+	void CopyMessageToClipboard()
+	{
+		FPlatformMisc::ClipboardCopy(*MyMessage.Get().ToString());
+	}
+
+
+private:
+
+	// Handles clicking a message box button.
+	FReply HandleButtonClicked(EAppReturnType::Type InResponse)
+	{
+		Response = InResponse;
+
+		ResultCallback.ExecuteIfBound(ParentWindow.ToSharedRef(), Response);
+
+
+		ParentWindow->RequestDestroyWindow();
+
+		return FReply::Handled();
+	}
+
+	// Handles clicking the 'Copy Message' hyper link.
+	void HandleCopyMessageHyperlinkNavigate()
+	{
+		CopyMessageToClipboard();
+	}
+	
+	void HandleFilePathHyperlinkNaviate()
+	{
+		FPlatformProcess::ExploreFolder(*FilePath);
+	}
+
+public:
+	/** Callback delegate that is triggered, when the dialog is run in non-modal mode */
+	FOnMsgDlgResult ResultCallback;
+
+private:
+
+	EAppReturnType::Type Response;
+	TSharedPtr<SWindow> ParentWindow;
+	TAttribute<FText> MyMessage;
+	FString FilePath;
+};
 
 const FVector2D FPackageContent::DEFAULT_WINDOW_SIZE = FVector2D(600, 400);
 
@@ -131,15 +384,7 @@ public:
 	FPackageContentCompleteTask(const FString& InDLCName)
 		: DLCName(InDLCName)
 	{ }
-
-	void ShowOpenExplorerDialog(const FString& FullPath)
-	{
-		if (EAppReturnType::Yes == FMessageDialog::Open(EAppMsgType::YesNo, LOCTEXT("ShowInExplorer", "Your content is ready to use, would you like to open an explorer window?")))
-		{
-			FPlatformProcess::ExploreFolder(*FullPath);
-		}
-	}
-
+	
 	void DoTask(ENamedThreads::Type CurrentThread, const FGraphEventRef& MyCompletionGraphEvent)
 	{
 		IDesktopPlatform* DesktopPlatform = FDesktopPlatformModule::Get();
@@ -167,7 +412,32 @@ public:
 
 			}
 
-			if (EAppReturnType::Yes == FMessageDialog::Open(EAppMsgType::YesNo, LOCTEXT("PublishContentSuccess", "Content packaged successfully. Would you like to share it now?")))
+			TSharedPtr<SWindow> MsgWindow = NULL;
+			TSharedPtr<SPackageCompleteChoiceDialog> MsgDialog = NULL;
+
+			MsgWindow = SNew(SWindow)
+				.Title(NSLOCTEXT("PublishContentSuccessTitle", "MessageTitle", "Would you like to share?"))
+				.SizingRule(ESizingRule::Autosized)
+				.AutoCenter(EAutoCenter::PreferredWorkArea)
+				.SupportsMinimize(false).SupportsMaximize(false);
+
+			MsgDialog = SNew(SPackageCompleteChoiceDialog)
+				.ParentWindow(MsgWindow)
+				.Message(LOCTEXT("PublishContentSuccess", "Content packaged successfully. Would you like to share it now?"))
+				.WrapMessageAt(512.0f)
+				.FilePath(DestinationPath)
+				.FilePathLinkText(LOCTEXT("PublishContentSuccessOpenWindow", "Open Window To Packaged Content"))
+				.MessageType(EAppMsgType::YesNo);
+
+			MsgDialog->ResultCallback = nullptr;
+
+			MsgWindow->SetContent(MsgDialog.ToSharedRef());
+
+			GEditor->EditorAddModalWindow(MsgWindow.ToSharedRef());
+
+			EAppReturnType::Type Response = MsgDialog->GetResponse();
+
+			if (EAppReturnType::Yes == Response)
 			{
 				FString LauncherCommandLine = TEXT("-assetuploadcategory=ut -assetuploadpath=\"") + PakPath + TEXT("\"");
 
@@ -192,8 +462,6 @@ public:
 					}
 				}
 			}
-
-			ShowOpenExplorerDialog(DestinationPath);
 		}
 	}
 
@@ -509,4 +777,5 @@ void SPackageContentDialog::ClassChosen(UClass* ChosenClass)
 
 	ParentWindow->RequestDestroyWindow();
 }
+
 #undef LOCTEXT_NAMESPACE
