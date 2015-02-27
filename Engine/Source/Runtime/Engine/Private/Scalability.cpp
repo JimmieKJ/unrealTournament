1ee73f685cab6e5a5e8e4d7b88169ed403f033ed
Bob Tellez <Bob.Tellez@epicgames.com>
2015-02-27 12:13:36 -0500

    #UT Scaled down breaking points for scalability to allow high end cards to hit epic settings.
    [CL 2464080 by Bob Tellez in UT-Clean branch]

--- Engine/Source/Runtime/Engine/Private/Scalability.cpp
+++ Engine/Source/Runtime/Engine/Private/Scalability.cpp
@@ -247,12 +247,12 @@ FQualityLevels BenchmarkQualityLevels(uint32 WorkScale)
 	// decide on the actual quality needed
 
 	Results.ResolutionQuality = GetRenderScaleLevelFromQualityLevel(ComputeOptionFromPerfIndex(GPUPerfIndex, 15, 45, 70));
-	Results.ViewDistanceQuality = ComputeOptionFromPerfIndex(MinPerfIndex, 50, 90, 130);
-	Results.AntiAliasingQuality = ComputeOptionFromPerfIndex(GPUPerfIndex, 60, 130, 200);
-	Results.ShadowQuality = ComputeOptionFromPerfIndex(MinPerfIndex, 50, 90, 130);
-	Results.PostProcessQuality = ComputeOptionFromPerfIndex(GPUPerfIndex, 60, 130, 200);
-	Results.TextureQuality = ComputeOptionFromPerfIndex(GPUPerfIndex, 60, 130, 200);
-	Results.EffectsQuality = ComputeOptionFromPerfIndex(MinPerfIndex, 50, 90, 130);
+	Results.ViewDistanceQuality = ComputeOptionFromPerfIndex(MinPerfIndex, 20, 55, 110);
+	Results.AntiAliasingQuality = ComputeOptionFromPerfIndex(GPUPerfIndex, 15, 70, 150);
+	Results.ShadowQuality = ComputeOptionFromPerfIndex(MinPerfIndex, 15, 55, 110);
+	Results.PostProcessQuality = ComputeOptionFromPerfIndex(GPUPerfIndex, 20, 70, 150);
+	Results.TextureQuality = ComputeOptionFromPerfIndex(GPUPerfIndex, 10, 70, 150);
+	Results.EffectsQuality = ComputeOptionFromPerfIndex(MinPerfIndex, 25, 55, 110);
 
 	UE_LOG(LogConsoleResponse, Display, TEXT("CPUPerf: %f"), CPUPerfIndex);
 	UE_LOG(LogConsoleResponse, Display, TEXT("GPUPerf: %f"), GPUPerfIndex);
