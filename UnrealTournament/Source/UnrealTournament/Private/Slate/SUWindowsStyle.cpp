78250466b4bf240616bae890c0763bea85a195be
Sam Zamani <sam.zamani@epicgames.com>
2015-02-27 16:45:31 -0500

    #ut - added updated ut,fortnite,epic icons for social
    #codereview: justin.sargen, chris.wood
    
    [CL 2464529 by Sam Zamani in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/Slate/SUWindowsStyle.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/Slate/SUWindowsStyle.cpp
@@ -1358,9 +1358,10 @@ void SUWindowsStyle::SetFriendsChatStyle(TSharedRef<FSlateStyleSet> StyleRef)
 	Style.Set("OfflineState", new IMAGE_BRUSH("Social-WIP/FriendOfflineStatus", FVector2D(16, 16)));
 	Style.Set("AwayState", new IMAGE_BRUSH("Social-WIP/FriendAwayStatus", FVector2D(16, 16)));
 
-	Style.Set("FriendImageBrush", new IMAGE_BRUSH("Social-WIP/OfflineIcon", FVector2D(55, 50)));
-	Style.Set("LauncherImageBrush", new IMAGE_BRUSH("Social-WIP/LauncherIcon", FVector2D(55, 50)));
-	Style.Set("FortniteImageBrush", new IMAGE_BRUSH("Social-WIP/FortniteIcon", FVector2D(55, 50)));
+	Style.Set("FriendImageBrush", new IMAGE_BRUSH("Social-WIP/OfflineIcon", FVector2D(40, 36)));
+	Style.Set("LauncherImageBrush", new IMAGE_BRUSH("Social-WIP/LauncherIcon", FVector2D(40, 36)));
+	Style.Set("UTImageBrush", new IMAGE_BRUSH("Social-WIP/UTIcon", FVector2D(40, 36)));
+	Style.Set("FortniteImageBrush", new IMAGE_BRUSH("Social-WIP/FortniteIcon", FVector2D(40, 36)));
 
 	Style.Set("GlobalChatIcon", new IMAGE_BRUSH("Social-WIP/Icon-ChatGlobal-XS", Icon16x16));
 	Style.Set("WhisperChatIcon", new IMAGE_BRUSH("Social-WIP/Icon-ChatWhisper-XS", Icon16x16));
@@ -1421,6 +1422,7 @@ void SUWindowsStyle::SetFriendsChatStyle(TSharedRef<FSlateStyleSet> StyleRef)
 		.SetFriendsListItemButtonSimpleStyle(FriendsListItemButtonStyle)
 		.SetFriendImageBrush(*Style.GetBrush("FriendImageBrush"))
 		.SetLauncherImageBrush(*Style.GetBrush("LauncherImageBrush"))
+		.SetUTImageBrush(*Style.GetBrush("UTImageBrush"))
 		.SetFortniteImageBrush(*Style.GetBrush("FortniteImageBrush"))
 		.SetTextStyle(FriendsTextStyle)
 		.SetFontStyle(ExoRegular)
