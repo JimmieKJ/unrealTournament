8047226b87c81ab5e9391b5bfa437cdb8f532eab
Peter Knepley <peter.knepley@epicgames.com>
2015-03-12 16:48:48 -0400

    Merge to releases
    [CL 2477448 by Peter Knepley in UT-Release branch]

--- UnrealTournament/Source/UnrealTournament/Private/UTProj_FlakShell.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/UTProj_FlakShell.cpp
@@ -18,7 +18,7 @@ AUTProj_FlakShell::AUTProj_FlakShell(const class FObjectInitializer& ObjectIniti
 	TossZ = 430.0f;
 
 	DamageParams.BaseDamage = 100.0f;
-	DamageParams.OuterRadius = 410.0f;
+	DamageParams.OuterRadius = 370.0f;
 
 	Momentum = 150000.0f;
 
