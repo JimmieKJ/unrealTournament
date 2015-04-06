dc326d3b73e502913628a8365a661ada5e485a30
Matt Oelfke <Matt.Oelfke@epicgames.com>
2015-04-06 14:38:44 -0400

    Build geometry no longer triggers a navigation rebuild when auto update navigation is not enabled
    [CL 2502894 by Matt Oelfke in UT-Clean branch]

--- Engine/Source/Editor/UnrealEd/Private/EditorBuildUtils.cpp
+++ Engine/Source/Editor/UnrealEd/Private/EditorBuildUtils.cpp
@@ -233,7 +233,10 @@ bool FEditorBuildUtils::EditorBuild( UWorld* InWorld, EBuildOptions::Type Id, co
 
 			GUnrealEd->Exec( InWorld, TEXT("MAP REBUILD") );
 
-			TriggerNavigationBuilder(InWorld, Id);
+			if (GetDefault<ULevelEditorMiscSettings>()->bNavigationAutoUpdate)
+			{
+				TriggerNavigationBuilder(InWorld, Id);
+			}
 
 			// No need to dirty the persient level if we're building BSP for a sub-level.
 			bDirtyPersistentLevel = false;
@@ -251,7 +254,10 @@ bool FEditorBuildUtils::EditorBuild( UWorld* InWorld, EBuildOptions::Type Id, co
 
 				GUnrealEd->Exec( InWorld, TEXT("MAP REBUILD ALLVISIBLE") );
 
-				TriggerNavigationBuilder(InWorld, Id);
+				if (GetDefault<ULevelEditorMiscSettings>()->bNavigationAutoUpdate)
+				{
+					TriggerNavigationBuilder(InWorld, Id);
+				}
 			}
 			break;
 		}
