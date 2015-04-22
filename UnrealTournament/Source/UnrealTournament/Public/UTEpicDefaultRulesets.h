4d37576d1b015ba4914c1cc7511b6dba887b9144
Peter Knepley <peter.knepley@epicgames.com>
2015-04-22 16:01:30 -0400

    Merge rules adjustments and game start bug fix to releases
    [CL 2521618 by Peter Knepley in UT-Release branch]

--- UnrealTournament/Source/UnrealTournament/Public/UTEpicDefaultRulesets.h
+++ UnrealTournament/Source/UnrealTournament/Public/UTEpicDefaultRulesets.h
@@ -46,13 +46,13 @@ public:
 			TEXT("Casual"), 
 			TEXT("Deathmatch"), 
 			TEXT("Standard free-for-all Deathmatch."), 
-			TEXT("Standard free-for-all deathmatch.\n\n<UT.Hub.RulesText_Small>FragLimit : 30</>\n<UT.Hub.RulesText_Small>TimeLimit : 10 minutes</>\n<UT.Hub.RulesText_Small>Mercy Rule : On</>\n\nMaximum of 6 players allowed!"),
+			TEXT("Standard free-for-all deathmatch.\n\n<UT.Hub.RulesText_Small>TimeLimit : 10 minutes</>\nMaximum of 6 players allowed!"),
 			6, 
 			TEXT("DM-Outpost23,DM-Tuba,DM-NickTest1,DM-Chill,DM-ASDF,DM-Focus,DM-Temple,DM-Lea"), 
 			6, 
 			TEXT("Texture2D'/Game/RestrictedAssets/UI/GameModeBadges/GB_DM.GB_DM'"), 
 			TEXT("DM"),
-			TEXT("?GoalScore=30?TimeLimit=10"));
+			TEXT("?TimeLimit=10"));
 
 		if (NewRuleset) Storage.Add(NewRuleset);
 
@@ -61,13 +61,13 @@ public:
 			TEXT("Big"), 
 			TEXT("Big Deathmatch"), 
 			TEXT("Deathmatch with an insane # of players on big maps."), 
-			TEXT("Deathmatch with big player counts on big maps!\n\n<UT.Hub.RulesText_Small>FragLimit : 50</>\n<UT.Hub.RulesText_Small>TimeLimit : 15 minutes</>\n<UT.Hub.RulesText_Small>Mercy Rule : On</>\n\nMaximum of 16 players allowed!"),
+			TEXT("Deathmatch with big player counts on big maps!\n\n<UT.Hub.RulesText_Small>TimeLimit : 20 minutes</>\n\nMaximum of 16 players allowed!"),
 			6, 
-			TEXT("DM-Outpost23,DM-SidCastle,DM-Cannon,DM-Deadfall,DM-Spacer"), 
+			TEXT("DM-Outpost23,DM-DeckTest,DM-SidCastle,DM-Cannon,DM-Deadfall,DM-Spacer"), 
 			16, 
 			TEXT("Texture2D'/Game/RestrictedAssets/UI/GameModeBadges/GB_LargeDM.GB_LargeDM'"), 
 			TEXT("DM"),
-			TEXT("?GoalScore=50?TimeLimit=15"));
+			TEXT("?TimeLimit=10"));
 
 		if (NewRuleset) Storage.Add(NewRuleset);
 
@@ -76,13 +76,13 @@ public:
 			TEXT("TeamPlay"), 
 			TEXT("Team Deathmatch"), 
 			TEXT("Two teams trying to kill each other."), 
-			TEXT("Team-Deathmatch pits two teams against each other!\n\n<UT.Hub.RulesText_Small>FragLimit : 100</>\n<UT.Hub.RulesText_Small>TimeLimit : 15 minutes</>\n<UT.Hub.RulesText_Small>Mercy Rule : On</>\n\nMaximum of 12 players allowed!"),
+			TEXT("Team-Deathmatch pits two teams against each other!\n\n<UT.Hub.RulesText_Small>TimeLimit : 20 minutes</>\n<UT.Hub.RulesText_Small>Mercy Rule : On</>\n\nMaximum of 12 players allowed!"),
 			6, 
-			TEXT("DM-Outpost23,DM-SidCastle,DM-Cannon,DM-Deadfall,DM-Spacer"), 
+			TEXT("DM-Outpost23,DM-DeckTest,DM-SidCastle,DM-Cannon,DM-Deadfall,DM-Spacer"), 
 			12, 
 			TEXT("Texture2D'/Game/RestrictedAssets/UI/GameModeBadges/GB_TDM.GB_TDM'"), 
 			TEXT("TDM"),
-			TEXT("?GoalScore=100?TimeLimit=15"));
+			TEXT("?TimeLimit=20"));
 
 		if (NewRuleset) Storage.Add(NewRuleset);
 
@@ -91,13 +91,13 @@ public:
 			TEXT("Competitive"), 
 			TEXT("Duel"), 
 			TEXT("Two players enter, one player leaves!"), 
-			TEXT("You against one other opponent!\n\n<UT.Hub.RulesText_Small>FragLimit : 20</>\n<UT.Hub.RulesText_Small>TimeLimit : 10 minutes</>\n<UT.Hub.RulesText_Small>Mercy Rule : OFF</>\n\nMaximum of 2 players allowed!"),
+			TEXT("You against one other opponent!\n\n<UT.Hub.RulesText_Small>TimeLimit : 10 minutes</>\n<UT.Hub.RulesText_Small>Mercy Rule : OFF</>\n\nMaximum of 2 players allowed!"),
 			3, 
-			TEXT("DM-Outpost23,DM-Tuba,DM-NickTest1,DM-Chill,DM-ASDF,DM-Focus,DM-Temple,DM-Lea"), 
+			TEXT("DM-Outpost23,DM-Tuba,DM-DeckTest,DM-NickTest1,DM-Chill,DM-ASDF,DM-Focus,DM-Temple,DM-Lea"), 
 			2, 
 			TEXT("Texture2D'/Game/RestrictedAssets/UI/GameModeBadges/GB_Duel.GB_Duel'"), 
 			TEXT("Duel"),
-			TEXT("?GoalScore=20?TimeLimit=10"));
+			TEXT("?TimeLimit=10"));
 
 		if (NewRuleset) Storage.Add(NewRuleset);
 
@@ -106,13 +106,13 @@ public:
 			TEXT("Casual,TeamPlay"), 
 			TEXT("Capture the Flag"), 
 			TEXT("Two teams trying to steal each other flags."), 
-			TEXT("Try to steal the enemy flag and return it home.!\n\n<UT.Hub.RulesText_Small>TimeLimit : 2x 7 minute halfs</>\n<UT.Hub.RulesText_Small>Mercy Rule : On</>\n\nMaximum of 12 players allowed!"),
+			TEXT("Try to steal the enemy flag and return it home.!\n\n<UT.Hub.RulesText_Small>TimeLimit : 2x 10 minute halfs</>\n<UT.Hub.RulesText_Small>Mercy Rule : On</>\n\nMaximum of 12 players allowed!"),
 			6, 
 			TEXT("CTF-Blank,CTF-Outside,CTF-FaceTest,CTF-BigRock,CTF-Dam,CTF-Crashsite"), 
 			12, 
 			TEXT("Texture2D'/Game/RestrictedAssets/UI/GameModeBadges/GB_CTF.GB_CTF'"), 
 			TEXT("CTF"),
-			TEXT("?TimeLimit=14"));
+			TEXT("?TimeLimit=20"));
 
 		if (NewRuleset) Storage.Add(NewRuleset);
 
@@ -122,13 +122,13 @@ public:
 			TEXT("Big"), 
 			TEXT("Big Capture the Flag"), 
 			TEXT("Two teams trying to steal each other flags with insane player counts."), 
-			TEXT("Try to steal the enemy flag and return it home.!\n\n<UT.Hub.RulesText_Small>TimeLimit : 2x 7 minute halfs</>\n<UT.Hub.RulesText_Small>Mercy Rule : On</>\n\nMaximum of 20 players allowed!"),
+			TEXT("Try to steal the enemy flag and return it home.!\n\n<UT.Hub.RulesText_Small>TimeLimit : 2x 10 minute halfs</>\n<UT.Hub.RulesText_Small>Mercy Rule : On</>\n\nMaximum of 20 players allowed!"),
 			6, 
 			TEXT("CTF-Blank,CTF-Outside,CTF-FaceTest,CTF-BigRock,CTF-Dam,CTF-Crashsite"), 
 			20, 
 			TEXT("Texture2D'/Game/RestrictedAssets/UI/GameModeBadges/GB_LargeCTF.GB_LargeCTF'"), 
 			TEXT("CTF"),
-			TEXT("?TimeLimit=14"));
+			TEXT("?TimeLimit=20"));
 
 		if (NewRuleset) Storage.Add(NewRuleset);
 
@@ -137,13 +137,13 @@ public:
 			TEXT("Instagib"), 
 			TEXT("Instagib DM"), 
 			TEXT("One hit one kill deathmatch."), 
-			TEXT("One hit one kill Deathmatch.\n\n<UT.Hub.RulesText_Small>Mutators : Instagib</>\n<UT.Hub.RulesText_Small>FragLimit : 30</>\n<UT.Hub.RulesText_Small>TimeLimit : 10 minutes</>\n<UT.Hub.RulesText_Small>Mercy Rule : On</>\n\nMaximum of 6 players allowed!"),
+			TEXT("One hit one kill Deathmatch.\n\n<UT.Hub.RulesText_Small>Mutators : Instagib</>\n<UT.Hub.RulesText_Small>TimeLimit : 10 minutes</>\n\nMaximum of 6 players allowed!"),
 			6, 
 			TEXT("DM-Outpost23,DM-Tuba,DM-NickTest1,DM-Chill,DM-ASDF,DM-Focus,DM-Temple,DM-Lea"), 
 			6, 
 			TEXT("Texture2D'/Game/RestrictedAssets/UI/GameModeBadges/GB_InstagibDM.GB_InstagibDM'"), 
 			TEXT("DM"),
-			TEXT("?GoalScore=30?TimeLimit=10?Mutator=Instagib"));
+			TEXT("?TimeLimit=10?Mutator=Instagib"));
 
 		if (NewRuleset) Storage.Add(NewRuleset);
 
@@ -152,16 +152,16 @@ public:
 			TEXT("Instagib"), 
 			TEXT("Instagib TDM"), 
 			TEXT("One hit one kill team deathmatch."), 
-			TEXT("One hit one kill Deathmatch.\n\n<UT.Hub.RulesText_Small>Mutators : Instagib</>\n<UT.Hub.RulesText_Small>FragLimit : 30</>\n<UT.Hub.RulesText_Small>TimeLimit : 10 minutes</>\n<UT.Hub.RulesText_Small>Mercy Rule : On</>\n\nMaximum of 6 players allowed!"),
+			TEXT("One hit one kill Deathmatch.\n\n<UT.Hub.RulesText_Small>Mutators : Instagib</>\n<UT.Hub.RulesText_Small>TimeLimit : 20 minutes</>\n<UT.Hub.RulesText_Small>Mercy Rule : On</>\n\nMaximum of 6 players allowed!"),
 			6, 
 			TEXT("DM-Outpost23,DM-SidCastle,DM-Cannon,DM-Deadfall,DM-Spacer"), 
 			12, 
 			TEXT("Texture2D'/Game/RestrictedAssets/UI/GameModeBadges/GB_InstagibDuel.GB_InstagibDuel'"), 
 			TEXT("TDM"),
-			TEXT("?GoalScore=100?TimeLimit=15?Mutator=Instagib"));
+			TEXT("?TimeLimit=20?Mutator=Instagib"));
 
 		if (NewRuleset) Storage.Add(NewRuleset);
-
+/*
 		NewRuleset = UUTEpicDefaultRulesets::AddDefaultRuleset(Owner, 
 			TEXT("iDuel"), 
 			TEXT("Competitive,Instagib"), 
@@ -176,20 +176,20 @@ public:
 			TEXT("?GoalScore=20?TimeLimit=10?Mutator=Instagib"));
 
 		if (NewRuleset) Storage.Add(NewRuleset);
-
+*/
 
 		NewRuleset = UUTEpicDefaultRulesets::AddDefaultRuleset(Owner, 
 			TEXT("iCTF"), 
 			TEXT("Instagib"), 
 			TEXT("Instgib CTF"), 
 			TEXT("One hit one kill CTF."), 
-			TEXT("Try to steal the enemy flag and return it home while avoiding even a single hit!\n\n<UT.Hub.RulesText_Small>Mutators : Instagib</>\n<UT.Hub.RulesText_Small>TimeLimit : 2x 7 minute halfs</>\n<UT.Hub.RulesText_Small>Mercy Rule : On</>\n\nMaximum of 12 players allowed!"),
+			TEXT("Try to steal the enemy flag and return it home while avoiding even a single hit!\n\n<UT.Hub.RulesText_Small>Mutators : Instagib</>\n<UT.Hub.RulesText_Small>TimeLimit : 2x 10 minute halfs</>\n<UT.Hub.RulesText_Small>Mercy Rule : On</>\n\nMaximum of 12 players allowed!"),
 			6, 
 			TEXT("CTF-Blank,CTF-Outside,CTF-FaceTest,CTF-BigRock,CTF-Dam,CTF-Crashsite"), 
 			12, 
 			TEXT("Texture2D'/Game/RestrictedAssets/UI/GameModeBadges/GB_InstagibCTF.GB_InstagibCTF'"), 
 			TEXT("CTF"),
-			TEXT("?TimeLimit=14?Mutator=Instagib"));
+			TEXT("?TimeLimit=20?Mutator=Instagib"));
 
 		if (NewRuleset) Storage.Add(NewRuleset);
 	}
@@ -209,6 +209,7 @@ public:
 			NewReplicatedRuleset->Tooltip = inTooltip;
 			NewReplicatedRuleset->Description = inDescription;
 			NewReplicatedRuleset->MapPlaylistSize = inMapPlaylistSize;
+			NewReplicatedRuleset->MinPlayersToStart = 2;
 			NewReplicatedRuleset->MaxPlayers = inMaxPlayers;
 			NewReplicatedRuleset->DisplayTexture = inDisplayTexture;
 			NewReplicatedRuleset->GameMode = inGameMode;
