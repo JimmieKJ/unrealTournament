e72ee6c3a39ea5b82b4c4dd4031935952922afa4
Steve Polge <steve.polge@epicgames.com>
2015-04-24 16:08:42 -0400

    Added UTSpectatorCamera class.
    [CL 2524883 by Steve Polge in UT-Clean branch]

---
+++ UnrealTournament/Source/UnrealTournament/Public/UTSpectatorCamera.h
@@ -0,0 +1,12 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "UTSpectatorCamera.generated.h"
+
+UCLASS()
+class UNREALTOURNAMENT_API AUTSpectatorCamera : public ACameraActor
+{
+	GENERATED_UCLASS_BODY()
+
+};
