f65babe3e437b6444e444d4dc8cf38f3c6d9ca46
Matt Oelfke <Matt.Oelfke@epicgames.com>
2015-06-05 13:57:15 -0400

    Fixed behindview bug
    [CL 2578661 by Matt Oelfke in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/UTPlayerController.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/UTPlayerController.cpp
@@ -2031,7 +2031,10 @@ void AUTPlayerController::SetViewTarget(class AActor* NewViewTarget, FViewTarget
 			LastSpectatedPlayerState = NULL;
 		}
 
-		BehindView(NewViewTarget != GetSpectatorPawn() && bSpectateBehindView);
+		if (NewViewTarget != GetSpectatorPawn())
+		{
+			BehindView(bSpectateBehindView);
+		}
 
 		// FIXME: HACK: PlayerState->bOnlySpectator check is workaround for bug possessing new Pawn where we are actually in the spectating state for a short time after getting the new pawn as viewtarget
 		//				happens because Pawn is replicated via property replication and ViewTarget is RPC'ed so comes first
