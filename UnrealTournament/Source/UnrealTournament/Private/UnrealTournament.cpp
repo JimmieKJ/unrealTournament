01183c533f88d8202ea8a1dc6393d02ef50cd325
Peter Knepley <peter.knepley@epicgames.com>
2015-06-19 15:15:13 -0400

    PK EpicInternal to editor asset reg scan
    [CL 2594097 by Peter Knepley in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/UnrealTournament.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/UnrealTournament.cpp
@@ -273,6 +273,7 @@ void GetAllBlueprintAssetData(UClass* BaseClass, TArray<FAssetData>& AssetList, 
 	RootPaths.Add(TEXT("/Game/RestrictedAssets/Pickups/"));
 	RootPaths.Add(TEXT("/Game/RestrictedAssets/Weapons/"));
 	RootPaths.Add(TEXT("/Game/RestrictedAssets/Character/"));
+	RootPaths.Add(TEXT("/Game/EpicInternal/PK/"));
 	// Cooked data has the asset data already set up
 	AssetRegistry.ScanPathsSynchronous(RootPaths);
 #endif
