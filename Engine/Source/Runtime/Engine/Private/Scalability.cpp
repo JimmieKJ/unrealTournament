81a4cd9c204a691fb17fe7ed390deaa9f9f9507b
Steve Polge <steve.polge@epicgames.com>
2015-07-15 18:19:09 -0400

    Re-added some graphics scalability code that got lost in the upgrade to UE4.8.  Improves performance of default settings for lower end systems.
    [CL 2622575 by Steve Polge in UT-Clean branch]

--- Engine/Source/Runtime/Engine/Private/Scalability.cpp
+++ Engine/Source/Runtime/Engine/Private/Scalability.cpp
@@ -247,12 +247,12 @@ FQualityLevels BenchmarkQualityLevels(uint32 WorkScale)
 	// decide on the actual quality needed
 
 	Results.ResolutionQuality = GetRenderScaleLevelFromQualityLevel(ComputeOptionFromPerfIndex(GPUPerfIndex, 15, 45, 70));
-	Results.ViewDistanceQuality = ComputeOptionFromPerfIndex(MinPerfIndex, 20, 50, 70);
-	Results.AntiAliasingQuality = ComputeOptionFromPerfIndex(GPUPerfIndex, 15, 50, 70);
-	Results.ShadowQuality = ComputeOptionFromPerfIndex(MinPerfIndex, 15, 50, 70);
-	Results.PostProcessQuality = ComputeOptionFromPerfIndex(GPUPerfIndex, 20, 50, 70);
-	Results.TextureQuality = ComputeOptionFromPerfIndex(GPUPerfIndex, 10, 40, 70);
-	Results.EffectsQuality = ComputeOptionFromPerfIndex(MinPerfIndex, 25, 55, 70);
+	Results.ViewDistanceQuality = ComputeOptionFromPerfIndex(MinPerfIndex, 20, 55, 110);
+	Results.AntiAliasingQuality = ComputeOptionFromPerfIndex(GPUPerfIndex, 15, 70, 150);
+	Results.ShadowQuality = ComputeOptionFromPerfIndex(MinPerfIndex, 15, 55, 110);
+	Results.PostProcessQuality = ComputeOptionFromPerfIndex(GPUPerfIndex, 20, 70, 150);
+	Results.TextureQuality = ComputeOptionFromPerfIndex(GPUPerfIndex, 10, 70, 150);
+	Results.EffectsQuality = ComputeOptionFromPerfIndex(MinPerfIndex, 25, 55, 110);
 
 	return Results;
 }
