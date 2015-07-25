28fc92a7a9b6a51d1bb333340751c8843418e091
buildmachine <builder-notify@epicgames.com>
2015-07-24 22:51:22 -0400

    UnrealTournamentBuild Version Files from changelist 2632905
    [CL 2633261 by buildmachine in UT-Release branch]

--- Engine/Source/Runtime/Core/Private/UObject/ObjectVersion.cpp
+++ Engine/Source/Runtime/Core/Private/UObject/ObjectVersion.cpp
@@ -8,7 +8,7 @@
 
 // Defined separately so the build script can get to it easily (DO NOT CHANGE THIS MANUALLY)
 // caution when merging, we want the UE3 version in ENGINE_VERSION_UE3
-#define	ENGINE_VERSION	2631377
+#define	ENGINE_VERSION	2632905
 
 /** Version numbers for networking */
 int32   GEngineNetVersion			= ENGINE_VERSION;
