97087923a647204b5cd304ee83bd547ba4920389
Peter Knepley <peter.knepley@epicgames.com>
2015-07-29 13:03:12 -0400

    Fix UT analytics on tokens
    [CL 2637247 by Peter Knepley in UT-Clean branch]

---
+++ UnrealTournament/Source/UnrealTournament/Private/UTAnalyticsBlueprintLibrary.cpp
@@ -0,0 +1,424 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealTournament.h"
+
+#include "Runtime/Analytics/Analytics/Public/Analytics.h"
+#include "Runtime/Analytics/Analytics/Public/Interfaces/IAnalyticsProvider.h"
+#include "UTAnalytics.h"
+#include "UTAnalyticsBlueprintLibrary.h"
+
+DEFINE_LOG_CATEGORY_STATIC(LogUTAnalyticsBPLib, Display, All);
+
+/**
+* Converts the UObject accessible array into the native only analytics array type
+*/
+static inline TArray<FAnalyticsEventAttribute> ConvertAttrs(const TArray<FUTAnalyticsEventAttr>& Attributes)
+{
+	TArray<FAnalyticsEventAttribute> Converted;
+	Converted.AddZeroed(Attributes.Num());
+	for (int32 Index = 0; Index < Attributes.Num(); Index++)
+	{
+		Converted[Index].AttrName = Attributes[Index].Name;
+		Converted[Index].AttrValue = Attributes[Index].Value;
+	}
+	return Converted;
+}
+
+
+UUTAnalyticsBlueprintLibrary::UUTAnalyticsBlueprintLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+}
+
+bool UUTAnalyticsBlueprintLibrary::StartSession()
+{
+	TSharedPtr<IAnalyticsProvider> Provider = FUTAnalytics::GetProviderPtr();
+	if (Provider.IsValid())
+	{
+		return Provider->StartSession();
+	}
+	else
+	{
+		UE_LOG(LogUTAnalyticsBPLib, Warning, TEXT("StartSession: Failed to get the default analytics provider. Double check your [Analytics] configuration in your INI"));
+	}
+	return false;
+}
+
+bool UUTAnalyticsBlueprintLibrary::StartSessionWithAttributes(const TArray<FUTAnalyticsEventAttr>& Attributes)
+{
+	TSharedPtr<IAnalyticsProvider> Provider = FUTAnalytics::GetProviderPtr();
+	if (Provider.IsValid())
+	{
+		return Provider->StartSession(ConvertAttrs(Attributes));
+	}
+	else
+	{
+		UE_LOG(LogUTAnalyticsBPLib, Warning, TEXT("StartSessionWithAttributes: Failed to get the default analytics provider. Double check your [Analytics] configuration in your INI"));
+	}
+	return false;
+}
+
+void UUTAnalyticsBlueprintLibrary::EndSession()
+{
+	TSharedPtr<IAnalyticsProvider> Provider = FUTAnalytics::GetProviderPtr();
+	if (Provider.IsValid())
+	{
+		Provider->EndSession();
+	}
+	else
+	{
+		UE_LOG(LogUTAnalyticsBPLib, Warning, TEXT("EndSession: Failed to get the default analytics provider. Double check your [Analytics] configuration in your INI"));
+	}
+}
+
+void UUTAnalyticsBlueprintLibrary::FlushEvents()
+{
+	TSharedPtr<IAnalyticsProvider> Provider = FUTAnalytics::GetProviderPtr();
+	if (Provider.IsValid())
+	{
+		Provider->FlushEvents();
+	}
+	else
+	{
+		UE_LOG(LogUTAnalyticsBPLib, Warning, TEXT("FlushEvents: Failed to get the default analytics provider. Double check your [Analytics] configuration in your INI"));
+	}
+}
+
+void UUTAnalyticsBlueprintLibrary::RecordEvent(const FString& EventName)
+{
+	TSharedPtr<IAnalyticsProvider> Provider = FUTAnalytics::GetProviderPtr();
+	if (Provider.IsValid())
+	{
+		Provider->RecordEvent(EventName);
+	}
+	else
+	{
+		static bool bHasLogged = false;
+		if (!bHasLogged)
+		{
+			UE_LOG(LogUTAnalyticsBPLib, Warning, TEXT("RecordEvent: Failed to get the default analytics provider. Double check your [Analytics] configuration in your INI"));
+		}
+	}
+}
+
+void UUTAnalyticsBlueprintLibrary::RecordEventWithAttribute(const FString& EventName, const FString& AttributeName, const FString& AttributeValue)
+{
+	TSharedPtr<IAnalyticsProvider> Provider = FUTAnalytics::GetProviderPtr();
+	if (Provider.IsValid())
+	{
+		FAnalyticsEventAttribute Attribute(AttributeName, AttributeValue);
+		Provider->RecordEvent(EventName, Attribute);
+	}
+	else
+	{
+		static bool bHasLogged = false;
+		if (!bHasLogged)
+		{
+			UE_LOG(LogUTAnalyticsBPLib, Warning, TEXT("RecordEventWithAttribute: Failed to get the default analytics provider. Double check your [Analytics] configuration in your INI"));
+		}
+	}
+}
+
+void UUTAnalyticsBlueprintLibrary::RecordEventWithAttributes(const FString& EventName, const TArray<FUTAnalyticsEventAttr>& Attributes)
+{
+	TSharedPtr<IAnalyticsProvider> Provider = FUTAnalytics::GetProviderPtr();
+	if (Provider.IsValid())
+	{
+		Provider->RecordEvent(EventName, ConvertAttrs(Attributes));
+	}
+	else
+	{
+		static bool bHasLogged = false;
+		if (!bHasLogged)
+		{
+			UE_LOG(LogUTAnalyticsBPLib, Warning, TEXT("RecordEventWithAttributes: Failed to get the default analytics provider. Double check your [Analytics] configuration in your INI"));
+		}
+	}
+}
+
+void UUTAnalyticsBlueprintLibrary::RecordItemPurchase(const FString& ItemId, const FString& Currency, int32 PerItemCost, int32 ItemQuantity)
+{
+	TSharedPtr<IAnalyticsProvider> Provider = FUTAnalytics::GetProviderPtr();
+	if (Provider.IsValid())
+	{
+		Provider->RecordItemPurchase(ItemId, Currency, PerItemCost, ItemQuantity);
+	}
+	else
+	{
+		static bool bHasLogged = false;
+		if (!bHasLogged)
+		{
+			UE_LOG(LogUTAnalyticsBPLib, Warning, TEXT("RecordItemPurchase: Failed to get the default analytics provider. Double check your [Analytics] configuration in your INI"));
+		}
+	}
+}
+
+void UUTAnalyticsBlueprintLibrary::RecordSimpleItemPurchase(const FString& ItemId, int32 ItemQuantity)
+{
+	TSharedPtr<IAnalyticsProvider> Provider = FUTAnalytics::GetProviderPtr();
+	if (Provider.IsValid())
+	{
+		Provider->RecordItemPurchase(ItemId, ItemQuantity);
+	}
+	else
+	{
+		static bool bHasLogged = false;
+		if (!bHasLogged)
+		{
+			UE_LOG(LogUTAnalyticsBPLib, Warning, TEXT("RecordSimpleItemPurchase: Failed to get the default analytics provider. Double check your [Analytics] configuration in your INI"));
+		}
+	}
+}
+
+void UUTAnalyticsBlueprintLibrary::RecordSimpleItemPurchaseWithAttributes(const FString& ItemId, int32 ItemQuantity, const TArray<FUTAnalyticsEventAttr>& Attributes)
+{
+	TSharedPtr<IAnalyticsProvider> Provider = FUTAnalytics::GetProviderPtr();
+	if (Provider.IsValid())
+	{
+		Provider->RecordItemPurchase(ItemId, ItemQuantity, ConvertAttrs(Attributes));
+	}
+	else
+	{
+		static bool bHasLogged = false;
+		if (!bHasLogged)
+		{
+			UE_LOG(LogUTAnalyticsBPLib, Warning, TEXT("RecordSimpleItemPurchaseWithAttributes: Failed to get the default analytics provider. Double check your [Analytics] configuration in your INI"));
+		}
+	}
+}
+
+void UUTAnalyticsBlueprintLibrary::RecordSimpleCurrencyPurchase(const FString& GameCurrencyType, int32 GameCurrencyAmount)
+{
+	TSharedPtr<IAnalyticsProvider> Provider = FUTAnalytics::GetProviderPtr();
+	if (Provider.IsValid())
+	{
+		Provider->RecordCurrencyPurchase(GameCurrencyType, GameCurrencyAmount);
+	}
+	else
+	{
+		UE_LOG(LogUTAnalyticsBPLib, Warning, TEXT("RecordSimpleCurrencyPurchase: Failed to get the default analytics provider. Double check your [Analytics] configuration in your INI"));
+	}
+}
+
+void UUTAnalyticsBlueprintLibrary::RecordSimpleCurrencyPurchaseWithAttributes(const FString& GameCurrencyType, int32 GameCurrencyAmount, const TArray<FUTAnalyticsEventAttr>& Attributes)
+{
+	TSharedPtr<IAnalyticsProvider> Provider = FUTAnalytics::GetProviderPtr();
+	if (Provider.IsValid())
+	{
+		Provider->RecordCurrencyPurchase(GameCurrencyType, GameCurrencyAmount, ConvertAttrs(Attributes));
+	}
+	else
+	{
+		UE_LOG(LogUTAnalyticsBPLib, Warning, TEXT("RecordSimpleCurrencyPurchaseWithAttributes: Failed to get the default analytics provider. Double check your [Analytics] configuration in your INI"));
+	}
+}
+
+void UUTAnalyticsBlueprintLibrary::RecordCurrencyPurchase(const FString& GameCurrencyType, int32 GameCurrencyAmount, const FString& RealCurrencyType, float RealMoneyCost, const FString& PaymentProvider)
+{
+	TSharedPtr<IAnalyticsProvider> Provider = FUTAnalytics::GetProviderPtr();
+	if (Provider.IsValid())
+	{
+		Provider->RecordCurrencyPurchase(GameCurrencyType, GameCurrencyAmount, RealCurrencyType, RealMoneyCost, PaymentProvider);
+	}
+	else
+	{
+		UE_LOG(LogUTAnalyticsBPLib, Warning, TEXT("RecordCurrencyPurchase: Failed to get the default analytics provider. Double check your [Analytics] configuration in your INI"));
+	}
+}
+
+void UUTAnalyticsBlueprintLibrary::RecordCurrencyGiven(const FString& GameCurrencyType, int32 GameCurrencyAmount)
+{
+	TSharedPtr<IAnalyticsProvider> Provider = FUTAnalytics::GetProviderPtr();
+	if (Provider.IsValid())
+	{
+		Provider->RecordCurrencyGiven(GameCurrencyType, GameCurrencyAmount);
+	}
+	else
+	{
+		UE_LOG(LogUTAnalyticsBPLib, Warning, TEXT("RecordCurrencyGiven: Failed to get the default analytics provider. Double check your [Analytics] configuration in your INI"));
+	}
+}
+
+void UUTAnalyticsBlueprintLibrary::RecordCurrencyGivenWithAttributes(const FString& GameCurrencyType, int32 GameCurrencyAmount, const TArray<FUTAnalyticsEventAttr>& Attributes)
+{
+	TSharedPtr<IAnalyticsProvider> Provider = FUTAnalytics::GetProviderPtr();
+	if (Provider.IsValid())
+	{
+		Provider->RecordCurrencyGiven(GameCurrencyType, GameCurrencyAmount, ConvertAttrs(Attributes));
+	}
+	else
+	{
+		UE_LOG(LogUTAnalyticsBPLib, Warning, TEXT("RecordCurrencyGivenWithAttributes: Failed to get the default analytics provider. Double check your [Analytics] configuration in your INI"));
+	}
+}
+
+FString UUTAnalyticsBlueprintLibrary::GetSessionId()
+{
+	TSharedPtr<IAnalyticsProvider> Provider = FUTAnalytics::GetProviderPtr();
+	if (Provider.IsValid())
+	{
+		return Provider->GetSessionID();
+	}
+	return FString();
+}
+
+void UUTAnalyticsBlueprintLibrary::SetSessionId(const FString& SessionId)
+{
+	TSharedPtr<IAnalyticsProvider> Provider = FUTAnalytics::GetProviderPtr();
+	if (Provider.IsValid())
+	{
+		Provider->SetSessionID(SessionId);
+	}
+	else
+	{
+		UE_LOG(LogUTAnalyticsBPLib, Warning, TEXT("SetSessionId: Failed to get the default analytics provider. Double check your [Analytics] configuration in your INI"));
+	}
+}
+
+FString UUTAnalyticsBlueprintLibrary::GetUserId()
+{
+	TSharedPtr<IAnalyticsProvider> Provider = FUTAnalytics::GetProviderPtr();
+	if (Provider.IsValid())
+	{
+		return Provider->GetUserID();
+	}
+	return FString();
+}
+
+void UUTAnalyticsBlueprintLibrary::SetUserId(const FString& UserId)
+{
+	TSharedPtr<IAnalyticsProvider> Provider = FUTAnalytics::GetProviderPtr();
+	if (Provider.IsValid())
+	{
+		Provider->SetUserID(UserId);
+	}
+	else
+	{
+		UE_LOG(LogUTAnalyticsBPLib, Warning, TEXT("SetUserId: Failed to get the default analytics provider. Double check your [Analytics] configuration in your INI"));
+	}
+}
+
+FUTAnalyticsEventAttr UUTAnalyticsBlueprintLibrary::MakeEventAttribute(const FString& AttributeName, const FString& AttributeValue)
+{
+	FUTAnalyticsEventAttr EventAttr;
+	EventAttr.Name = AttributeName;
+	EventAttr.Value = AttributeValue;
+	return EventAttr;
+}
+
+void UUTAnalyticsBlueprintLibrary::SetAge(int32 Age)
+{
+	TSharedPtr<IAnalyticsProvider> Provider = FUTAnalytics::GetProviderPtr();
+	if (Provider.IsValid())
+	{
+		Provider->SetAge(Age);
+	}
+	else
+	{
+		UE_LOG(LogUTAnalyticsBPLib, Warning, TEXT("SetAge: Failed to get the default analytics provider. Double check your [Analytics] configuration in your INI"));
+	}
+}
+
+void UUTAnalyticsBlueprintLibrary::SetLocation(const FString& Location)
+{
+	TSharedPtr<IAnalyticsProvider> Provider = FUTAnalytics::GetProviderPtr();
+	if (Provider.IsValid())
+	{
+		Provider->SetLocation(Location);
+	}
+	else
+	{
+		UE_LOG(LogUTAnalyticsBPLib, Warning, TEXT("SetLocation: Failed to get the default analytics provider. Double check your [Analytics] configuration in your INI"));
+	}
+}
+
+void UUTAnalyticsBlueprintLibrary::SetGender(const FString& Gender)
+{
+	TSharedPtr<IAnalyticsProvider> Provider = FUTAnalytics::GetProviderPtr();
+	if (Provider.IsValid())
+	{
+		Provider->SetGender(Gender);
+	}
+	else
+	{
+		UE_LOG(LogUTAnalyticsBPLib, Warning, TEXT("SetGender: Failed to get the default analytics provider. Double check your [Analytics] configuration in your INI"));
+	}
+}
+
+void UUTAnalyticsBlueprintLibrary::SetBuildInfo(const FString& BuildInfo)
+{
+	TSharedPtr<IAnalyticsProvider> Provider = FUTAnalytics::GetProviderPtr();
+	if (Provider.IsValid())
+	{
+		Provider->SetBuildInfo(BuildInfo);
+	}
+	else
+	{
+		UE_LOG(LogUTAnalyticsBPLib, Warning, TEXT("SetBuildInfo: Failed to get the default analytics provider. Double check your [Analytics] configuration in your INI"));
+	}
+}
+
+void UUTAnalyticsBlueprintLibrary::RecordErrorWithAttributes(const FString& Error, const TArray<FUTAnalyticsEventAttr>& Attributes)
+{
+	TSharedPtr<IAnalyticsProvider> Provider = FUTAnalytics::GetProviderPtr();
+	if (Provider.IsValid())
+	{
+		Provider->RecordError(Error, ConvertAttrs(Attributes));
+	}
+	else
+	{
+		UE_LOG(LogUTAnalyticsBPLib, Warning, TEXT("RecordErrorWithAttributes: Failed to get the default analytics provider. Double check your [Analytics] configuration in your INI"));
+	}
+}
+
+void UUTAnalyticsBlueprintLibrary::RecordError(const FString& Error)
+{
+	TSharedPtr<IAnalyticsProvider> Provider = FUTAnalytics::GetProviderPtr();
+	if (Provider.IsValid())
+	{
+		Provider->RecordError(Error);
+	}
+	else
+	{
+		UE_LOG(LogUTAnalyticsBPLib, Warning, TEXT("RecordError: Failed to get the default analytics provider. Double check your [Analytics] configuration in your INI"));
+	}
+}
+
+void UUTAnalyticsBlueprintLibrary::RecordProgressWithFullHierarchyAndAttributes(const FString& ProgressType, const TArray<FString>& ProgressNames, const TArray<FUTAnalyticsEventAttr>& Attributes)
+{
+	TSharedPtr<IAnalyticsProvider> Provider = FUTAnalytics::GetProviderPtr();
+	if (Provider.IsValid())
+	{
+		Provider->RecordProgress(ProgressType, ProgressNames, ConvertAttrs(Attributes));
+	}
+	else
+	{
+		UE_LOG(LogUTAnalyticsBPLib, Warning, TEXT("RecordProgressWithFullHierarchyAndAttributes: Failed to get the default analytics provider. Double check your [Analytics] configuration in your INI"));
+	}
+}
+
+void UUTAnalyticsBlueprintLibrary::RecordProgressWithAttributes(const FString& ProgressType, const FString& ProgressName, const TArray<FUTAnalyticsEventAttr>& Attributes)
+{
+	TSharedPtr<IAnalyticsProvider> Provider = FUTAnalytics::GetProviderPtr();
+	if (Provider.IsValid())
+	{
+		Provider->RecordProgress(ProgressType, ProgressName, ConvertAttrs(Attributes));
+	}
+	else
+	{
+		UE_LOG(LogUTAnalyticsBPLib, Warning, TEXT("RecordProgressWithAttributes: Failed to get the default analytics provider. Double check your [Analytics] configuration in your INI"));
+	}
+}
+
+void UUTAnalyticsBlueprintLibrary::RecordProgress(const FString& ProgressType, const FString& ProgressName)
+{
+	TSharedPtr<IAnalyticsProvider> Provider = FUTAnalytics::GetProviderPtr();
+	if (Provider.IsValid())
+	{
+		Provider->RecordProgress(ProgressType, ProgressName);
+	}
+	else
+	{
+		UE_LOG(LogUTAnalyticsBPLib, Warning, TEXT("RecordProgress: Failed to get the default analytics provider. Double check your [Analytics] configuration in your INI"));
+	}
+}
---
+++ UnrealTournament/Source/UnrealTournament/Public/UTAnalyticsBlueprintLibrary.h
@@ -0,0 +1,140 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "UTAnalyticsBlueprintLibrary.generated.h"
+
+/** Blueprint accessible version of the analytics event struct */
+USTRUCT(BlueprintType)
+struct FUTAnalyticsEventAttr
+{
+	GENERATED_USTRUCT_BODY();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="UTAnalytics")
+	FString Name;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="UTAnalytics")
+	FString Value;
+};
+
+UCLASS()
+class UUTAnalyticsBlueprintLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_UCLASS_BODY()
+
+	/** Starts an analytics session without any custom attributes specified */
+	UFUNCTION(BlueprintCallable, Category="UTAnalytics")
+	static bool StartSession();
+
+	/** Starts an analytics session with custom attributes specified */
+	UFUNCTION(BlueprintCallable, Category="UTAnalytics")
+	static bool StartSessionWithAttributes(const TArray<FUTAnalyticsEventAttr>& Attributes);
+
+	/** Ends an analytics session */
+	UFUNCTION(BlueprintCallable, Category="UTAnalytics")
+	static void EndSession();
+
+	/** Requests that any cached events be sent immediately */
+	UFUNCTION(BlueprintCallable, Category="UTAnalytics")
+	static void FlushEvents();
+
+	/** Records an event has happened by name without any attributes (an event counter) */
+	UFUNCTION(BlueprintCallable, Category="UTAnalytics")
+	static void RecordEvent(const FString& EventName);
+
+	/** Records an event has happened by name with a single attribute */
+	UFUNCTION(BlueprintCallable, Category="UTAnalytics")
+	static void RecordEventWithAttribute(const FString& EventName, const FString& AttributeName, const FString& AttributeValue);
+
+	/** Records an event has happened by name with a single attribute */
+	UFUNCTION(BlueprintCallable, Category="UTAnalytics")
+	static void RecordEventWithAttributes(const FString& EventName, const TArray<FUTAnalyticsEventAttr>& Attributes);
+
+	/** Records an in-game item was purchased using the specified in-game currency */
+	UFUNCTION(BlueprintCallable, Category="UTAnalytics")
+	static void RecordItemPurchase(const FString& ItemId, const FString& Currency, int32 PerItemCost, int32 ItemQuantity);
+
+	/** Records an in-game item was purchased */
+	UFUNCTION(BlueprintCallable, Category="UTAnalytics")
+	static void RecordSimpleItemPurchase(const FString& ItemId, int32 ItemQuantity);
+
+	/** Records an in-game item was purchased with attributes */
+	UFUNCTION(BlueprintCallable, Category="UTAnalytics")
+	static void RecordSimpleItemPurchaseWithAttributes(const FString& ItemId, int32 ItemQuantity, const TArray<FUTAnalyticsEventAttr>& Attributes);
+
+	/** Records an in-game currency was purchased using real-world money */
+	UFUNCTION(BlueprintCallable, Category="UTAnalytics")
+	static void RecordSimpleCurrencyPurchase(const FString& GameCurrencyType, int32 GameCurrencyAmount);
+
+	/** Records an in-game currency was purchased using real-world money */
+	UFUNCTION(BlueprintCallable, Category="UTAnalytics")
+	static void RecordSimpleCurrencyPurchaseWithAttributes(const FString& GameCurrencyType, int32 GameCurrencyAmount, const TArray<FUTAnalyticsEventAttr>& Attributes);
+
+	/** Records an in-game currency was purchased using real-world money */
+	UFUNCTION(BlueprintCallable, Category="UTAnalytics")
+	static void RecordCurrencyPurchase(const FString& GameCurrencyType, int32 GameCurrencyAmount, const FString& RealCurrencyType, float RealMoneyCost, const FString& PaymentProvider);
+
+	/** Records an in-game currency was granted by the game with no real-world money being involved */
+	UFUNCTION(BlueprintCallable, Category="UTAnalytics")
+	static void RecordCurrencyGiven(const FString& GameCurrencyType, int32 GameCurrencyAmount);
+
+	/** Records an in-game currency was granted by the game with no real-world money being involved */
+	UFUNCTION(BlueprintCallable, Category="UTAnalytics")
+	static void RecordCurrencyGivenWithAttributes(const FString& GameCurrencyType, int32 GameCurrencyAmount, const TArray<FUTAnalyticsEventAttr>& Attributes);
+
+	/** Builds a struct from the attribute name and value */
+	UFUNCTION(BlueprintPure, Category="UTAnalytics")
+	static FUTAnalyticsEventAttr MakeEventAttribute(const FString& AttributeName, const FString& AttributeValue);
+
+	/** Gets the current session id from the analytics provider */
+	UFUNCTION(BlueprintCallable, Category="UTAnalytics")
+	static FString GetSessionId();
+
+	/** Sets the session id (if supported) on the analytics provider */
+	UFUNCTION(BlueprintCallable, Category="UTAnalytics")
+	static void SetSessionId(const FString& SessionId);
+
+	/** Gets the current user id from the analytics provider */
+	UFUNCTION(BlueprintCallable, Category="UTAnalytics")
+	static FString GetUserId();
+
+	/** Sets the user id (if supported) on the analytics provider */
+	UFUNCTION(BlueprintCallable, Category="UTAnalytics")
+	static void SetUserId(const FString& UserId);
+
+	/** Sets the user's age (if supported) on the analytics provider */
+	UFUNCTION(BlueprintCallable, Category="UTAnalytics")
+	static void SetAge(int32 Age);
+
+	/** Sets the user's location (if supported) on the analytics provider */
+	UFUNCTION(BlueprintCallable, Category="UTAnalytics")
+	static void SetLocation(const FString& Location);
+
+	/** Sets the user's gender (if supported) on the analytics provider */
+	UFUNCTION(BlueprintCallable, Category="UTAnalytics")
+	static void SetGender(const FString& Gender);
+
+	/** Sets the game's build info (if supported) on the analytics provider */
+	UFUNCTION(BlueprintCallable, Category="UTAnalytics")
+	static void SetBuildInfo(const FString& BuildInfo);
+
+	/** Records an error event has happened with attributes */
+	UFUNCTION(BlueprintCallable, Category="UTAnalytics")
+	static void RecordErrorWithAttributes(const FString& Error, const TArray<FUTAnalyticsEventAttr>& Attributes);
+
+	/** Records an error event has happened */
+	UFUNCTION(BlueprintCallable, Category="UTAnalytics")
+	static void RecordError(const FString& Error);
+
+	/** Records a user progress event has happened with a full list of progress hierarchy names and with attributes */
+	UFUNCTION(BlueprintCallable, Category="UTAnalytics")
+	static void RecordProgressWithFullHierarchyAndAttributes(const FString& ProgressType, const TArray<FString>& ProgressNames, const TArray<FUTAnalyticsEventAttr>& Attributes);
+
+	/** Records a user progress event has happened with attributes */
+	UFUNCTION(BlueprintCallable, Category="UTAnalytics")
+	static void RecordProgressWithAttributes(const FString& ProgressType, const FString& ProgressName, const TArray<FUTAnalyticsEventAttr>& Attributes);
+
+	/** Records a user progress event has happened */
+	UFUNCTION(BlueprintCallable, Category="UTAnalytics")
+	static void RecordProgress(const FString& ProgressType, const FString& ProgressName);
+};
