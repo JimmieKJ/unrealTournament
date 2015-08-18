4de307ea253603fc24edeadf700112d256047328
Peter Knepley <peter.knepley@epicgames.com>
2015-08-18 13:47:56 -0400

    Try to fix non-editor build. Next step is to move this commandlet into the editor folder.
    [CL 2659612 by Peter Knepley in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/UTResetPostProcessVolumesCommandlet.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/UTResetPostProcessVolumesCommandlet.cpp
@@ -1,11 +1,11 @@
 // Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
 
 #include "UnrealTournament.h"
+
+#if UE_EDITOR
 #include "EngineUtils.h"
 #include "ISourceControlModule.h"
 #include "UTResetPostProcessVolumesCommandlet.h"
-
-#if UE_EDITOR
 #include "UnrealEd.h"
 #endif
 
