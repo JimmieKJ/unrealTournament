73b7a31326f30049ee380e3251290157654fe209
Peter Knepley <peter.knepley@epicgames.com>
2015-08-25 18:26:02 -0400

    StringAssetReferences and StringClassReferences in native CDOs now cause the linked object to be cooked
    Merging 2666887
    
    //depot/UE4-UT/...
    
    to //depot/UE4-UT/...
    
    [CL 2668601 by Peter Knepley in UT-Release branch]

--- UnrealTournament/Source/UnrealTournamentEditor/Public/UTEditorEngine.h
+++ UnrealTournament/Source/UnrealTournamentEditor/Public/UTEditorEngine.h
@@ -15,5 +15,31 @@ class UUTEditorEngine : public UEditorEngine
 	: Super(ObjectInitializer)
 	{}
 
+	// hacky location for this because InitEditor() isn't virtual
+	virtual void InitializeObjectReferences() override
+	{
+		Super::InitializeObjectReferences();
+
+		// manually add FStringClassReferences and FStringAssetReferences in native UClasses to the cook list
+		// this will not happen by default because that is done via Serialize() which does not happen for native CDOs
+		UScriptStruct* FStringClassReferenceStruct = FindObject<UScriptStruct>(NULL, TEXT("/Script/CoreUObject.StringClassReference"), true);
+		UScriptStruct* FStringAssetReferenceStruct = FindObject<UScriptStruct>(NULL, TEXT("/Script/CoreUObject.StringAssetReference"), true);
+		for (TObjectIterator<UClass> It; It; ++It)
+		{
+			if (It->HasAnyClassFlags(CLASS_Native))
+			{
+				for (TFieldIterator<UStructProperty> PropIt(*It, EFieldIteratorFlags::ExcludeSuper); PropIt; ++PropIt)
+				{
+					if (!PropIt->HasAnyPropertyFlags(CPF_EditorOnly) && (PropIt->Struct == FStringClassReferenceStruct || PropIt->Struct == FStringAssetReferenceStruct))
+					{
+						FString ContentPath;
+						PropIt->ExportTextItem(ContentPath, PropIt->ContainerPtrToValuePtr<void>(It->GetDefaultObject()), NULL, NULL, 0, NULL);
+						FCoreUObjectDelegates::StringAssetReferenceLoaded.Execute(ContentPath);
+					}
+				}
+			}
+		}
+	}
+
 	UT_LOADMAP_DEFINITION()
 };
