bdd10b1107f34a53c8ec895e2c2eb6d145cf59bd
Joe Wilcox <Joe.Wilcox@epicgames.com>
2015-09-02 15:30:46 -0400

    UT-1427 - Added the Game type display name to custom matches.
    [CL 2677484 by Joe Wilcox in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/UTLobbyMatchInfo.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/UTLobbyMatchInfo.cpp
@@ -741,6 +741,8 @@ void AUTLobbyMatchInfo::ServerCreateCustomRule_Implementation(const FString& Gam
 		AUTGameMode* CustomGameModeDefaultObject = NewReplicatedRuleset->GetDefaultGameModeObject();
 		if (CustomGameModeDefaultObject)
 		{
+			NewReplicatedRuleset->Title = FString::Printf(TEXT("Custom Rule - %s"), *CustomGameModeDefaultObject->DisplayName.ToString());
+
 			TArray< TSharedPtr<TAttributePropertyBase> > AllowedProps;
 			CustomGameModeDefaultObject->CreateGameURLOptions(AllowedProps);
 
