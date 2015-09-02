f0a910f16a3957e435c95e181c850ff98d65c1e2
Steve Polge <steve.polge@epicgames.com>
2015-09-02 12:42:46 -0400

    Filling out offline challenge enemy teams WIP.
    [CL 2677204 by Steve Polge in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/UTChallengeManager.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/UTChallengeManager.cpp
@@ -4,70 +4,65 @@
 #include "UTBotCharacter.h"
 
 /*
-[2015.09.01-21.14.33:684][ 67]UT:Warning: Trollface Skill 0.000000 character
-[2015.09.01-21.14.33:684][ 67]UT:Warning: Judas Skill 1.000000 character /Game/EpicInternal/Teams/BlackLegion/NecrisMale03.NecrisMale03_C
-[2015.09.01-21.14.33:684][ 67]UT:Warning: Skirge Skill 1.000000 character /Game/EpicInternal/Teams/ThunderCrash/TC_Male01.TC_Male01_C
-[2015.09.01-21.14.33:684][ 67]UT:Warning: Damian Skill 1.000000 character /Game/EpicInternal/Teams/BlackLegion/NecrisMale04.NecrisMale04_C
-[2015.09.01-21.14.33:684][ 67]UT:Warning: Arachne Skill 1.200000 character /Game/RestrictedAssets/Character/Human/Female/FemaleNecrisCharacter.FemaleNecrisCharacter_C
-[2015.09.01-21.14.33:684][ 67]UT:Warning: Kraagesh Skill 1.500000 character /Game/RestrictedAssets/Character/Skaarji/SkaarjiCharacterContent.SkaarjiCharacterContent_C
-[2015.09.01-21.14.33:684][ 67]UT:Warning: Guardian Skill 2.000000 character /Game/RestrictedAssets/Character/Skaarji/SkaarjiCharacterContent.SkaarjiCharacterContent_C
-[2015.09.01-21.14.33:684][ 67]UT:Warning: Harbinger Skill 2.000000 character /Game/EpicInternal/Teams/BlackLegion/NecrisMale05.NecrisMale05_C
-[2015.09.01-21.14.33:684][ 67]UT:Warning: Nocturne Skill 2.000000 character /Game/RestrictedAssets/Character/Human/Female/FemaleNecrisCharacter.FemaleNecrisCharacter_C
-[2015.09.01-21.14.33:684][ 67]UT:Warning: Deslok Skill 2.000000 character /Game/EpicInternal/Teams/ThunderCrash/TC_Male04.TC_Male04_C
-[2015.09.01-21.14.33:684][ 67]UT:Warning: Karag Skill 2.000000 character
-[2015.09.01-21.14.33:684][ 67]UT:Warning: Genghis Skill 2.000000 character /Game/EpicInternal/Teams/ThunderCrash/TC_Male03.TC_Male03_C
-[2015.09.01-21.14.33:684][ 67]UT:Warning: Samael Skill 2.500000 character /Game/EpicInternal/Teams/BlackLegion/NecrisMale01.NecrisMale01_C
-[2015.09.01-21.14.33:684][ 67]UT:Warning: Cadaver Skill 3.000000 character /Game/EpicInternal/Teams/BlackLegion/NecrisMale04.NecrisMale04_C
-[2015.09.01-21.14.33:684][ 67]UT:Warning: Cryss Skill 3.000000 character /Game/RestrictedAssets/Character/Human/Female/FemaleNecrisCharacter.FemaleNecrisCharacter_C
-[2015.09.01-21.14.33:684][ 67]UT:Warning: Taye Skill 3.000000 character /Game/EpicInternal/Teams/ThunderCrash/TC_Male01.TC_Male01_C
-[2015.09.01-21.14.33:684][ 67]UT:Warning: Malise Skill 3.000000 character /Game/RestrictedAssets/Character/Human/Female/FemaleNecrisCharacter.FemaleNecrisCharacter_C
-[2015.09.01-21.14.33:684][ 67]UT:Warning: Drekorig Skill 3.200000 character /Game/RestrictedAssets/Character/Skaarji/SkaarjiCharacterContent.SkaarjiCharacterContent_C
-[2015.09.01-21.14.33:684][ 67]UT:Warning: Leeb Skill 3.500000 character /Game/EpicInternal/Teams/ThunderCrash/TC_Male02.TC_Male02_C
-[2015.09.01-21.14.33:684][ 67]UT:Warning: Gkoblok Skill 4.000000 character /Game/RestrictedAssets/Character/Skaarji/SkaarjiCharacterContent.SkaarjiCharacterContent_C
-[2015.09.01-21.14.33:684][ 67]UT:Warning: Othello Skill 4.000000 character /Game/EpicInternal/Teams/ThunderCrash/TC_Male05.TC_Male05_C
-[2015.09.01-21.14.33:684][ 67]UT:Warning: Kryss Skill 4.000000 character /Game/RestrictedAssets/Character/Human/Female/FemaleNecrisCharacter.FemaleNecrisCharacter_C
 [2015.09.01-21.14.33:684][ 67]UT:Warning: Riker Skill 4.000000 character /Game/EpicInternal/Teams/ThunderCrash/TC_Male03.TC_Male03_C
-[2015.09.01-21.14.33:684][ 67]UT:Warning: Kragoth Skill 4.000000 character /Game/EpicInternal/Teams/BlackLegion/NecrisMale02.NecrisMale02_C
-[2015.09.01-21.14.33:684][ 67]UT:Warning: Grail Skill 4.000000 character /Game/EpicInternal/Teams/BlackLegion/NecrisMale03.NecrisMale03_C
-[2015.09.01-21.14.33:684][ 67]UT:Warning: Gaargod Skill 4.500000 character /Game/RestrictedAssets/Character/Skaarji/SkaarjiCharacterContent.SkaarjiCharacterContent_C
 [2015.09.01-21.14.33:684][ 67]UT:Warning: Thannis Skill 4.500000 character /Game/EpicInternal/Teams/ThunderCrash/TC_Male02.TC_Male02_C
 [2015.09.01-21.14.33:684][ 67]UT:Warning: Garog Skill 4.500000 character /Game/RestrictedAssets/Character/Skaarji/SkaarjiCharacterContent.SkaarjiCharacterContent_C
 [2015.09.01-21.14.33:684][ 67]UT:Warning: Alanna Skill 5.000000 character /Game/RestrictedAssets/Character/Human/Female/FemaleNecrisCharacter.FemaleNecrisCharacter_C
-[2015.09.01-21.14.33:684][ 67]UT:Warning: Malakai Skill 5.000000 character /Game/EpicInternal/Teams/BlackLegion/NecrisMale01.NecrisMale01_C
-[2015.09.01-21.14.33:684][ 67]UT:Warning: Barktooth Skill 5.000000 character
-[2015.09.01-21.14.33:684][ 67]UT:Warning: Picard Skill 5.000000 character /Game/EpicInternal/Teams/ThunderCrash/TC_Male02.TC_Male02_C
 [2015.09.01-21.14.33:684][ 67]UT:Warning: Torgr Skill 5.000000 character /Game/RestrictedAssets/Character/Skaarji/SkaarjiCharacterContent.SkaarjiCharacterContent_C
-[2015.09.01-21.14.33:684][ 67]UT:Warning: Raven Skill 5.300000 character /Game/RestrictedAssets/Character/Human/Female/FemaleNecrisCharacter.FemaleNecrisCharacter_C
-[2015.09.01-21.14.33:685][ 67]UT:Warning: Jakob Skill 6.000000 character /Game/EpicInternal/Teams/ThunderCrash/TC_Male05.TC_Male05_C
-[2015.09.01-21.14.33:685][ 67]UT:Warning: Freylis Skill 6.000000 character /Game/RestrictedAssets/Character/Human/Female/FemaleNecrisCharacter.FemaleNecrisCharacter_C
-[2015.09.01-21.14.33:685][ 67]UT:Warning: Dominator Skill 6.000000 character /Game/RestrictedAssets/Character/Skaarji/SkaarjiCharacterContent.SkaarjiCharacterContent_C
-[2015.09.01-21.14.33:685][ 67]UT:Warning: Necroth Skill 6.000000 character /Game/EpicInternal/Teams/BlackLegion/NecrisMale03.NecrisMale03_C
-[2015.09.01-21.14.33:685][ 67]UT:Warning: Lauren Skill 6.500000 character
-[2015.09.01-21.14.33:685][ 67]UT:Warning: Visse Skill 6.800000 character /Game/RestrictedAssets/Character/Human/Female/FemaleNecrisCharacter.FemaleNecrisCharacter_C
-[2015.09.01-21.14.33:685][ 67]UT:Warning: Skakruk Skill 7.000000 character /Game/RestrictedAssets/Character/Skaarji/SkaarjiCharacterContent.SkaarjiCharacterContent_C
-[2015.09.01-21.14.33:685][ 67]UT:Warning: Loque Skill 7.500000 character /Game/EpicInternal/Teams/BlackLegion/NecrisMale01.NecrisMale01_C
-[2015.09.01-21.14.33:685][ 67]UT:Warning: Clanlord Skill 7.500000 character /Game/RestrictedAssets/Character/Skaarji/SkaarjiCharacterContent.SkaarjiCharacterContent_C
-[2015.09.01-21.14.33:685][ 67]UT:Warning: Malcolm Skill 7.900000 character /Game/RestrictedAssets/Character/Malcom_New/Malcolm_New.Malcolm_New_C
 */
 /*
 1/3.5/6
 
 15 challenges, unlock new character every 5.  Challenges ramp up enemy skill along the way
 
-Damian    1.0   NecrisM  (become 0.5)
-Skirge    1.0   TC
-Arachne   1.2   NecrisF
-Kraagesh  1.5   Skaarj
+1v1 
+Kali 1.2
+Harbinger 2.0
+Othello 4.0
+Harbinger 4.5
+Jakob 6.0
+Loque 7.5
 
-Samael    2.5   NecrisM
-Taye      3.0   TC
-Malisse   3.0   NecrisF
-Gkoblok   4.0   Skaarj
+FFA Easy 
+Genghis 2.5
+Kali 1.2
+Acolyte 0.5
+Judas 1.0
+Guardian 2.0 ++
 
-Grail     4.0   NecrisM  (Become 4.3)
-Barktooth 5.0
-Raven     5.3            (Become 5.5)
-Dominator 6.0
+FFA Medium 
+Genghis  2.5
+Drekorig 3.2
+Cadaver 3.0
+Kryss 4.0
+Othello  4.0
+
+FFA Hard
+Freylis 6.0
+Jakob 6.0
+Picard 5.0
+Skakruk 7.0
+Necroth 6.0
+
+Medium TC/Skaarj
+Genghis  2.5
+Drekorig 3.2
+Leeb     3.5
+Othello  4.0
+Gaargod  4.5
+
+
+Hard Skaarj/TC
+[2015.09.01-21.14.33:685][ 67]UT:Warning: Clanlord Skill 7.500000 character /Game/RestrictedAssets/Character/Skaarji/SkaarjiCharacterContent.SkaarjiCharacterContent_C
+[2015.09.01-21.14.33:685][ 67]UT:Warning: Skakruk Skill 7.000000 character /Game/RestrictedAssets/Character/Skaarji/SkaarjiCharacterContent.SkaarjiCharacterContent_C
+[2015.09.01-21.14.33:685][ 67]UT:Warning: Malcolm Skill 7.900000 character /Game/RestrictedAssets/Character/Malcom_New/Malcolm_New.Malcolm_New_C
+[2015.09.01-21.14.33:685][ 67]UT:Warning: Jakob Skill 6.000000 character /Game/EpicInternal/Teams/ThunderCrash/TC_Male05.TC_Male05_C
+[2015.09.01-21.14.33:684][ 67]UT:Warning: Picard Skill 5.000000 character /Game/EpicInternal/Teams/ThunderCrash/TC_Male02.TC_Male02_C
+
+rename Kryss (too close to Cryss) - new Solace
+
+remove Lauren
+Get rid of UTBotConfig ini
 */
 
 UUTChallengeManager::UUTChallengeManager(const FObjectInitializer& ObjectInitializer)
@@ -75,43 +70,58 @@ UUTChallengeManager::UUTChallengeManager(const FObjectInitializer& ObjectInitial
 {
 	// @TODO FIXMESTEVE build fnames for all bots in .h
 
+	// player team roster, includes roster upgrades
 	PlayerTeamRoster = FTeamRoster(NSLOCTEXT("Challenges","PlayersTeamRoster","Players Team"));
-	PlayerTeamRoster.Roster.Add(FName(TEXT("Damian")));
-	PlayerTeamRoster.Roster.Add(FName(TEXT("Skirge")));
-	PlayerTeamRoster.Roster.Add(FName(TEXT("Arachne")));
-	PlayerTeamRoster.Roster.Add(FName(TEXT("Kraagesh")));
+	PlayerTeamRoster.Roster.Add(NAME_Damian);
+	PlayerTeamRoster.Roster.Add(NAME_Skirge);
+	PlayerTeamRoster.Roster.Add(NAME_Arachne);
+	PlayerTeamRoster.Roster.Add(NAME_Kraagesh);
 
-	PlayerTeamRoster.Roster.Add(FName(TEXT("Samael")));
-	PlayerTeamRoster.Roster.Add(FName(TEXT("Taye")));
-	PlayerTeamRoster.Roster.Add(FName(TEXT("Malise")));
-	PlayerTeamRoster.Roster.Add(FName(TEXT("Gkoblok")));
+	PlayerTeamRoster.Roster.Add(NAME_Samael);
+	PlayerTeamRoster.Roster.Add(NAME_Taye);
+	PlayerTeamRoster.Roster.Add(NAME_Malise);
+	PlayerTeamRoster.Roster.Add(NAME_Gkoblok);
 
-	PlayerTeamRoster.Roster.Add(FName(TEXT("Grail")));
-	PlayerTeamRoster.Roster.Add(FName(TEXT("Barktooth")));
-	PlayerTeamRoster.Roster.Add(FName(TEXT("Raven")));
-	PlayerTeamRoster.Roster.Add(FName(TEXT("Dominator")));
+	PlayerTeamRoster.Roster.Add(NAME_Grail);
+	PlayerTeamRoster.Roster.Add(NAME_Barktooth);
+	PlayerTeamRoster.Roster.Add(NAME_Raven);
+	PlayerTeamRoster.Roster.Add(NAME_Dominator);
 
-	FTeamRoster EnemyRoster;
+	// enemy team rosters
+	FTeamRoster EasyNecrisRoster = FTeamRoster(NSLOCTEXT("Challenges","EasyEnemyNecris","Necris Recruit Team"));
+	EasyNecrisRoster.Roster.Add(NAME_Acolyte);
+	EasyNecrisRoster.Roster.Add(NAME_Judas);
+	EasyNecrisRoster.Roster.Add(NAME_Kali);
+	EasyNecrisRoster.Roster.Add(NAME_Harbinger);
+	EasyNecrisRoster.Roster.Add(NAME_Nocturne);
+	EnemyTeamRosters.Add(NAME_EasyNecrisTeam, EasyNecrisRoster);
 
-	// easy enemy team
-	EnemyRoster = FTeamRoster(NSLOCTEXT("Challenges","EnemyTeam_Skaarj_Default","Default Skaarj Team"));
-	EnemyRoster.Roster.Add(FName(TEXT("Cadaver")));
-	EnemyRoster.Roster.Add(FName(TEXT("Leeb")));
-	EnemyRoster.Roster.Add(FName(TEXT("Genghis")));
-	EnemyRoster.Roster.Add(FName(TEXT("Trollface")));
-	EnemyRoster.Roster.Add(FName(TEXT("Malakai")));
-	EnemyTeamRosters.Add(NAME_EnemyTeam_SkaarjDefault, EnemyRoster);
+	FTeamRoster MediumNecrisRoster = FTeamRoster(NSLOCTEXT("Challenges", "MediumEnemyNecris", "Necris Adept Team"));
+	MediumNecrisRoster.Roster.Add(NAME_Cadaver);
+	MediumNecrisRoster.Roster.Add(NAME_Cryss);
+	MediumNecrisRoster.Roster.Add(NAME_Kragoth);
+	MediumNecrisRoster.Roster.Add(NAME_Kryss);
+	MediumNecrisRoster.Roster.Add(NAME_Malakai);
+	EnemyTeamRosters.Add(NAME_MediumNecrisTeam, MediumNecrisRoster);
+
+	FTeamRoster HardNecrisRoster = FTeamRoster(NSLOCTEXT("Challenges", "HardEnemyNecris", "Necris Inhuman Team"));
+	HardNecrisRoster.Roster.Add(NAME_Loque);
+	HardNecrisRoster.Roster.Add(NAME_Freylis);
+	HardNecrisRoster.Roster.Add(NAME_Necroth);
+	HardNecrisRoster.Roster.Add(NAME_Visse);
+	HardNecrisRoster.Roster.Add(NAME_Malakai);
+	EnemyTeamRosters.Add(NAME_HardNecrisTeam, HardNecrisRoster);
 
 	Challenges.Add(NAME_ChallengeDM, 
 		FUTChallengeInfo(TEXT("Deathmatch Challenge"), TEXT("/Game/RestrictedAssets/Maps/DM-Outpost23"),
 		TEXT("DM"),
 		TEXT("This is a test of the challenge system.  Steve replace me please with the actual text.  Note you can use various rich text statements in here.\n\nThis is only a test."), 
-		4, 5, NAME_EnemyTeam_SkaarjDefault, NAME_EnemyTeam_SkaarjDefault, NAME_EnemyTeam_SkaarjDefault, NAME_ChallengeSlateBadgeName_DM));
+		4, 5, NAME_EasyNecrisTeam, NAME_EasyNecrisTeam, NAME_EasyNecrisTeam, NAME_ChallengeSlateBadgeName_DM));
 	Challenges.Add(NAME_ChallengeCTF, 
 		FUTChallengeInfo(TEXT("CTF Challenge"), TEXT("/Game/RestrictedAssets/Maps/WIP/CTF-Outside"),
 		TEXT("CTF"),
 		TEXT("This is a test of the challenge system part 2.  Steve replace me please with the actual text.  Note you can use various rich text statements in here."), 
-		4, 5, NAME_EnemyTeam_SkaarjDefault, NAME_EnemyTeam_SkaarjDefault, NAME_EnemyTeam_SkaarjDefault, NAME_ChallengeSlateBadgeName_CTF));
+		4, 5, NAME_EasyNecrisTeam, NAME_EasyNecrisTeam, NAME_EasyNecrisTeam, NAME_ChallengeSlateBadgeName_CTF));
 }
 
 UUTBotCharacter* UUTChallengeManager::ChooseBotCharacter(AUTGameMode* CurrentGame, uint8& TeamNum, int32 TotalStars) const
--- UnrealTournament/Source/UnrealTournament/Public/UTChallengeManager.h
+++ UnrealTournament/Source/UnrealTournament/Public/UTChallengeManager.h
@@ -9,8 +9,39 @@ static const FName NAME_PlayerTeamRoster(TEXT("PlayersTeam"));
 static const FName NAME_ChallengeDM(TEXT("InitialDeathmatchChallenge"));
 static const FName NAME_ChallengeCTF(TEXT("InitialCaptureTheFlagChallenge"));
 
-static const FName NAME_EnemyTeam_SkaarjDefault(TEXT("EnemyTeamSkaarjDefault"));
+static const FName NAME_EasyNecrisTeam(TEXT("EasyNecrisTeam"));
+static const FName NAME_MediumNecrisTeam(TEXT("MediumNecrisTeam"));
+static const FName NAME_HardNecrisTeam(TEXT("HardNecrisTeam"));
 
+static const FName NAME_Damian(TEXT("Damian"));
+static const FName NAME_Skirge(TEXT("Skirge"));
+static const FName NAME_Arachne(TEXT("Arachne"));
+static const FName NAME_Kraagesh(TEXT("Kraagesh"));
+static const FName NAME_Samael(TEXT("Samael"));
+static const FName NAME_Taye(TEXT("Taye"));
+static const FName NAME_Malise(TEXT("Malise"));
+static const FName NAME_Gkoblok(TEXT("Gkoblok"));
+static const FName NAME_Grail(TEXT("Grail"));
+static const FName NAME_Barktooth(TEXT("Barktooth"));
+static const FName NAME_Raven(TEXT("Raven"));
+static const FName NAME_Dominator(TEXT("Dominator"));
+
+static const FName NAME_Acolyte(TEXT("Acolyte"));
+static const FName NAME_Judas(TEXT("Judas"));
+static const FName NAME_Kali(TEXT("Kali"));
+static const FName NAME_Harbinger(TEXT("Harbinger"));
+static const FName NAME_Nocturne(TEXT("Nocturne"));
+
+static const FName NAME_Cadaver(TEXT("Cadaver"));
+static const FName NAME_Cryss(TEXT("Cryss"));
+static const FName NAME_Kragoth(TEXT("Kragoth"));
+static const FName NAME_Kryss(TEXT("Kryss"));
+static const FName NAME_Malakai(TEXT("Malakai"));
+
+static const FName NAME_Loque(TEXT("Loque"));
+static const FName NAME_Freylis(TEXT("Freylis"));
+static const FName NAME_Necroth(TEXT("Necroth"));
+static const FName NAME_Visse(TEXT("Visse"));
 
 UCLASS(Abstract)
 class UNREALTOURNAMENT_API UUTChallengeManager : public UObject
