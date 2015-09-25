ca0634b9cf937b0efaf154abc35f86cfc56a5d61
Steve Polge <steve.polge@epicgames.com>
2015-09-25 11:52:37 -0400

    Added transloc icon for translocator telefrag kills
    [CL 2706261 by Steve Polge in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Public/UTDmgType_Telefragged.h
+++ UnrealTournament/Source/UnrealTournament/Public/UTDmgType_Telefragged.h
@@ -18,5 +18,13 @@ class UNREALTOURNAMENT_API UUTDmgType_Telefragged : public UUTDamageType
 		FemaleSuicideMessage = NSLOCTEXT("UTDeathMessages", "FemaleSuicideMessage_Telefragged", "{Player2Name} telefragged herself.");
 		GibHealthThreshold = 10000000;
 		StatsName = "Telefrag";
+
+		static ConstructorHelpers::FObjectFinder<UTexture> WeaponTexture(TEXT("Texture2D'/Game/RestrictedAssets/Proto/UI/HUD/Elements/UI_HUD_BaseB.UI_HUD_BaseB'"));
+		HUDIcon.Texture = WeaponTexture.Object;
+
+		HUDIcon.U = 599.f;
+		HUDIcon.V = 462.f;
+		HUDIcon.UL = 121.f;
+		HUDIcon.VL = 50.f;
 	}
 };
