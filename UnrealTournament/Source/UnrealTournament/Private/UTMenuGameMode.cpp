bb5af2bcc4ea428f13ee3b9c02b00a1c324f692e
Peter Knepley <peter.knepley@epicgames.com>
2015-10-19 11:39:35 -0400

    Fix music cooking
    Merging 2733651
    
    //depot/UE4-UT/...
    
    to //depot/UE4-UT/...
    
    [CL 2733657 by Peter Knepley in UT-Release branch]

--- UnrealTournament/Source/UnrealTournament/Private/UTMenuGameMode.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/UTMenuGameMode.cpp
@@ -14,7 +14,8 @@ AUTMenuGameMode::AUTMenuGameMode(const class FObjectInitializer& ObjectInitializ
 	PlayerStateClass = AUTPlayerState::StaticClass();
 	PlayerControllerClass = AUTPlayerController::StaticClass();
 
-	static ConstructorHelpers::FObjectFinder<USoundBase> DefaultMusicRef[] = { TEXT("/Game/RestrictedAssets/Audio/Music/Music_UTMenu_Classic.Music_UTMenu_Classic"), TEXT("/Game/RestrictedAssets/Audio/Music/Music_HaveAnUnrealHalloween.Music_HaveAnUnrealHalloween") };
+	static ConstructorHelpers::FObjectFinder<USoundBase> DefaultMusicRef[] = { TEXT("/Game/RestrictedAssets/Audio/Music/Music_UTMenu_Classic.Music_UTMenu_Classic"), TEXT("/Game/RestrictedAssets/Audio/Music/FragCenterIntro.FragCenterIntro"),
+		TEXT("/Game/RestrictedAssets/Audio/Music/Music_HaveAnUnrealHalloween.Music_HaveAnUnrealHalloween") };
 
 	for (int32 i = 0; i < ARRAY_COUNT(DefaultMusicRef); i++)
 	{
