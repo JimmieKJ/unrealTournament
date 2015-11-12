b7f90080deae72900c08a00216749d9a22ccc18a
Joe Wilcox <Joe.Wilcox@epicgames.com>
2015-11-12 09:15:47 -0500

    - Allow the match summary to close to the menu
    [CL 2764026 by Joe Wilcox in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/Slate/SUWMatchSummary.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/Slate/SUWMatchSummary.cpp
@@ -1424,6 +1424,7 @@ FReply SUWMatchSummary::OnKeyUp(const FGeometry& MyGeometry, const FKeyEvent& In
 		{
 			if (InKeyEvent.GetKey().GetFName() == MenuKey.Key)
 			{
+				GetPlayerOwner()->CloseMatchSummary();
 				UTPC->execShowMenu();
 				return FReply::Handled();
 			}
