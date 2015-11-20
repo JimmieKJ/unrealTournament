80fc5450e74e9c70fd1e54961d8d66790af120ce
Peter Knepley <peter.knepley@epicgames.com>
2015-11-20 16:19:19 -0500

    Set up for unversioned cook
    [CL 2776030 by Peter Knepley in UT-Clean branch]

--- UnrealTournament/Build/Scripts/UnrealTournamentProto.Automation.cs
+++ UnrealTournament/Build/Scripts/UnrealTournamentProto.Automation.cs
@@ -345,6 +345,7 @@ class UnrealTournamentProto_BasicBuild : BuildCommand
             NoDebugInfo: Cmd.ParseParam("NoDebugInfo"),
             CrashReporter: !Cmd.ParseParam("mac"), // @todo Mac: change to true when Mac implementation is ready
             CreateReleaseVersion: "UTVersion0",
+            UnversionedCookedContent: true,
 			// if we are running, we assume this is a local test and don't chunk
 			Run: Cmd.ParseParam("Run"),
             StageDirectoryParam: UnrealTournamentBuild.GetArchiveDir()
