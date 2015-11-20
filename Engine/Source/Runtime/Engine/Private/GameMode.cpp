0e5d2ad31d3b75a32a75242b0f7657d615b05f3b
Peter Knepley <peter.knepley@epicgames.com>
2015-11-20 16:33:23 -0500

    Don't allow AGameMode::HandleMatchHasEnded to stop replay recording, UT does it later
    [CL 2776066 by Peter Knepley in UT-Clean branch]

--- Engine/Source/Runtime/Engine/Private/GameMode.cpp
+++ Engine/Source/Runtime/Engine/Private/GameMode.cpp
@@ -663,10 +663,13 @@ void AGameMode::HandleMatchHasEnded()
 {
 	GameSession->HandleMatchHasEnded();
 
+	// PLK - going to end replays on timer for UT
+	/*
 	if (IsHandlingReplays() && GetGameInstance() != nullptr)
 	{
 		GetGameInstance()->StopRecordingReplay();
 	}
+	*/
 }
 
 void AGameMode::StartToLeaveMap()
