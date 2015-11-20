2868d2f0996bedba6bbb872795282dce3db9c839
Peter Knepley <peter.knepley@epicgames.com>
2015-11-20 17:05:33 -0500

    Fixed issues with getting out of water.
    [CL 2776175 by Peter Knepley in UT-Clean branch]

--- Engine/Source/Runtime/Engine/Private/Components/CharacterMovementComponent.cpp
+++ Engine/Source/Runtime/Engine/Private/Components/CharacterMovementComponent.cpp
@@ -3054,6 +3054,8 @@ void UCharacterMovementComponent::PhysSwimming(float deltaTime, int32 Iterations
 	float NetFluidFriction  = 0.f;
 	float Depth = ImmersionDepth();
 	float NetBuoyancy = Buoyancy * Depth;
+	float OriginalAccelZ = Acceleration.Z;
+	bool bLimitedUpAccel = false;
 
 	if (!HasAnimRootMotion() && (Velocity.Z > 0.33f * MaxSwimSpeed) && (NetBuoyancy != 0.f))
 	{
@@ -3062,6 +3064,7 @@ void UCharacterMovementComponent::PhysSwimming(float deltaTime, int32 Iterations
 	}
 	else if (Depth < 0.65f)
 	{
+		bLimitedUpAccel = (Acceleration.Z > 0.f);
 		Acceleration.Z = FMath::Min(0.1f, Acceleration.Z);
 	}
 
@@ -3090,6 +3093,18 @@ void UCharacterMovementComponent::PhysSwimming(float deltaTime, int32 Iterations
 
 	if ( Hit.Time < 1.f && CharacterOwner)
 	{
+		if (bLimitedUpAccel && (Velocity.Z >= 0.f))
+		{
+			// allow upward velocity at surface if against obstacle
+			Velocity.Z += OriginalAccelZ * deltaTime;
+			Adjusted = Velocity * (1.f - Hit.Time)*deltaTime;
+			Swim(Adjusted, Hit);
+			if (!IsSwimming())
+			{
+				StartNewPhysics(remainingTime, Iterations);
+				return;
+			}
+		}
 		const FVector GravDir = FVector(0.f,0.f,-1.f);
 		const FVector VelDir = Velocity.GetSafeNormal();
 		const float UpDown = GravDir | VelDir;
