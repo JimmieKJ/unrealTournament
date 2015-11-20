6a9bf059c92d4620cbefd67945a61445074d7d3c
Peter Knepley <peter.knepley@epicgames.com>
2015-11-20 13:46:35 -0500

    Version files
    [CL 2775723 by Peter Knepley in UT-Clean branch]

--- Engine/Source/Programs/DotNETCommon/MetaData.cs
+++ Engine/Source/Programs/DotNETCommon/MetaData.cs
@@ -19,6 +19,6 @@ using System.Resources;
 #if !SPECIFIC_VERSION
 // Automatically generate a version number based on the time of compilation
 [assembly: AssemblyVersion( "4.11.0.0" )]
-[assembly: AssemblyInformationalVersion("4.11.0-2746626-UE4-UT")]
+[assembly: AssemblyInformationalVersion("4.11.0-2771652-UE4-UT")]
 #endif
 
