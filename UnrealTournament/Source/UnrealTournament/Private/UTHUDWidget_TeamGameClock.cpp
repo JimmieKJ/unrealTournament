334520f35c6ec7d2439e1dac07fa6476b106155c
Steve Polge <steve.polge@epicgames.com>
2015-11-30 10:01:47 -0500

    Don't show mouse warning red dot for spectators.
    [CL 2782807 by Steve Polge in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/UTHUDWidget_TeamGameClock.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/UTHUDWidget_TeamGameClock.cpp
@@ -44,7 +44,7 @@ void UUTHUDWidget_TeamGameClock::Draw_Implementation(float DeltaTime)
 	}
 
 	TSharedPtr<GenericApplication> GenericApplication = FSlateApplication::Get().GetPlatformApplication();
-	if (GenericApplication.IsValid() && !GenericApplication->IsUsingHighPrecisionMouseMode())
+	if (GenericApplication.IsValid() && !GenericApplication->IsUsingHighPrecisionMouseMode() && UTHUDOwner->PlayerOwner && UTHUDOwner->PlayerOwner->PlayerState && !UTHUDOwner->PlayerOwner->PlayerState->bOnlySpectator)
 	{
 		Canvas->SetDrawColor(FColor::Red);
 		Canvas->DrawTile(Canvas->DefaultTexture, Canvas->ClipX - 5, Canvas->ClipY - 5, 5, 5, 0.0f, 0.0f, 1.0f, 1.0f);
