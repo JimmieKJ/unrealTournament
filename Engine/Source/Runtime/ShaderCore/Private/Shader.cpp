59b2cd0006b83a5b2055d70b27e8414e8610d317
Mark Satterthwaite <Mark.Satterthwaite@epicgames.com>
2015-12-09 09:06:16 -0500

    Copying CL #2726383: Shaders are only compatible if they are the same API.
    [CL 2796138 by Mark Satterthwaite in UT-Clean branch]

--- Engine/Source/Runtime/ShaderCore/Private/Shader.cpp
+++ Engine/Source/Runtime/ShaderCore/Private/Shader.cpp
@@ -506,17 +506,23 @@ bool FShaderResource::ArePlatformsCompatible(EShaderPlatform CurrentPlatform, ES
 			bFeatureLevelCompatible = GetMaxSupportedFeatureLevel(CurrentPlatform) >= GetMaxSupportedFeatureLevel(TargetPlatform);
 		}
 
-		bool bIsTargetD3D = TargetPlatform == SP_PCD3D_SM5 ||
-								TargetPlatform == SP_PCD3D_SM4 ||
-								TargetPlatform == SP_PCD3D_ES3_1 ||
-								TargetPlatform == SP_PCD3D_ES2;
-
-		bool bIsCurrentPlatformD3D = CurrentPlatform == SP_PCD3D_SM5 ||
-								CurrentPlatform == SP_PCD3D_SM4 ||
-								TargetPlatform == SP_PCD3D_ES3_1 ||
-								CurrentPlatform == SP_PCD3D_ES2;
-
-		bFeatureLevelCompatible = bFeatureLevelCompatible && (bIsCurrentPlatformD3D == bIsTargetD3D);
+		bool const bIsTargetD3D = TargetPlatform == SP_PCD3D_SM5 ||
+		TargetPlatform == SP_PCD3D_SM4 ||
+		TargetPlatform == SP_PCD3D_ES3_1 ||
+		TargetPlatform == SP_PCD3D_ES2;
+		
+		bool const bIsCurrentPlatformD3D = CurrentPlatform == SP_PCD3D_SM5 ||
+		CurrentPlatform == SP_PCD3D_SM4 ||
+		TargetPlatform == SP_PCD3D_ES3_1 ||
+		CurrentPlatform == SP_PCD3D_ES2;
+		
+		bool const bIsCurrentMetal = IsMetalPlatform(CurrentPlatform);
+		bool const bIsTargetMetal = IsMetalPlatform(TargetPlatform);
+		
+		bool const bIsCurrentOpenGL = IsOpenGLPlatform(CurrentPlatform);
+		bool const bIsTargetOpenGL = IsOpenGLPlatform(TargetPlatform);
+		
+		bFeatureLevelCompatible = bFeatureLevelCompatible && (bIsCurrentPlatformD3D == bIsTargetD3D && bIsCurrentMetal == bIsTargetMetal && bIsCurrentOpenGL == bIsTargetOpenGL);
 	}
 
 	return bFeatureLevelCompatible;
