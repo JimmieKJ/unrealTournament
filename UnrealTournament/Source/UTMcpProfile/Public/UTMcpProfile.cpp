822b7391f16131d97858015adaf99b3bf1107c08
unrealbot <unrealbot@epicgames.com>
2016-01-18 14:33:34 -0500

    Branch snapshot for CL 2774475
    [CL 2774475 in UT-Release branch]

--- UnrealTournament/Source/UTMcpProfile/Public/UTMcpProfile.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
-
-#include "UTMcpProfile.h"
-
-UUTMcpProfile::UUTMcpProfile(const FObjectInitializer& ObjectInitializer)
-: Super(ObjectInitializer)
-{
-}
