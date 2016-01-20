b37495e19ca57df78c89b5dcf3390922f2d837bb
buildmachine <builder-notify@epicgames.com>
2016-01-20 17:20:10 -0500

    UnrealTournamentBuild Version Files from changelist 2836021
    [CL 2837013 by buildmachine in UT-Release branch]

--- Engine/Source/Programs/DotNETCommon/MetaData.cs
+++ Engine/Source/Programs/DotNETCommon/MetaData.cs
@@ -19,6 +19,6 @@ using System.Resources;
 #if !SPECIFIC_VERSION
 // Automatically generate a version number based on the time of compilation
 [assembly: AssemblyVersion( "4.0.0.0" )]
-[assembly: AssemblyInformationalVersion("4.8.0-2771652+++depot+UE4-UT-Releases")]
+[assembly: AssemblyInformationalVersion("4.11.0-2836021+++depot+UE4-UT-Releases")]
 #endif
 
