c9a4790e11cca4109e055af92760735571d08f6f
Steve Polge <steve.polge@epicgames.com>
2016-02-04 19:27:42 -0500

    Changed warning to log
    [CL 2856512 by Steve Polge in UT-Clean branch]

--- Engine/Source/Runtime/Engine/Private/RepLayout.cpp
+++ Engine/Source/Runtime/Engine/Private/RepLayout.cpp
@@ -782,7 +782,7 @@ void FRepLayout::UpdateChangelistHistory( FRepState * RepState, UClass * ObjectC
 	// If our buffer is currently full, forcibly send the entire history
 	if ( DumpHistory )
 	{
-		UE_LOG( LogRep, Warning, TEXT( "FRepLayout::UpdateChangelistHistory: History overflow, forcing history dump %s" ), *ObjectClass->GetName() );
+		UE_LOG( LogRep, Log, TEXT( "FRepLayout::UpdateChangelistHistory: History overflow, forcing history dump %s" ), *ObjectClass->GetName() );
 	}
 
 	for ( int32 i = RepState->HistoryStart; i < RepState->HistoryEnd; i++ )
