67ca18ebc652edc50d3b2beafcbc2cdab395fa92
Peter Knepley <peter.knepley@epicgames.com>
2016-02-14 16:31:15 -0500

    Missed file in submission
    [CL 2866641 by Peter Knepley in UT-Clean branch]

---
+++ UnrealTournament/Source/UnrealTournament/Private/Online/UTPartyGameState.cpp
@@ -0,0 +1,10 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealTournament.h"
+#include "UTPartyGameState.h"
+
+
+UUTPartyGameState::UUTPartyGameState(const FObjectInitializer& ObjectInitializer) :
+	Super(ObjectInitializer)
+{
+}
