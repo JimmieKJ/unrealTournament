13117cac8b979152e0b3d0da5cf4cbf79fe5860c
Matt Oelfke <Matt.Oelfke@epicgames.com>
2016-02-22 11:25:37 -0500

    Removed 'editfixedsize' from firing states array
    [CL 2875742 by Matt Oelfke in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Public/UTWeapon.h
+++ UnrealTournament/Source/UnrealTournament/Public/UTWeapon.h
@@ -142,7 +142,7 @@ class UNREALTOURNAMENT_API AUTWeapon : public AUTInventory
 	TArray<FInstantHitDamageInfo> InstantHitInfo;
 
 	/** firing state for mode, contains core firing sequence and directs to appropriate global firing functions */
-	UPROPERTY(Instanced, EditAnywhere, EditFixedSize, BlueprintReadWrite, Category = "Weapon", NoClear)
+	UPROPERTY(Instanced, EditAnywhere, BlueprintReadWrite, Category = "Weapon", NoClear)
 	TArray<class UUTWeaponStateFiring*> FiringState;
 
 	/** True for melee weapons affected by "stopping power" (momentum added for weapons that don't normally impart much momentum) */
