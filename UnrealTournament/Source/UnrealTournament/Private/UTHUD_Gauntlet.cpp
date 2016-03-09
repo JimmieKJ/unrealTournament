b02d8c12331dbf6ff858dc0f35e525b2833ea89a
Joe Wilcox <Joe.Wilcox@epicgames.com>
2016-03-09 12:35:14 -0500

    Missing HUD files
    [CL 2901320 by Joe Wilcox in UT-Clean branch]

---
+++ UnrealTournament/Source/UnrealTournament/Private/UTHUD_Gauntlet.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+#include "UnrealTournament.h"
+#include "UTHUD_Gauntlet.h"
+
+AUTHUD_Gauntlet::AUTHUD_Gauntlet(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+}
+
---
+++ UnrealTournament/Source/UnrealTournament/Public/UTHUD_Gauntlet.h
@@ -0,0 +1,12 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "UTHUD_CTF.h"
+#include "UTHUD_Gauntlet.generated.h"
+
+UCLASS()
+class UNREALTOURNAMENT_API AUTHUD_Gauntlet : public AUTHUD_CTF
+{
+	GENERATED_UCLASS_BODY()
+};
