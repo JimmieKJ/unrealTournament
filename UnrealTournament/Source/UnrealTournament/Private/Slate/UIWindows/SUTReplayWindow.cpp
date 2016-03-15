4ea400a7b1b16b180993307add7e1d9b292d9352
Peter Knepley <peter.knepley@epicgames.com>
2016-03-15 13:17:28 -0400

    Replay window should go collapsed instead of hidden
    [CL 2910249 by Peter Knepley in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/Slate/UIWindows/SUTReplayWindow.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/Slate/UIWindows/SUTReplayWindow.cpp
@@ -672,7 +672,7 @@ FReply SUTReplayWindow::OnMouseMove(const FGeometry& MyGeometry, const FPointerE
 	}
 
 	//Update the tooltip if the mouse is over the slider
-	if (GetVis() != EVisibility::Hidden)
+	if (GetVis() != EVisibility::Collapsed && GetVis() != EVisibility::Hidden)
 	{
 		const FGeometry TimeSliderGeometry = FindChildGeometry(MyGeometry, TimeSlider.ToSharedRef());
 		bDrawTooltip = TimeSliderGeometry.IsUnderLocation(MouseEvent.GetScreenSpacePosition());
@@ -906,7 +906,7 @@ FText SUTReplayWindow::GetTooltipText() const
 
 EVisibility SUTReplayWindow::GetVis() const
 {
-	return PlayerOwner->AreMenusOpen() ? EVisibility::Hidden : EVisibility::Visible;
+	return PlayerOwner->AreMenusOpen() ? EVisibility::Collapsed : EVisibility::Visible;
 }
 
 void SUTReplayWindow::OnArrangeChildren(const FGeometry& AllottedGeometry, FArrangedChildren& ArrangedChildren) const
