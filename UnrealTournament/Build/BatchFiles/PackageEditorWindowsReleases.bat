fe5a132131c6aa29817cd25bc6a9f32df4ce78fc
Peter Knepley <peter.knepley@epicgames.com>
2016-03-16 13:18:54 -0400

    Update editor packaging script
    [CL 2911758 by Peter Knepley in UT-Clean branch]

--- UnrealTournament/Build/BatchFiles/PackageEditorWindowsReleases.bat
+++ UnrealTournament/Build/BatchFiles/PackageEditorWindowsReleases.bat
@@ -1,5 +1,18 @@
-pushd P:\Builds\UnrealTournament\++depot+UE4-UT-Releases-CL-%1
+@ECHO OFF
+if not "%1" == "" GOTO USECOMMANDLINE
 
-"C:\Program Files\7-Zip\7z" a UnrealTournament-Editor-XAN-%1-Win64.zip WindowsEditor
+:USERINPUT
+ECHO Zip up a release editor build.
+set /p BUILDCL=Enter the CL of the release editor build you want to package:
+GOTO ZIPUP
+
+:USECOMMANDLINE
+set BUILDCL=%1
+GOTO ZIPUP
+
+:ZIPUP
+pushd P:\Builds\UnrealTournament\++depot+UE4-UT-Releases-CL-%BUILDCL%
+
+"C:\Program Files\7-Zip\7z" a UnrealTournament-Editor-XAN-%BUILDCL%-Win64.zip WindowsEditor
 
 popd
