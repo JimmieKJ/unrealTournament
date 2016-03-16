d63b8892a6467666d791b0db9b4635385efb71af
Peter Knepley <peter.knepley@epicgames.com>
2016-03-16 15:11:42 -0400

    Add checksum support to linux server
    [CL 2912005 by Peter Knepley in UT-Clean branch]

--- UnrealTournament/Build/BatchFiles/PackageDirsReleases.bat
+++ UnrealTournament/Build/BatchFiles/PackageDirsReleases.bat
@@ -11,12 +11,15 @@ set BUILDCL=%1
 GOTO ZIPUP
 
 :ZIPUP
+
 xcopy RunUT-Shipping.bat \\isi-shares\Builds_UnrealTournament\++depot+UE4-UT-Releases-CL-%BUILDCL%\WindowsNoEditor
 xcopy RunUT-Shipping32.bat \\isi-shares\Builds_UnrealTournament\++depot+UE4-UT-Releases-CL-%BUILDCL%\WindowsNoEditor
 xcopy RunUT-Test.bat \\isi-shares\Builds_UnrealTournament\++depot+UE4-UT-Releases-CL-%BUILDCL%\WindowsNoEditor
 xcopy RunUT-Test32.bat \\isi-shares\Builds_UnrealTournament\++depot+UE4-UT-Releases-CL-%BUILDCL%\WindowsNoEditor
 
 pushd \\isi-shares\Builds_UnrealTournament\++depot+UE4-UT-Releases-CL-%BUILDCL%
+
+"C:\Program Files (x86)\wxChecksums\wxcksums" --create=LinuxServer\%BUILDCL%.sfv LinuxServer\
 
 "C:\Program Files\7-Zip\7z" a UnrealTournament-Client-XAN-%BUILDCL%-Win.zip WindowsNoEditor
 
