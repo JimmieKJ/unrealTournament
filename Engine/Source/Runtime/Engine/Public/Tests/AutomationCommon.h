15f78b2c1640c534f1468edc028b6df6ee71bd65
Peter Knepley <peter.knepley@epicgames.com>
2016-03-31 15:04:35 -0400

    Fix shipping build
    [CL 2929402 by Peter Knepley in UT-Clean branch]

--- Engine/Source/Runtime/Engine/Public/Tests/AutomationCommon.h
+++ Engine/Source/Runtime/Engine/Public/Tests/AutomationCommon.h
@@ -6,6 +6,8 @@
 #include "AutomationTest.h"
 #include "Delegate.h"
 
+#if (WITH_DEV_AUTOMATION_TESTS || WITH_PERF_AUTOMATION_TESTS)
+
 ///////////////////////////////////////////////////////////////////////
 // Common Latent commands which are used across test type. I.e. Engine, Network, etc...
 
@@ -171,3 +173,5 @@ DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FMatineePerformanceCaptureCommand
 * Latent command to run an exec command that also requires a UWorld.
 */
 DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FExecWorldStringLatentCommand, FString, ExecCommand);
+
+#endif // (WITH_DEV_AUTOMATION_TESTS || WITH_PERF_AUTOMATION_TESTS)
