7850c7a71f1267737f103cfb02e3a0af29b3938f
Steve Polge <steve.polge@epicgames.com>
2016-04-08 08:39:13 -0400

    Increased flag gradual return time.
    [CL 2937888 by Steve Polge in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/UTCTFRoundGame.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/UTCTFRoundGame.cpp
@@ -495,7 +495,7 @@ void AUTCTFRoundGame::InitFlags()
 		if (Base != NULL && Base->MyFlag)
 		{
 			AUTCarriedObject* Flag = Base->MyFlag;
-			Flag->AutoReturnTime = 8.f; // fixmesteve make config
+			Flag->AutoReturnTime = 9.f; // fixmesteve make config
 			Flag->bGradualAutoReturn = true;
 			Flag->bDisplayHolderTrail = true;
 			Flag->bShouldPingFlag = true;
