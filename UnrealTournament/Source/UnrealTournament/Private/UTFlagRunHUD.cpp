d1b3869c09007e18dbea9fa3b6cbf51883dbf033
Steve Polge <steve.polge@epicgames.com>
2016-04-12 19:23:31 -0400

    Integrate showing own lives remaining into player pips, and made them all larger.
    [CL 2941646 by Steve Polge in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/UTFlagRunHUD.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/UTFlagRunHUD.cpp
@@ -26,76 +26,85 @@ void AUTFlagRunHUD::DrawHUD()
 	if (!bShowScores && GS && GS->GetMatchState() == MatchState::InProgress && GS->bAsymmetricVictoryConditions)
 	{
 		// draw pips for players alive on each team @TODO move to widget
-		TArray<AUTPlayerState*> LivePlayers;
 		int32 OldRedCount = RedPlayerCount;
 		int32 OldBlueCount = BluePlayerCount;
 		RedPlayerCount = 0;
 		BluePlayerCount = 0;
 		float XAdjust = 0.015f * Canvas->ClipX * GetHUDWidgetScaleOverride();
-		float PipSize = 0.02f * Canvas->ClipX * GetHUDWidgetScaleOverride();
-		float SkullPipSize = PipSize * 0.6f;
-		float XOffsetRed = 0.5f * Canvas->ClipX - XAdjust - PipSize;
+		float BasePipSize = 0.025f * Canvas->ClipX * GetHUDWidgetScaleOverride();
+		float XOffsetRed = 0.5f * Canvas->ClipX - XAdjust - BasePipSize;
 		float XOffsetBlue = 0.5f * Canvas->ClipX + XAdjust;
 		float YOffset = 0.1f * Canvas->ClipY * GetHUDWidgetScaleOverride();
 		float XOffsetText = 0.f;
 		FFontRenderInfo TextRenderInfo;
 		TextRenderInfo.bEnableShadow = true;
+		TArray<AUTPlayerState*> LivePlayers;
+		AUTPlayerState* HUDPS = GetScorerPlayerState();
 		for (APlayerState* PS : GS->PlayerArray)
 		{
 			AUTPlayerState* UTPS = Cast<AUTPlayerState>(PS);
 			if (UTPS != NULL && UTPS->Team != NULL && !UTPS->bOnlySpectator  && !UTPS->bIsInactive)
 			{
-				bool bIsAttacker = (GS->bRedToCap == (UTPS->Team->TeamIndex == 0));
-				if (bIsAttacker ? GS->bAttackerLivesLimited : GS->bDefenderLivesLimited)
+				UTPS->SelectionOrder = (UTPS == HUDPS) ? 0 : 1;
+				LivePlayers.Add(UTPS);
+			}
+		}
+		LivePlayers.Sort([](AUTPlayerState& A, AUTPlayerState& B) { return A.SelectionOrder < B.SelectionOrder; });
+		for (AUTPlayerState* UTPS : LivePlayers)
+		{
+			bool bIsAttacker = (GS->bRedToCap == (UTPS->Team->TeamIndex == 0));
+			float OwnerPipScaling = (UTPS == GetScorerPlayerState()) ? 1.5f : 1.f;
+			float PipSize = BasePipSize * OwnerPipScaling;
+			float SkullPipSize = 0.6f * BasePipSize * OwnerPipScaling;
+			if (bIsAttacker ? GS->bAttackerLivesLimited : GS->bDefenderLivesLimited)
+			{
+				if (!UTPS->bOutOfLives)
 				{
-					if (!UTPS->bOutOfLives)
+					bool bLastLife = (UTPS->RemainingLives == 0);
+					if (UTPS->Team->TeamIndex == 0)
 					{
-						bool bLastLife = (UTPS->RemainingLives == 0);
-						if (UTPS->Team->TeamIndex == 0)
-						{
-							RedPlayerCount++;
-							Canvas->SetLinearDrawColor(bLastLife ? FLinearColor::Yellow : FLinearColor::Red, 0.7f);
-							Canvas->DrawTile(PlayerStartIcon.Texture, XOffsetRed, YOffset, PipSize, PipSize, PlayerStartIcon.U, PlayerStartIcon.V, PlayerStartIcon.UL, PlayerStartIcon.VL, BLEND_Translucent);
-							XOffsetText = XOffsetRed;
-							XOffsetRed -= 1.1f*PipSize;
-						}
-						else
-						{
-							BluePlayerCount++;
-							Canvas->SetLinearDrawColor(bLastLife ? FLinearColor::Yellow : FLinearColor::Blue, 0.7f);
-							Canvas->DrawTile(PlayerStartIcon.Texture, XOffsetBlue, YOffset, PipSize, PipSize, PlayerStartIcon.U, PlayerStartIcon.V, PlayerStartIcon.UL, PlayerStartIcon.VL, BLEND_Translucent);
-							XOffsetText = XOffsetBlue;
-							XOffsetBlue += 1.1f*PipSize;
-						}
-						if (!bLastLife)
-						{
-							Canvas->SetLinearDrawColor(FLinearColor::White, 1.f);
-							Canvas->DrawText(TinyFont, FText::AsNumber(UTPS->RemainingLives), XOffsetText + 0.4f*PipSize, YOffset, 0.5f, 0.5f, TextRenderInfo);
-						}
+						RedPlayerCount++;
+						Canvas->SetLinearDrawColor(bLastLife ? FLinearColor::White : FLinearColor::Red, 0.7f);
+						Canvas->DrawTile(PlayerStartIcon.Texture, XOffsetRed, YOffset, PipSize, PipSize, PlayerStartIcon.U, PlayerStartIcon.V, PlayerStartIcon.UL, PlayerStartIcon.VL, BLEND_Translucent);
+						XOffsetText = XOffsetRed;
+						XOffsetRed -= 1.1f*PipSize;
+					}
+					else
+					{
+						BluePlayerCount++;
+						Canvas->SetLinearDrawColor(bLastLife ? FLinearColor::Yellow : FLinearColor::Blue, 0.7f);
+						Canvas->DrawTile(PlayerStartIcon.Texture, XOffsetBlue, YOffset, PipSize, PipSize, PlayerStartIcon.U, PlayerStartIcon.V, PlayerStartIcon.UL, PlayerStartIcon.VL, BLEND_Translucent);
+						XOffsetText = XOffsetBlue;
+						XOffsetBlue += 1.1f*PipSize;
+					}
+					if (!bLastLife)
+					{
+						Canvas->SetLinearDrawColor(FLinearColor::White, 1.f);
+						Canvas->DrawText(TinyFont, FText::AsNumber(UTPS->RemainingLives), XOffsetText + 0.4f*PipSize, YOffset + 0.2f*PipSize, 0.75f, 0.75f, TextRenderInfo);
 					}
 				}
-				else
+			}
+			else
+			{
+				// @todo fixmesteve optimize - one pass through pawns, set the value of cachedcharacter, show those with none or dead
+				AUTCharacter* Character = UTPS->GetUTCharacter();
+				Canvas->SetLinearDrawColor(FLinearColor::White, 0.7f);
+				if (!Character || Character->IsDead())
 				{
-					// @todo fixmesteve optimize - one pass through pawns, set the value of cachedcharacter, show those with none or dead
-					AUTCharacter* Character = UTPS->GetUTCharacter();
-					Canvas->SetLinearDrawColor(FLinearColor::White, 0.7f);
-					if (!Character || Character->IsDead())
+					if (UTPS->Team->TeamIndex == 0)
 					{
-						if (UTPS->Team->TeamIndex == 0)
-						{
-							Canvas->DrawTile(HUDAtlas, XOffsetRed, YOffset, SkullPipSize, SkullPipSize, 725, 0, 28, 36, BLEND_Translucent);
-							XOffsetText = XOffsetRed;
-							XOffsetRed -= 1.8f*SkullPipSize;
-						}
-						else
-						{
-							Canvas->DrawTile(HUDAtlas, XOffsetBlue, YOffset, SkullPipSize, SkullPipSize, 725, 0, 28, 36, BLEND_Translucent);
-							XOffsetText = XOffsetBlue;
-							XOffsetBlue += 1.8f*SkullPipSize;
-						}
-						Canvas->SetLinearDrawColor(FLinearColor::White, 1.f);
-						Canvas->DrawText(TinyFont, FText::AsNumber(int32(UTPS->RespawnTime + 1.f)), XOffsetText + 0.4f*SkullPipSize, YOffset, 0.5f, 0.5f, TextRenderInfo);
+						Canvas->DrawTile(HUDAtlas, XOffsetRed, YOffset, SkullPipSize, SkullPipSize, 725, 0, 28, 36, BLEND_Translucent);
+						XOffsetText = XOffsetRed;
+						XOffsetRed -= 1.8f*SkullPipSize;
 					}
+					else
+					{
+						Canvas->DrawTile(HUDAtlas, XOffsetBlue, YOffset, SkullPipSize, SkullPipSize, 725, 0, 28, 36, BLEND_Translucent);
+						XOffsetText = XOffsetBlue;
+						XOffsetBlue += 1.8f*SkullPipSize;
+					}
+					Canvas->SetLinearDrawColor(FLinearColor::White, 1.f);
+					Canvas->DrawText(TinyFont, FText::AsNumber(int32(UTPS->RespawnTime + 1.f)), XOffsetText + 0.4f*SkullPipSize, YOffset, 0.75f, 0.75f, TextRenderInfo);
 				}
 			}
 		}
@@ -113,7 +122,7 @@ void AUTFlagRunHUD::DrawHUD()
 		{
 			Canvas->SetLinearDrawColor(FLinearColor::Red, 0.5f - TimeSinceRedDeath);
 			float ScaledSize = 1.f + 2.f*TimeSinceRedDeath;
-			Canvas->DrawTile(PlayerStartIcon.Texture, XOffsetRed - 0.5f*(ScaledSize - 1.f)*PipSize, YOffset - 0.5f*(ScaledSize - 1.f)*PipSize, PipSize, PipSize, PlayerStartIcon.U, PlayerStartIcon.V, PlayerStartIcon.UL, PlayerStartIcon.VL, BLEND_Translucent);
+			Canvas->DrawTile(PlayerStartIcon.Texture, XOffsetRed - 0.5f*(ScaledSize - 1.f)*BasePipSize, YOffset - 0.5f*(ScaledSize - 1.f)*BasePipSize, BasePipSize, BasePipSize, PlayerStartIcon.U, PlayerStartIcon.V, PlayerStartIcon.UL, PlayerStartIcon.VL, BLEND_Translucent);
 		}
 
 		float TimeSinceBlueDeath = GetWorld()->GetTimeSeconds() - BlueDeathTime;
@@ -121,7 +130,7 @@ void AUTFlagRunHUD::DrawHUD()
 		{
 			Canvas->SetLinearDrawColor(FLinearColor::Blue, 0.5f - TimeSinceBlueDeath);
 			float ScaledSize = 1.f + 2.f*TimeSinceBlueDeath;
-			Canvas->DrawTile(PlayerStartIcon.Texture, XOffsetBlue - 0.5f*(ScaledSize - 1.f)*PipSize, YOffset - 0.5f*(ScaledSize - 1.f)*PipSize, PipSize, PipSize, PlayerStartIcon.U, PlayerStartIcon.V, PlayerStartIcon.UL, PlayerStartIcon.VL, BLEND_Translucent);
+			Canvas->DrawTile(PlayerStartIcon.Texture, XOffsetBlue - 0.5f*(ScaledSize - 1.f)*BasePipSize, YOffset - 0.5f*(ScaledSize - 1.f)*BasePipSize, BasePipSize, BasePipSize, PlayerStartIcon.U, PlayerStartIcon.V, PlayerStartIcon.UL, PlayerStartIcon.VL, BLEND_Translucent);
 		}
 	}
 }
