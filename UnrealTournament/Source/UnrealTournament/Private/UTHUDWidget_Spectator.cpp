2fade253afd1d4d14a5a7732b878e01f01104c01
Peter Knepley <peter.knepley@epicgames.com>
2016-04-12 11:43:34 -0400

    Get rid of press fire to ready up in ranked sessions
    [CL 2940979 by Peter Knepley in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/UTHUDWidget_Spectator.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/UTHUDWidget_Spectator.cpp
@@ -142,6 +142,10 @@ FText UUTHUDWidget_Spectator::GetSpectatorMessageText(bool &bViewingMessage)
 					? NSLOCTEXT("UUTHUDWidget_Spectator", "WaitingForCaster", "All players are ready. Press [Enter] to start match.")
 					: NSLOCTEXT("UUTHUDWidget_Spectator", "WaitingForReady", "Waiting for players to ready up.");
 			}
+			else if (UTGameState->bRankedSession)
+			{
+				SpectatorMessage = NSLOCTEXT("UUTHUDWidget_Spectator", "RankedSessionStart", "Your game will start shortly.");
+			}
 			else if (UTPS && UTPS->bOnlySpectator)
 			{
 				SpectatorMessage = NSLOCTEXT("UUTHUDWidget_Spectator", "WaitingForReady", "Waiting for players to ready up.");
