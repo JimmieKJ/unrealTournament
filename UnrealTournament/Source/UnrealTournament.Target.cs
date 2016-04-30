e09d1d3e346350a8959df9878f65722407ab6ea7
Joe Wilcox <Joe.Wilcox@epicgames.com>
2016-04-30 17:18:51 -0400

    Fixes the VS2003 dll errors.
    [CL 2962212 by Joe Wilcox in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament.Target.cs
+++ UnrealTournament/Source/UnrealTournament.Target.cs
@@ -19,14 +19,6 @@ public class UnrealTournamentTarget : TargetRules
     //
     // TargetRules interface.
     //
-    public override bool ConfigureToolchain(TargetInfo Target)
-    {
-        if (Target.Platform == UnrealTargetPlatform.Win32 || Target.Platform == UnrealTargetPlatform.Win64)
-        {
-            WindowsPlatform.Compiler = WindowsCompiler.VisualStudio2013;
-        }
-        return true;
-    }
 
     public override void SetupBinaries(
 		TargetInfo Target,
