5ce75dca0adae0b5a96608651cf3982fa83622e7
Steve Polge <steve.polge@epicgames.com>
2016-05-23 09:00:56 -0400

    UTGameVolumes support teams.  Players that are the same team as the volume are invulnerable while in it, while enemies are killed when entering.
    [CL 2986520 by Steve Polge in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/UTGameVolume.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/UTGameVolume.cpp
@@ -1,12 +1,53 @@
 // Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
 #include "UnrealTournament.h"
 #include "UTGameVolume.h"
+#include "UTDmgType_Suicide.h"
+#include "UTGameState.h"
 
 AUTGameVolume::AUTGameVolume(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
-	VolumeName = NSLOCTEXT("Volume", "GameVolume", "UnNamed");
+	VolumeName = NSLOCTEXT("Volume", "GameVolume", "Unnamed");
+	TeamIndex = 255;
 }
+
+void AUTGameVolume::ActorEnteredVolume(class AActor* Other)
+{
+	if (Other && bIsTeamSafeVolume)
+	{
+		AUTCharacter* P = Cast<AUTCharacter>(Other);
+		AUTGameState* GS = GetWorld()->GetGameState<AUTGameState>();
+		if (P && GS)
+		{
+			if (!GS->OnSameTeam(this, P))
+			{
+				P->TakeDamage(1000.f, FDamageEvent(UUTDmgType_Suicide::StaticClass()), nullptr, this);
+			}
+			else
+			{
+				P->bDamageHurtsHealth = false;
+			}
+		}
+	}
+}
+
+void AUTGameVolume::ActorLeavingVolume(class AActor* Other)
+{
+	if (Other && bIsTeamSafeVolume)
+	{
+		AUTCharacter* P = Cast<AUTCharacter>(Other);
+		if (P)
+		{
+			P->bDamageHurtsHealth = true;
+		}
+	}
+}
+
+void AUTGameVolume::SetTeamForSideSwap_Implementation(uint8 NewTeamNum)
+{
+	TeamIndex = NewTeamNum;
+}
+
 
 
 
--- UnrealTournament/Source/UnrealTournament/Public/UTGameVolume.h
+++ UnrealTournament/Source/UnrealTournament/Public/UTGameVolume.h
@@ -8,12 +8,30 @@
 */
 
 UCLASS(BlueprintType)
-class UNREALTOURNAMENT_API AUTGameVolume : public APhysicsVolume
+class UNREALTOURNAMENT_API AUTGameVolume : public APhysicsVolume, public IUTTeamInterface
 {
 	GENERATED_UCLASS_BODY()
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Gameplay")
 		FText VolumeName;
+
+	/** If team safe volume, associated team members are invulnerable and everyone else is killed entering this volume. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Gameplay")
+	bool bIsTeamSafeVolume;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Gameplay")
+		uint8 TeamIndex;
+
+	virtual void ActorEnteredVolume(class AActor* Other) override;
+	virtual void ActorLeavingVolume(class AActor* Other) override;
+
+	UFUNCTION(BlueprintImplementableEvent, meta = (DisplayName = "GetTeamNum"))
+		uint8 ScriptGetTeamNum();
+
+	/** return team number the object is on, or 255 for no team */
+	virtual uint8 GetTeamNum() const { return TeamIndex; };
+
+	virtual void SetTeamForSideSwap_Implementation(uint8 NewTeamNum) override;
 };
 
 
