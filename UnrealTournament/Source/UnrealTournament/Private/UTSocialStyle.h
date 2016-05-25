82a38b503c363f7e76d9543c049199a01836f102
Peter Knepley <peter.knepley@epicgames.com>
2016-05-25 13:09:28 -0400

    Fix github build issue
    [CL 2989862 by Peter Knepley in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/UTSocialStyle.h
+++ UnrealTournament/Source/UnrealTournament/Private/UTSocialStyle.h
@@ -1,6 +1,8 @@
 // Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
 #pragma once
 
+#if WITH_SOCIAL
+
 #include "Social.h"
 
 #include "UTSocialStyle.generated.h"
@@ -9,8 +11,11 @@ UCLASS(hidecategories = Object, MinimalAPI, BlueprintType)
 class USocialStyleAsset : public UDataAsset
 {
 	GENERATED_BODY()
+
 public:
 
 	UPROPERTY(EditAnywhere, Category = Properties)
-		FSocialStyle Style;
+	FSocialStyle Style;
 };
+
+#endif
