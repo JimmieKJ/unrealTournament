baa505a07e0002609ed825b26c28b466c3089f8c
Peter Knepley <peter.knepley@epicgames.com>
2016-05-25 14:50:28 -0400

    Unfix github issue, breaks UHT
    [CL 2990086 by Peter Knepley in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/UTSocialStyle.h
+++ UnrealTournament/Source/UnrealTournament/Private/UTSocialStyle.h
@@ -1,8 +1,6 @@
 // Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
 #pragma once
 
-#if WITH_SOCIAL
-
 #include "Social.h"
 
 #include "UTSocialStyle.generated.h"
@@ -17,5 +15,3 @@ public:
 	UPROPERTY(EditAnywhere, Category = Properties)
 	FSocialStyle Style;
 };
-
-#endif
