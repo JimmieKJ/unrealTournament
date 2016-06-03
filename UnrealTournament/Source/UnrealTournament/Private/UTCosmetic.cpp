840293221d7cf0c4a843e92c9efe21838a10457b
Peter Knepley <peter.knepley@epicgames.com>
2016-06-03 14:26:01 -0400

    Fix tutorial map issue
    [CL 2999944 by Peter Knepley in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/UTCosmetic.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/UTCosmetic.cpp
@@ -50,7 +50,7 @@ void AUTCosmetic::PreInitializeComponents()
 			{
 				if (bRootNotPrimitiveComponent)
 				{
-					PrimComponent->DetachFromParent(false);
+					PrimComponent->DetachFromParent(true);
 					SetRootComponent(PrimComponent);
 					bRootNotPrimitiveComponent = false;
 				}
