fd76ba9da61cb277fcf4e64a921f602e7c7d9ab1
Peter Knepley <peter.knepley@epicgames.com>
2016-06-08 10:38:14 -0400

    Copy LevelSequenceEditor plugin with editor
    [CL 3005787 by Peter Knepley in UT-Clean branch]

--- UnrealTournament/Build/Scripts/UnrealTournamentProto.Automation.cs
+++ UnrealTournament/Build/Scripts/UnrealTournamentProto.Automation.cs
@@ -856,8 +856,9 @@ namespace UnrealTournamentGame.Automation
 				UnusedPluginFilter.Exclude("/Engine/Plugins/.../UdpMessaging/...");
 				UnusedPluginFilter.Exclude("/Engine/Plugins/.../Developer/...");
 				UnusedPluginFilter.Exclude("/Engine/Plugins/.../Blendables/...");
+                UnusedPluginFilter.Exclude("/Engine/Plugins/.../LevelSequenceEditor/...");
 
-				RequiredFiles.RemoveWhere(FileName => UnusedPluginFilter.Matches(FileName));
+                RequiredFiles.RemoveWhere(FileName => UnusedPluginFilter.Matches(FileName));
 			}
 
 			static void RemoveConfidentialFiles(UnrealTargetPlatform Platform, SortedSet<string> RequiredFiles)
