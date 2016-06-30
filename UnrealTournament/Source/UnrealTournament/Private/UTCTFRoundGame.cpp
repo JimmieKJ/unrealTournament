2d47974f24f3734ce025b29749910ccfeb8a07f7
Steve Polge <steve.polge@epicgames.com>
2016-06-30 11:15:35 -0400

    Rally available 30 seconds into match, rather than one minute.
    [CL 3034278 by Steve Polge in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/UTCTFRoundGame.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/UTCTFRoundGame.cpp
@@ -905,7 +905,7 @@ void AUTCTFRoundGame::InitRound()
 		AUTPlayerState* PS = Cast<AUTPlayerState>(UTGameState->PlayerArray[i]);
 		PS->bHasLifeLimit = bPerPlayerLives;
 		PS->RoundKills = 0;
-		PS->NextRallyTime = GetWorld()->GetTimeSeconds() + ((PS->Team && IsTeamOnOffense(PS->Team->TeamIndex)) ? 60.f : 45.f);
+		PS->NextRallyTime = GetWorld()->GetTimeSeconds() + 30.f;
 		PS->RespawnWaitTime = IsPlayerOnLifeLimitedTeam(PS) ? LimitedRespawnWaitTime : UnlimitedRespawnWaitTime;
 		PS->SetRemainingBoosts(InitialBoostCount);
 		PS->bSpecialTeamPlayer = false;
