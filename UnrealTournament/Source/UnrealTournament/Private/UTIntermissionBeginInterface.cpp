892900b211d2d999615d1bb637b2b988d1a7c1ef
Thomas Ross <Thomas.Ross@epicgames.com>
2016-07-25 11:11:37 -0400

    Uploading missing interface .CPP file.
    [CL 3063207 by Thomas Ross in UT-Clean branch]

---
+++ UnrealTournament/Source/UnrealTournament/Private/UTIntermissionBeginInterface.cpp
@@ -0,0 +1,8 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealTournament.h"
+#include "UTIntermissionBeginInterface.h"
+
+UUTIntermissionBeginInterface::UUTIntermissionBeginInterface(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{}
