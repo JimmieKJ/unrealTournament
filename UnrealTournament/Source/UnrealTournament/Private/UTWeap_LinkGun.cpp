5984cc26f24c2c143d35e49f2a79715a0f13547b
Steve Polge <steve.polge@epicgames.com>
2016-08-16 12:58:01 -0400

    Link gun crosshair display of overheat shows full overheat range.
    [CL 3090653 by Steve Polge in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/UTWeap_LinkGun.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/UTWeap_LinkGun.cpp
@@ -737,7 +737,7 @@ void AUTWeap_LinkGun::DrawWeaponCrosshair_Implementation(UUTHUDWidget* WeaponHud
 			{
 				ChargeColor = (OverheatFactor > 0.8f) ? FLinearColor::Yellow : FLinearColor::White;
 			}
-			float ChargePct = FMath::Clamp(OverheatFactor, 0.f, 1.f);
+			float ChargePct = FMath::Clamp(OverheatFactor, 0.f, 1.5f)/1.5F;
 			WeaponHudWidget->DrawTexture(WeaponHudWidget->UTHUDOwner->HUDAtlas, 0, 0.5f * ScaledCircleSize*(1.f - ChargePct), ScaledCircleSize, ScaledCircleSize*ChargePct, 98, 936 + CircleSize*(1.f - ChargePct), CircleSize, CircleSize*ChargePct, 0.7f, ChargeColor, FVector2D(0.5f, 0.5f));
 		}
 	}
