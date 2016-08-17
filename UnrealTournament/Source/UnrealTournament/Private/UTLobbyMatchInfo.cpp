37738f1377b22550222dab7e1a74f2605e94cc5c
Steve Polge <steve.polge@epicgames.com>
2016-08-17 17:23:34 -0400

    Adjusted quickmatch bot skill levels:  Up to Green 5 is skill 1 , Up to Green 7 is skill 2, Up to Green 9 is skill 3.  Bronze 1 is skill 1, bot skill level goes up by 1 every two player level increases.
    [CL 3092530 by Steve Polge in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/UTLobbyMatchInfo.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/UTLobbyMatchInfo.cpp
@@ -465,21 +465,21 @@ void AUTLobbyMatchInfo::LaunchMatch(bool bQuickPlay, int32 DebugCode)
 		if (bQuickPlay) 
 		{
 			BotSkillLevel = 1;
-			if (RankCheck < 5)
+			if (RankCheck < 6)
 			{
 				BotSkillLevel = 1;
 			}
-			else if (RankCheck < 7)
+			else if (RankCheck < 8)
 			{
 				BotSkillLevel = 2;
 			}
-			else if (RankCheck < 9)
+			else if (RankCheck < 10)
 			{
 				BotSkillLevel = 3;
 			}
 			else
 			{
-				BotSkillLevel = FMath::Clamp(1 + (RankCheck-9)/3, 3, 6);
+				BotSkillLevel = FMath::Clamp(1 + (RankCheck-10)/2, 1, 6);
 			}
 		}
 
