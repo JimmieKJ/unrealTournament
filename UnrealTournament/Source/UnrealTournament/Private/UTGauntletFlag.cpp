b647d93fa63c361b538724701f02d7488172280c
Joe Wilcox <Joe.Wilcox@epicgames.com>
2016-08-30 06:39:46 -0400

    Deleting old dead code / objects
    [CL 3105689 by Joe Wilcox in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/UTGauntletFlag.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
-#include "UnrealTournament.h"
-#include "UTGauntletFlag.h"
-
-AUTGauntletFlag::AUTGauntletFlag(const FObjectInitializer& ObjectInitializer)
-: Super(ObjectInitializer)
-{
-}
-
--- UnrealTournament/Source/UnrealTournament/Private/UTGauntletGameMessage.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
-#include "UnrealTournament.h"
-#include "UTGauntletGameMessage.h"
-
-UUTGauntletGameMessage::UUTGauntletGameMessage(const FObjectInitializer& ObjectInitializer)
-: Super(ObjectInitializer)
-{
-}
-
