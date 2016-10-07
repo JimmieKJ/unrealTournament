12b154602b96d0e684836dfbd3c013ecab3b945e
Steve Polge <steve.polge@epicgames.com>
2016-10-07 14:25:55 -0400

    Added UTRallyPoint class WIP.
    [CL 3155217 by Steve Polge in UT-Clean branch]

---
+++ UnrealTournament/Source/UnrealTournament/Private/UTRallyPoint.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+#include "UnrealTournament.h"
+#include "UTRallyPoint.h"
+
+AUTRallyPoint::AUTRallyPoint(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+
+}
---
+++ UnrealTournament/Source/UnrealTournament/Public/UTRallyPoint.h
@@ -0,0 +1,12 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "UTRallyPoint.generated.h"
+
+UCLASS(Blueprintable)
+class UNREALTOURNAMENT_API AUTRallyPoint : public AUTGameObjective
+{
+	GENERATED_UCLASS_BODY()
+
+};
