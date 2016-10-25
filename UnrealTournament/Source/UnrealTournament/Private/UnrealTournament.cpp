242609253cf50f9a2401d5b0109a394d1ba9784c
Joe Wilcox <Joe.Wilcox@epicgames.com>
2016-10-25 07:32:55 -0400

    Some extra logging for a bug we are tracking down
    [CL 3173290 by Joe Wilcox in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/UnrealTournament.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/UnrealTournament.cpp
@@ -27,6 +27,7 @@ IMPLEMENT_PRIMARY_GAME_MODULE(FUTModule, UnrealTournament, "UnrealTournament");
 DEFINE_LOG_CATEGORY(UT);
 DEFINE_LOG_CATEGORY(UTNet);
 DEFINE_LOG_CATEGORY(UTLoading);
+DEFINE_LOG_CATEGORY(UTConnection);
 
 static uint32 UTGetNetworkVersion()
 {
