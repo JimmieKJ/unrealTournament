2969777aab148383169eb8013c453bdb0c30cb45
Joe Wilcox <Joe.Wilcox@epicgames.com>
2016-11-07 10:36:18 -0500

    Fix for double-tap dodge.
    [CL 3188665 by Joe Wilcox in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Public/UTProfileSettings.h
+++ UnrealTournament/Source/UnrealTournament/Public/UTProfileSettings.h
@@ -12,11 +12,11 @@
 #include "UTProfileSettings.generated.h"
 
 
-static const uint32 CURRENT_PROFILESETTINGS_VERSION = 36;
+static const uint32 CURRENT_PROFILESETTINGS_VERSION = 37;
 static const uint32 VALID_PROFILESETTINGS_VERSION = 32;
 static const uint32 WEAPONBAR_FIXUP_VERSION = 33;
 static const uint32 COMMENU_FIXUP_VERSION = 34;
-static const uint32 ENABLE_DOUBLETAP_DODGE_FIXUP_VERSION = 35;
+static const uint32 ENABLE_DOUBLETAP_DODGE_FIXUP_VERSION = 37;
 
 class UUTLocalPlayer;
 
