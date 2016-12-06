814b99d3826a5e69131edc5d0617b9918ccb4b53
Steve Polge <steve.polge@epicgames.com>
2016-12-06 12:23:54 -0500

    Don't allow additional rally requests by player when one is already pending.
    [CL 3223398 by Steve Polge in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/UTFlagRunGame.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/UTFlagRunGame.cpp
@@ -642,7 +642,7 @@ bool AUTFlagRunGame::HandleRallyRequest(AController* C)
 	// if can rally, teleport with transloc effect, set last rally time
 	AUTFlagRunGameState* GS = GetWorld()->GetGameState<AUTFlagRunGameState>();
 	AUTTeamInfo* Team = UTPlayerState ? UTPlayerState->Team : nullptr;
-	if (Team && UTPlayerState->bCanRally && GS->bAttackersCanRally && UTCharacter && GS && IsMatchInProgress() && !GS->IsMatchIntermission() && ((Team->TeamIndex == 0) == GS->bRedToCap) && GS->FlagBases.IsValidIndex(Team->TeamIndex) && GS->FlagBases[Team->TeamIndex] != nullptr)
+	if (Team && UTPlayerState->bCanRally && !GetWorldTimerManager().IsTimerActive(UTPlayerState->RallyTimerHandle) && GS->bAttackersCanRally && UTCharacter && GS && IsMatchInProgress() && !GS->IsMatchIntermission() && ((Team->TeamIndex == 0) == GS->bRedToCap) && GS->FlagBases.IsValidIndex(Team->TeamIndex) && GS->FlagBases[Team->TeamIndex] != nullptr)
 	{
 		if (UTCharacter->GetCarriedObject())
 		{
