3a6e1b16b30cac10e7f7e940ad84c9682e113817
Matt Oelfke <Matt.Oelfke@epicgames.com>
2016-12-08 18:15:14 -0500

    Rocket Salvo: 3 rockets per target -> 2 per target; now caps at 10 targets (closest first); moved rocket spawn position so it's easier to see for the user
    [CL 3227986 by Matt Oelfke in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Public/UTRocketSalvoBoost.h
+++ UnrealTournament/Source/UnrealTournament/Public/UTRocketSalvoBoost.h
@@ -14,9 +14,10 @@ public:
 	AUTRocketSalvoBoost(const FObjectInitializer& OI)
 		: Super(OI)
 	{
-		TimeRemaining = 1.4f;
-		TriggeredTime = 1.4f;
-		TargetingRange = 6000.0f;
+		TimeRemaining = 1.0f;
+		TriggeredTime = 1.0f;
+		TargetingRange = 8000.0f;
+		MaxTargets = 10;
 	}
 
 	/** the projectile to use */
@@ -25,6 +26,9 @@ public:
 	/** range to look for enemies */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	float TargetingRange;
+	/** maximum number of targets */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 MaxTargets;
 
 	virtual void GivenTo(AUTCharacter* NewOwner, bool bAutoActivate) override
 	{
@@ -42,21 +46,28 @@ public:
 	virtual void FireSalvo()
 	{
 		AUTGameState* GS = GetWorld()->GetGameState<AUTGameState>();
-		FActorSpawnParameters Params;
-		Params.Instigator = UTOwner;
-		const FVector SpawnLoc = UTOwner->GetActorLocation() + FVector(0.0f, 0.0f, UTOwner->GetSimpleCollisionHalfHeight() * 1.2f);
+		const FVector MyLoc = UTOwner->GetActorLocation();
+		TArray<APawn*> Targets;
 		for (FConstPawnIterator It = GetWorld()->GetPawnIterator(); It; ++It)
 		{
 			if (It->IsValid() && 
 				It->Get() != UTOwner && 
 				!It->Get()->bTearOff && 
-				(It->Get()->GetActorLocation() - UTOwner->GetActorLocation()).Size() < TargetingRange && 
+				(It->Get()->GetActorLocation() - MyLoc).Size() < TargetingRange &&
 				(GS == nullptr || !GS->OnSameTeam(It->Get(), UTOwner)))
+			{
+				Targets.Add(It->Get());
+			}
+			Targets.Sort([this, MyLoc](const APawn& A, const APawn& B) { return (A.GetActorLocation() - MyLoc).Size() < (B.GetActorLocation() - MyLoc).Size(); });
+			FActorSpawnParameters Params;
+			Params.Instigator = UTOwner;
+			const FVector SpawnLoc = UTOwner->GetActorLocation() + UTOwner->GetActorRotation().Vector() * UTOwner->GetSimpleCollisionRadius() + FVector(0.0f, 0.0f, UTOwner->GetSimpleCollisionHalfHeight() * 0.9f);
+			for (int32 i = FMath::Min<int32>(Targets.Num(), MaxTargets) - 1; i >= 0; i--)
 			{
 				AUTProj_Rocket* Rocket = GetWorld()->SpawnActor<AUTProj_Rocket>(ProjClass, SpawnLoc, FRotator(90.0f, 0.0f, 0.0f), Params);
 				if (Rocket != nullptr)
 				{
-					Rocket->TargetActor = It->Get();
+					Rocket->TargetActor = Targets[i];
 				}
 			}
 		}
