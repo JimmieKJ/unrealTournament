a43ad26ff6685e5a297c4472ffcc33b905bfbe3c
Thomas Ross <Thomas.Ross@epicgames.com>
2016-12-16 13:24:21 -0500

    #codereview Steve.Polge,Peter.Knepley - Removing Analytics from Editor
    [CL 3238391 by Thomas Ross in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/UTAnalytics.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/UTAnalytics.cpp
@@ -61,7 +61,7 @@ static const FString SecureAnalyticsEndpoint = TEXT("https://datarouter.ol.epicg
 
 void FUTAnalytics::Initialize()
 {
-	if (IsRunningCommandlet())
+	if (IsRunningCommandlet() || GIsEditor)
 	{
 		return;
 	}
