a5e9a971f5118af0a5e60c08995501aca780cc89
Peter Knepley <peter.knepley@epicgames.com>
2016-12-21 14:53:24 -0500

    Change behavior per Uriel
    [CL 3242117 by Peter Knepley in UT-Clean branch]

--- Engine/Source/Runtime/Engine/Private/Streaming/TextureStreamingBuild.cpp
+++ Engine/Source/Runtime/Engine/Private/Streaming/TextureStreamingBuild.cpp
@@ -364,9 +364,8 @@ int32* FStreamingTextureLevelContext::GetBuildDataIndexRef(UTexture2D* Texture2D
 		}
 
 		FTextureBoundState& BoundState = BoundStates[Texture2D->LevelIndex];
-		check(BoundState.Texture == nullptr || BoundState.Texture == Texture2D);
 
-		if (BoundState.BuildDataTimestamp == BuildDataTimestamp)
+		if (BoundState.BuildDataTimestamp == BuildDataTimestamp && BoundState.Texture == Texture2D)
 		{
 			return &BoundState.BuildDataIndex; // Only return the bound static if it has data relative to this component.
 		}
